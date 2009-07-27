fac4e5c571f3503e1c529d0ff707c04b9696c7b1
Viktor Demydov <vdemidov@gmail.com>
2009-07-27 10:16:19 +0300

    Мелкое изменение форматирования кода

--- u_CoordConverterAbstract.pas
+++ u_CoordConverterAbstract.pas
@@ -1,12 +1,15 @@
 unit u_CoordConverterAbstract;
 
 interface
+
 uses
   Types;
+
 type
   TExtendedPoint = record
    X, Y: Extended;
   end;
+
   ICoordConverter = interface
   ['{3EE2987F-7681-425A-8EFE-B676C506CDD4}']
     function Pos2LonLat(XY : TPoint; Azoom : byte) : TExtendedPoint;
@@ -18,6 +21,7 @@ type
     function Pos2LonLat(XY : TPoint; Azoom : byte) : TExtendedPoint; virtual; abstract;
     function LonLat2Pos(Ll : TExtendedPoint; Azoom : byte) : Tpoint; virtual; abstract;
   end;
+
 implementation
 
 end.
