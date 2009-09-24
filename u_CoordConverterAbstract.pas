04779f428904b60d3c96b4464a0cb789cab009b8
Viktor Demydov <vdemidov@gmail.com>
2009-09-24 23:12:32 +0300

    Добавил в ICoordConverter функцию преобразования в координаты другой карты

--- u_CoordConverterAbstract.pas
+++ u_CoordConverterAbstract.pas
@@ -15,6 +15,7 @@ type
     function Pos2LonLat(XY : TPoint; Azoom : byte) : TExtendedPoint;
     function LonLat2Pos(Ll : TExtendedPoint; Azoom : byte) : Tpoint;
     function LonLat2Metr(Ll : TExtendedPoint) : TExtendedPoint;
+    function Pos2OtherMap(XY : TPoint; Azoom : byte; AOtherMapCoordConv: ICoordConverter):TPoint;
   end;
 
   TCoordConverterAbstract = class(TInterfacedObject, ICoordConverter)
@@ -22,8 +23,21 @@ type
     function Pos2LonLat(XY : TPoint; Azoom : byte) : TExtendedPoint; virtual; abstract;
     function LonLat2Pos(Ll : TExtendedPoint; Azoom : byte) : Tpoint; virtual; abstract;
     function LonLat2Metr(Ll : TExtendedPoint) : TExtendedPoint; virtual; abstract;
+    function Pos2OtherMap(XY : TPoint; Azoom : byte; AOtherMapCoordConv: ICoordConverter):TPoint; virtual;
   end;
 
 implementation
+
+{ TCoordConverterAbstract }
+
+function TCoordConverterAbstract.Pos2OtherMap(XY: TPoint; Azoom: byte;
+  AOtherMapCoordConv: ICoordConverter): TPoint;
+begin
+  if (AOtherMapCoordConv = nil) then begin
+    Result := XY;
+  end else begin
+    Result := AOtherMapCoordConv.LonLat2Pos(Pos2LonLat(XY, Azoom), Azoom);
+  end;
+end;
 
 end.
