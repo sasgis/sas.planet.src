0743114c07701f7054da09bbbf1a2223ac5ede95
Viktor Demydov <vdemidov@gmail.com>
2009-11-14 15:25:52 +0200

    Базовый класс для отображаемых слоев.

--- u_MapLayerBasic.pas
+++ u_MapLayerBasic.pas
@@ -16,15 +16,16 @@ type
     FParentMap: TImage32;
     FLayer: TBitmapLayer;
     FScreenCenterPos: TPoint;
+    FScale: Double;
+    FScaleCenter: TPoint;
+    FMove: TPoint;
     function GetVisible: Boolean;
     procedure SetVisible(const Value: Boolean);
     procedure SetScreenCenterPos(const Value: TPoint);
 
     function GetLoadedPixelRect: TRect; virtual;
-    function GetVisiblePixelRect: TRect; virtual;
     function GetLoadedSizeInPixel: TPoint; virtual;
     function GetLoadedSizeInTile: TPoint; virtual;
-    function GetVisibleTopLeft: TPoint; virtual;
     function GetVisibleSizeInPixel: TPoint; virtual;
     function GetMapLayerLocationRect: TRect; virtual;
     function GetLoadedTopLeft: TPoint; virtual;
@@ -33,16 +34,17 @@ type
     function VisiblePixel2MapPixel(Pnt: TExtendedPoint): TExtendedPoint; overload; virtual;
     function MapPixel2VisiblePixel(Pnt: TPoint): TPoint; overload; virtual;
     function MapPixel2VisiblePixel(Pnt: TExtendedPoint): TExtendedPoint; overload; virtual;
-    function VisiblePixel2LoadedPixel(Pnt: TPoint): TPoint; virtual;
+    function VisiblePixel2LoadedPixel(Pnt: TPoint): TPoint; overload; virtual;
+    function VisiblePixel2LoadedPixel(Pnt: TExtendedPoint): TExtendedPoint; overload; virtual;
 
     function LoadedPixel2MapPixel(Pnt: TPoint): TPoint; overload; virtual;
     function LoadedPixel2MapPixel(Pnt: TExtendedPoint): TExtendedPoint; overload; virtual;
     function MapPixel2LoadedPixel(Pnt: TPoint): TPoint; overload; virtual;
     function MapPixel2LoadedPixel(Pnt: TExtendedPoint): TExtendedPoint; overload; virtual;
+    function LoadedPixel2VisiblePixel(Pnt: TPoint): TPoint; overload; virtual;
+    function LoadedPixel2VisiblePixel(Pnt: TExtendedPoint): TExtendedPoint; overload; virtual;
 
-    property VisibleTopLeft: TPoint read GetVisibleTopLeft;
     property VisibleSizeInPixel: TPoint read GetVisibleSizeInPixel;
-    property VisiblePixelRect: TRect read GetVisiblePixelRect;
 
     property LoadedTopLeft: TPoint read GetLoadedTopLeft;
     property LoadedPixelRect: TRect read GetLoadedPixelRect;
@@ -56,6 +58,8 @@ type
     procedure Show; virtual;
     procedure Hide; virtual;
     procedure Redraw; virtual;
+    procedure MoveTo(Pnt: TPoint); virtual;
+    procedure ScaleTo(AScale: Double; ACenterPoint: TPoint); virtual;
     property Visible: Boolean read GetVisible write SetVisible;
     property ScreenCenterPos: TPoint read FScreenCenterPos write SetScreenCenterPos;
   end;
@@ -65,6 +69,7 @@ uses
   Types,
   Forms,
   Graphics,
+  Math,
   u_GlobalState;
 
 { TGPSTrackLayer }
@@ -73,6 +78,7 @@ constructor TMapLayerBasic.Create(AParentMap: TImage32; ACenter: TPoint);
 var
   VLoadedSizeInPixel: TPoint;
 begin
+  FScale := 1;
   FParentMap := AParentMap;
   FLayer := TBitmapLayer.Create(FParentMap.Layers);
   FScreenCenterPos := ACenter;
@@ -102,7 +108,6 @@ end;
 
 procedure TMapLayerBasic.Redraw;
 begin
-  FLayer.Location := floatrect(MapLayerLocationRect);
 
 end;
 
@@ -114,7 +119,10 @@ end;
 procedure TMapLayerBasic.SetScreenCenterPos(const Value: TPoint);
 begin
   FScreenCenterPos := Value;
+  FMove := Point(0, 0);
+  FScale := 1;
   Redraw;
+  FLayer.Location := floatrect(MapLayerLocationRect);
 end;
 
 procedure TMapLayerBasic.SetVisible(const Value: Boolean);
@@ -135,24 +143,10 @@ begin
 end;
 
 
-function TMapLayerBasic.GetVisiblePixelRect: TRect;
-begin
-  Result.Left := ScreenCenterPos.X - FParentMap.Width div 2;
-  Result.Top := ScreenCenterPos.Y - FParentMap.Height div 2;
-  Result.Right := ScreenCenterPos.X + FParentMap.Width div 2;
-  Result.Bottom := ScreenCenterPos.Y + FParentMap.Height div 2;
-end;
-
 function TMapLayerBasic.GetVisibleSizeInPixel: TPoint;
 begin
   Result.X := FParentMap.Width;
   Result.Y := FParentMap.Height;
-end;
-
-function TMapLayerBasic.GetVisibleTopLeft: TPoint;
-begin
-  Result.X := ScreenCenterPos.X - FParentMap.Width div 2;
-  Result.Y := ScreenCenterPos.Y - FParentMap.Height div 2;
 end;
 
 function TMapLayerBasic.GetLoadedPixelRect: TRect;
@@ -164,6 +158,14 @@ begin
   Result.Top := ScreenCenterPos.Y - VSizeInPixel.Y div 2;
   Result.Right := ScreenCenterPos.X + VSizeInPixel.X div 2;
   Result.Bottom := ScreenCenterPos.Y + VSizeInPixel.Y div 2;
+end;
+
+function TMapLayerBasic.GetLoadedSizeInTile: TPoint;
+begin
+  Result.X := Ceil(Screen.Width / 256) + GState.TilesOut;
+//  Result.X := round(Screen.Width / 256)+(integer((Screen.Width mod 256)>0))+GState.TilesOut;
+  Result.Y := Ceil(Screen.Height / 256) + GState.TilesOut;
+//  Result.Y := round(Screen.Height / 256)+(integer((Screen.height mod 256)>0))+GState.TilesOut;
 end;
 
 function TMapLayerBasic.GetLoadedSizeInPixel: TPoint;
@@ -180,28 +182,14 @@ begin
   end;
 end;
 
-function TMapLayerBasic.GetLoadedSizeInTile: TPoint;
-begin
-// Result.X := Ceil(Screen.Width / 256) + GState.TilesOut;
-  Result.X := round(Screen.Width / 256)+(integer((Screen.Width mod 256)>0))+GState.TilesOut;
-// Result.Y := Ceil(Screen.Height / 256) + GState.TilesOut;
-  Result.Y := round(Screen.Height / 256)+(integer((Screen.height mod 256)>0))+GState.TilesOut;
-end;
-
 
 function TMapLayerBasic.GetMapLayerLocationRect: TRect;
 var
   VLoadedSize: TPoint;
-  VVisibleSize: TPoint;
 begin
   VLoadedSize := GetLoadedSizeInPixel;
-  VVisibleSize := GetVisibleSizeInPixel;
-  Result := bounds(
-    (VVisibleSize.X - VLoadedSize.X) div 2,
-    (VVisibleSize.Y - VLoadedSize.Y) div 2,
-    VLoadedSize.X,
-    VLoadedSize.Y
-  );
+  Result.TopLeft := LoadedPixel2VisiblePixel(Point(0, 0));
+  Result.BottomRight := LoadedPixel2VisiblePixel(VLoadedSize);
 end;
 
 function TMapLayerBasic.GetLoadedTopLeft: TPoint;
@@ -214,37 +202,11 @@ begin
 end;
 
 
-function TMapLayerBasic.VisiblePixel2MapPixel(Pnt:TPoint):TPoint;
-begin
-  Result := GetVisibleTopLeft;
-  Result.X := Result.X + Pnt.X;
-  Result.Y := Result.Y + Pnt.y;
-end;
-
-
-function TMapLayerBasic.MapPixel2VisiblePixel(Pnt: TPoint): TPoint;
-var
-  VVisibleSize: TPoint;
-begin
-  VVisibleSize := GetVisibleSizeInPixel;
-  Result.X := Pnt.X - ScreenCenterPos.X + (VVisibleSize.X div 2);
-  Result.Y := Pnt.Y - ScreenCenterPos.Y + (VVisibleSize.Y div 2);
-end;
-
 function TMapLayerBasic.LoadedPixel2MapPixel(Pnt: TPoint): TPoint;
 begin
   Result := GetLoadedTopLeft;
   Result.X := Result.X + Pnt.X;
   Result.Y := Result.Y + Pnt.y;
-end;
-
-function TMapLayerBasic.MapPixel2LoadedPixel(Pnt: TPoint): TPoint;
-var
-  VSize: TPoint;
-begin
-  VSize := GetLoadedSizeInPixel;
-  Result.X := Pnt.X - ScreenCenterPos.X + (VSize.X div 2);
-  Result.Y := Pnt.Y - ScreenCenterPos.Y + (VSize.Y div 2);
 end;
 
 function TMapLayerBasic.LoadedPixel2MapPixel(Pnt: TExtendedPoint): TExtendedPoint;
@@ -256,6 +218,15 @@ begin
   Result.Y := VTopLeft.Y + Pnt.y;
 end;
 
+function TMapLayerBasic.MapPixel2LoadedPixel(Pnt: TPoint): TPoint;
+var
+  VSize: TPoint;
+begin
+  VSize := GetLoadedSizeInPixel;
+  Result.X := Pnt.X - ScreenCenterPos.X + (VSize.X div 2);
+  Result.Y := Pnt.Y - ScreenCenterPos.Y + (VSize.Y div 2);
+end;
+
 function TMapLayerBasic.MapPixel2LoadedPixel(Pnt: TExtendedPoint): TExtendedPoint;
 var
   VSize: TPoint;
@@ -265,22 +236,36 @@ begin
   Result.Y := Pnt.Y - ScreenCenterPos.Y + (VSize.Y / 2);
 end;
 
+function TMapLayerBasic.MapPixel2VisiblePixel(Pnt: TPoint): TPoint;
+var
+  VPoint: TPoint;
+begin
+  VPoint := MapPixel2LoadedPixel(Pnt);
+  Result := LoadedPixel2VisiblePixel(Pnt);
+end;
+
 function TMapLayerBasic.MapPixel2VisiblePixel(Pnt: TExtendedPoint): TExtendedPoint;
 var
-  VSize: TPoint;
+  VPoint: TExtendedPoint;
 begin
-  VSize := GetVisibleSizeInPixel;
-  Result.X := Pnt.X - ScreenCenterPos.X + (VSize.X / 2);
-  Result.Y := Pnt.Y - ScreenCenterPos.Y + (VSize.Y / 2);
+  VPoint := MapPixel2LoadedPixel(Pnt);
+  Result := LoadedPixel2VisiblePixel(VPoint);
+end;
+
+function TMapLayerBasic.VisiblePixel2MapPixel(Pnt:TPoint):TPoint;
+var
+  VPoint: TPoint;
+begin
+  VPoint := VisiblePixel2LoadedPixel(Pnt);
+  Result := LoadedPixel2MapPixel(VPoint);
 end;
 
 function TMapLayerBasic.VisiblePixel2MapPixel(Pnt: TExtendedPoint): TExtendedPoint;
 var
-  VTopLeft: TPoint;
+  VPoint: TExtendedPoint;
 begin
-  VTopLeft := GetVisibleTopLeft;
-  Result.X := VTopLeft.X + Pnt.X;
-  Result.Y := VTopLeft.Y + Pnt.y;
+  VPoint := VisiblePixel2LoadedPixel(Pnt);
+  Result := LoadedPixel2MapPixel(VPoint);
 end;
 
 function TMapLayerBasic.VisiblePixel2LoadedPixel(Pnt: TPoint): TPoint;
@@ -291,8 +276,59 @@ begin
   VVisibleSize := GetVisibleSizeInPixel;
   VLoadedSize := GetLoadedSizeInPixel;
 
-  Result.X := Pnt.X + (VLoadedSize.X - VVisibleSize.X) div 2;
-  Result.Y := Pnt.Y + (VLoadedSize.Y - VVisibleSize.Y) div 2;
+  Result.X := Trunc(((Pnt.X + FMove.X) - FScaleCenter.X) * FScale + FScaleCenter.X + (VLoadedSize.X - VVisibleSize.X) / 2);
+  Result.Y := Trunc(((Pnt.Y + FMove.Y) - FScaleCenter.Y) * FScale + FScaleCenter.Y + (VLoadedSize.Y - VVisibleSize.Y) / 2);
+end;
+
+function TMapLayerBasic.VisiblePixel2LoadedPixel(
+  Pnt: TExtendedPoint): TExtendedPoint;
+var
+  VVisibleSize: TPoint;
+  VLoadedSize: TPoint;
+begin
+  VVisibleSize := GetVisibleSizeInPixel;
+  VLoadedSize := GetLoadedSizeInPixel;
+
+  Result.X := ((Pnt.X + FMove.X) - FScaleCenter.X) * FScale + FScaleCenter.X + (VLoadedSize.X - VVisibleSize.X) / 2;
+  Result.Y := ((Pnt.Y + FMove.Y) - FScaleCenter.Y) * FScale + FScaleCenter.Y + (VLoadedSize.Y - VVisibleSize.Y) / 2;
+end;
+
+function TMapLayerBasic.LoadedPixel2VisiblePixel(Pnt: TPoint): TPoint;
+var
+  VVisibleSize: TPoint;
+  VLoadedSize: TPoint;
+begin
+  VVisibleSize := GetVisibleSizeInPixel;
+  VLoadedSize := GetLoadedSizeInPixel;
+
+  Result.X := trunc(((Pnt.X - (VLoadedSize.X - VVisibleSize.X) / 2) - FScaleCenter.X) / FScale + FScaleCenter.X - FMove.X);
+  Result.Y := trunc(((Pnt.Y - (VLoadedSize.Y - VVisibleSize.Y) / 2) - FScaleCenter.Y) / FScale + FScaleCenter.Y - FMove.Y);
+end;
+
+function TMapLayerBasic.LoadedPixel2VisiblePixel(
+  Pnt: TExtendedPoint): TExtendedPoint;
+var
+  VVisibleSize: TPoint;
+  VLoadedSize: TPoint;
+begin
+  VVisibleSize := GetVisibleSizeInPixel;
+  VLoadedSize := GetLoadedSizeInPixel;
+
+  Result.X := ((Pnt.X - (VLoadedSize.X - VVisibleSize.X) / 2) - FScaleCenter.X) / FScale + FScaleCenter.X - FMove.X;
+  Result.Y := ((Pnt.Y - (VLoadedSize.Y - VVisibleSize.Y) / 2) - FScaleCenter.Y) / FScale + FScaleCenter.Y - FMove.Y;
+end;
+
+procedure TMapLayerBasic.MoveTo(Pnt: TPoint);
+begin
+  FMove := Pnt;
+  FLayer.Location := FloatRect(MapLayerLocationRect);
+end;
+
+procedure TMapLayerBasic.ScaleTo(AScale: Double; ACenterPoint: TPoint);
+begin
+  FScaleCenter := ACenterPoint;
+  FScale := AScale;
+  FLayer.Location := FloatRect(MapLayerLocationRect);
 end;
 
 end.
