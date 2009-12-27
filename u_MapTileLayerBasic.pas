d69ca8998d956496dcab73bbcc4658eafd78a673
Viktor Demydov <vdemidov@gmail.com>
2009-12-27 23:52:01 +0200

    Продолжаю писать класс для отображения тайловых слоев карты.

--- u_MapTileLayerBasic.pas
+++ u_MapTileLayerBasic.pas
@@ -16,8 +16,9 @@ type
   protected
     FMapType: TMapType;
     FZoom: Byte;
+    function GetBitmapRectInTiles: TRect; virtual;
     function GetBitmapSizeInPixel: TPoint; override;
-    function GetFreezePointInBitmapPixel: TPoint; override;
+    function GetScreenCenterInBitmapPixels: TPoint; override;
   public
     procedure SetMapZoomAndPos(AMapType: TMapType; AXY: TPoint; AZoom: byte); virtual;
   end;
@@ -31,22 +32,23 @@ uses
 
 { TMapTileLayerBasic }
 
-function TMapTileLayerBasic.GetBitmapSizeInPixel: TPoint;
+function TMapTileLayerBasic.GetBitmapRectInTiles: TRect;
 var
   VConverter: ICoordConverter;
   VVisibleSize: TPoint;
   VVisibleMapRect: TRect;
-  VVisibleTilesRect: TRect;
-  VMapPixelsRect: TRect;
   VZoom: byte;
   VMapSize: Integer;
+  VMapSizeInTiles: Integer;
 begin
   VZoom := FZoom;
   VConverter := FMapType.GeoConvert;
   VMapSize := VConverter.PixelsAtZoom(VZoom);
+  VMapSizeInTiles := VConverter.TilesAtZoom(VZoom);
+
   VVisibleSize := GetVisibleSizeInPixel;
   if (VVisibleSize.X >= VMapSize) and (VVisibleSize.Y >= VMapSize) then begin
-    Result := VVisibleSize;
+    Result := Rect(0, 0, VMapSizeInTiles - 1, VMapSizeInTiles - 1);
   end else begin
     if (VVisibleSize.X >= VMapSize) or (VVisibleSize.Y >= VMapSize) then begin
       if (VVisibleSize.X >= VMapSize) then begin
@@ -68,29 +70,77 @@ begin
     end;
 
     VConverter.CheckPixelRect(VVisibleMapRect, VZoom, GState.CiclMap);
-    VVisibleTilesRect := VConverter.PixelRect2TileRect(VVisibleMapRect, VZoom);
-    VMapPixelsRect := VConverter.TileRect2PixelRect(VVisibleTilesRect, VZoom);
-    Result.X := VMapPixelsRect.Right - VMapPixelsRect.Left + 1;
-    Result.Y := VMapPixelsRect.Bottom - VMapPixelsRect.Top + 1;
-    if VMapPixelsRect.Left > VMapPixelsRect.Right then begin
-      Result.X := Result.X + VMapSize;
-    end;
-    if VMapPixelsRect.Top > VMapPixelsRect.Bottom then begin
-      Result.Y := Result.Y + VMapSize;
+    Result := VConverter.PixelRect2TileRect(VVisibleMapRect, VZoom);
+    if GState.TilesOut > 0 then begin
+      if (Result.Left > Result.Right) and (Result.Left - Result.Right <= GState.TilesOut * 2) then begin
+      end else begin
+        Dec(Result.Left, GState.TilesOut);
+        Inc(Result.Right, GState.TilesOut);
+      end;
+      Dec(Result.Top, GState.TilesOut);
+      Inc(Result.Bottom, GState.TilesOut);
+      VConverter.CheckTileRect(Result, VZoom, GState.CiclMap);
     end;
   end;
 end;
 
-function TMapTileLayerBasic.GetFreezePointInBitmapPixel: TPoint;
+function TMapTileLayerBasic.GetBitmapSizeInPixel: TPoint;
 var
-  VBitmapSize: TPoint;
+  VConverter: ICoordConverter;
+  VVisibleTilesRect: TRect;
+  VMapPixelsRect: TRect;
+  VZoom: byte;
+  VVisibleSize: TPoint;
+  VMapSize: Integer;
 begin
-  if FFreezeInCenter then begin
-    VBitmapSize := GetBitmapSizeInPixel;
-    Result := Point(VBitmapSize.X div 2, VBitmapSize.Y div 2);
-  end else begin
-    Result := FScaleCenterInBitmapPixel;
+  VZoom := FZoom;
+  VConverter := FMapType.GeoConvert;
+  VVisibleSize := GetVisibleSizeInPixel;
+  VMapSize := VConverter.PixelsAtZoom(VZoom);
+  VVisibleTilesRect := GetBitmapRectInTiles;
+  VMapPixelsRect := VConverter.TileRect2PixelRect(VVisibleTilesRect, VZoom);
+  Result.X := VMapPixelsRect.Right - VMapPixelsRect.Left + 1;
+  Result.Y := VMapPixelsRect.Bottom - VMapPixelsRect.Top + 1;
+  if VMapPixelsRect.Left > VMapPixelsRect.Right then begin
+    Result.X := Result.X + VMapSize;
   end;
+  if VMapPixelsRect.Top > VMapPixelsRect.Bottom then begin
+    Result.Y := Result.Y + VMapSize;
+  end;
+  if GState.CiclMap then begin
+    if VMapSize < VVisibleSize.X then begin
+      Result.X := (VVisibleSize.X div VMapSize) * VMapSize;
+    end;
+  end;
+end;
+
+function TMapTileLayerBasic.GetScreenCenterInBitmapPixels: TPoint;
+var
+  VConverter: ICoordConverter;
+  VVisibleSize: TPoint;
+  VBitmapSize: TPoint;
+  VZoom: byte;
+  VMapSize: Integer;
+begin
+  VZoom := FZoom;
+  VConverter := FMapType.GeoConvert;
+  VMapSize := VConverter.PixelsAtZoom(VZoom);
+  VVisibleSize := GetVisibleSizeInPixel;
+  VBitmapSize := GetBitmapSizeInPixel;
+  if (VVisibleSize.Y >= VMapSize) then begin
+    Result.Y := FScreenCenterPos.Y;
+  end else begin
+    if FScreenCenterPos.Y < VVisibleSize.Y div 2 then begin
+      Result.Y := FScreenCenterPos.Y;
+    end else begin
+      if VMapSize - FScreenCenterPos.Y < VVisibleSize.Y then begin
+        Result.Y := VBitmapSize.Y - (VMapSize - FScreenCenterPos.Y);
+      end else begin
+
+      end;
+    end;
+  end;
+
 end;
 
 procedure TMapTileLayerBasic.SetMapZoomAndPos(AMapType: TMapType;
@@ -102,4 +152,3 @@ begin
 end;
 
 end.
- 
