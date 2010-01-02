337a254c0220462f842d41f2e056cc9191e493d5
Viktor Demydov <vdemidov@gmail.com>
2010-01-03 00:03:06 +0200

    Для начала мелкие упрощения кода класса TECWWrite

--- UECWWrite.pas
+++ UECWWrite.pas
@@ -1,43 +1,58 @@
 unit UECWWrite;
-interface
- uses ECWwriter,ECWreader,
-  Windows, Messages, SysUtils, Variants, Classes, Graphics, JPEG, Controls, Forms,
-  Dialogs, StdCtrls,ExtCtrls;
 
- type
+interface
+
+uses
+  ECWwriter,
+  ECWreader,
+  Windows,
+  Messages,
+  SysUtils,
+  Variants,
+  Classes,
+  Graphics,
+  JPEG,
+  Controls,
+  Forms,
+  Dialogs,
+  StdCtrls,
+  ExtCtrls;
+
+type
   TlineRGB = array[0..0] of single;
   PlineRGB = ^TlineRGB;
 
- type Tptr = array [0..2] of pointer;
- ptr=^Tptr;
- TEcwRead = function(Sender:TObject;Line:cardinal; var lineR,LineG,LineB:PLineRGB):boolean;
- TEcwStatus = procedure(Line:cardinal);
+  TEcwRead = function(Sender:TObject;Line:cardinal; var lineR,LineG,LineB:PLineRGB):boolean;
+  TEcwStatus = procedure(Line:cardinal);
+  PNCSEcwCompressClient = ^NCSEcwCompressClient;
 
- type TECWWrite = class
+type
+  TECWWrite = class
   private
-   Handle: LongWord;
-   pClient:Pointer;
-   x:NCSError;
+    Handle: LongWord;
+    pClient: PNCSEcwCompressClient;
+    x:NCSError;
   public
-   constructor Create;
-   //ReadCallbackFunc(pClient:Pointer;nNextLine:cardinal;InputArray:ptr):boolean;
-   function Encode(ASender:TObject;FileName:string; Width,Height:cardinal;  CompressRatio:Single;
+    constructor Create();
+    function Encode(ASender:TObject;FileName:string; Width,Height:cardinal;  CompressRatio:Single;
              Hint:CompressHint; ReadDelegate:TEcwRead; StatusDelegate:TEcwStatus;
              Datum,Projection:string; SizeUnits:CellSizeUnits;
              CellIncrementX,CellIncrementY,OriginX,OriginY:double):integer;
- end;
+  end;
 
 
 var
   EcwRead:TEcwRead;
   Imwidth,ImHeight:cardinal;
-  lineR,lineG,lineB:PLineRGB;
   Sender:TObject;
-  cancelflag:boolean;
-//  function ReadCallbackFunc(pClient:Pointer;nNextLine:cardinal;InputArray:ptr):boolean;
 
 implementation
-uses Unit1,UImgFun,UThreadScleit,GR32;
+
+uses
+  Unit1,
+  UImgFun,
+  UThreadScleit,
+  GR32;
 
 constructor TECWWrite.Create;
 var _NCSEcwCompressAllocClient:NCSEcwCompressAllocClient;
@@ -53,9 +68,12 @@ begin
   pClient := _NCSEcwCompressAllocClient;
 end;
 
-function ReadCallbackFunc(pClient:Pointer;nNextLine:cardinal;InputArray:ptr):boolean;
+function ReadCallbackFunc(pClient:Pointer;nNextLine:cardinal;InputArray:Pointer):boolean; cdecl;
+  type
+    Tptr = array [0..2] of pointer;
+    Pptr=^Tptr;
 begin
- EcwRead(Sender,nNextLine,PlineRGB(InputArray[0]),PlineRGB(InputArray[1]),PlineRGB(InputArray[2]));
+ EcwRead(Sender,nNextLine,PlineRGB(PPtr(InputArray)[0]),PlineRGB(PPtr(InputArray)[1]),PlineRGB(PPtr(InputArray)[2]));
  result:=true;
 end;
 
@@ -84,33 +102,32 @@ begin
   EcwRead:=ReadDelegate;
   Imwidth:=Width;
   ImHeight:=Height;
-  cancelflag:=false;
-  NCSEcwCompressClient(pClient^).nInputBands:=3;
-  NCSEcwCompressClient(pClient^).nInOutSizeX:=Width;
-  NCSEcwCompressClient(pClient^).nInOutSizeY:=Height;
-  NCSEcwCompressClient(pClient^).eCompressFormat := COMPRESS_RGB;
-  NCSEcwCompressClient(pClient^).eCompressHint := Hint;
-  NCSEcwCompressClient(pClient^).fTargetCompression:=CompressRatio;
-  NCSEcwCompressClient(pClient^).eCellSizeUnits:= SizeUnits;
-  NCSEcwCompressClient(pClient^).fCellIncrementX:=CellIncrementX;
-  NCSEcwCompressClient(pClient^).fCellIncrementY:=CellIncrementY;
-  NCSEcwCompressClient(pClient^).fOriginX:=OriginX;
-  NCSEcwCompressClient(pClient^).fOriginY:=OriginY;
+  pClient^.nInputBands:=3;
+  pClient^.nInOutSizeX:=Width;
+  pClient^.nInOutSizeY:=Height;
+  pClient^.eCompressFormat := COMPRESS_RGB;
+  pClient^.eCompressHint := Hint;
+  pClient^.fTargetCompression:=CompressRatio;
+  pClient^.eCellSizeUnits:= SizeUnits;
+  pClient^.fCellIncrementX:=CellIncrementX;
+  pClient^.fCellIncrementY:=CellIncrementY;
+  pClient^.fOriginX:=OriginX;
+  pClient^.fOriginY:=OriginY;
 
-  for i:=0 to 15 do NCSEcwCompressClient(pClient^).szDatum[i]:=#0;
-  for i:=0 to 15 do NCSEcwCompressClient(pClient^).szProjection[i]:=#0;
+  for i:=0 to 15 do pClient^.szDatum[i]:=#0;
+  for i:=0 to 15 do pClient^.szProjection[i]:=#0;
   for i:=1 to 16 do
    begin
-    NCSEcwCompressClient(pClient^).szDatum[i-1]:=Datum[i];
-    NCSEcwCompressClient(pClient^).szProjection[i-1]:=Projection[i];
+    pClient^.szDatum[i-1]:=Datum[i];
+    pClient^.szProjection[i-1]:=Projection[i];
    end;
 
   for i:=1 to length(filename) do
-   NCSEcwCompressClient(pClient^).szOutputFilename[i-1]:=FileName[i];//FileName;//StringToOleStr('test.ecw');
+   pClient^.szOutputFilename[i-1]:=FileName[i];
 
-  NCSEcwCompressClient(pClient^).pReadCallback:= @ReadCallbackFunc;
-  NCSEcwCompressClient(pClient^).pStatusCallback:=nil;
-  NCSEcwCompressClient(pClient^).pCancelCallback:=@cancel;
+  pClient^.pReadCallback := ReadCallbackFunc;
+  pClient^.pStatusCallback:=nil;
+  pClient^.pCancelCallback:=@cancel;
   x:=_NCSEcwCompressOpen(pClient,false);
   if x = NCS_SUCCESS then
    begin
@@ -122,10 +139,7 @@ begin
 	if(x = NCS_SUCCESS) then
    begin
      FreeLibrary(Handle);
-	 end; {else
-   begin
-     FreeLibrary(Handle);
-   end;    }
+	 end;
   result:=integer(x);
 end;
 
