eb03ac412d754f7cf01de48af9d384632e6619e8
Viktor Demydov <vdemidov@gmail.com>
2010-01-23 15:19:48 +0200

    Добавил TKmlInfoSimple.Assign

--- u_KmlInfoSimple.pas
+++ u_KmlInfoSimple.pas
@@ -20,6 +20,7 @@ type
     Data: Array of TKMLData;
     constructor Create;
     destructor Destroy; override;
+    procedure Assign(Source: TKmlInfoSimple);
   end;
 
 
@@ -27,6 +28,25 @@ type
 implementation
 
 { TKmlInfoSimple }
+
+procedure TKmlInfoSimple.Assign(Source: TKmlInfoSimple);
+var
+  VElementsCount: Integer;
+  i: integer;
+begin
+  if Source <> nil then begin
+    VElementsCount := length(Source.Data);
+    SetLength(Data, VElementsCount);
+    for i := 0 to VElementsCount - 1 do begin
+      Data[i].PlacemarkID := Source.Data[i].PlacemarkID;
+      Data[i].Name := Source.Data[i].Name;
+      Data[i].description := Source.Data[i].description;
+      Data[i].coordinatesLT := Source.Data[i].coordinatesLT;
+      Data[i].coordinatesRD := Source.Data[i].coordinatesRD;
+      Data[i].coordinates := Copy(Source.Data[i].coordinates);
+    end;
+  end;
+end;
 
 constructor TKmlInfoSimple.Create;
 begin
