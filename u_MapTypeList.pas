693ef420da8c077d197ff5f6d5957e4362d48956
Viktor Demydov <vdemidov@gmail.com>
2010-05-24 22:44:54 +0300

    Добавил классы для работы со списками MapType-ов.

---
+++ u_MapTypeList.pas
@@ -0,0 +1,56 @@
+unit u_MapTypeList;
+
+interface
+
+uses
+  ActiveX,
+  i_IGUIDList,
+  i_MapTypes,
+  UMapType;
+
+type
+  TMapTypeList = class(TInterfacedObject, IMapTypeList)
+  private
+    FList: IGUIDList;
+    function GetMapTypeByGUID(AGUID: TGUID): IMapType;
+    function GetIterator: IEnumGUID;
+  public
+    procedure Add(AMap: IMapType);
+    constructor Create;
+    destructor Destroy; override;
+  end;
+
+implementation
+
+uses
+  u_GUIDList;
+
+{ TMapTypeList }
+
+procedure TMapTypeList.Add(AMap: IMapType);
+begin
+  FList.Add(AMap.GetMapType.GUID, AMap);
+end;
+
+constructor TMapTypeList.Create;
+begin
+  FList := TGUIDList.Create(False);
+end;
+
+destructor TMapTypeList.Destroy;
+begin
+  FList := nil;
+  inherited;
+end;
+
+function TMapTypeList.GetIterator: IEnumGUID;
+begin
+  Result := FList.GetGUIDEnum;
+end;
+
+function TMapTypeList.GetMapTypeByGUID(AGUID: TGUID): IMapType;
+begin
+  Result := Flist.GetByGUID(AGUID) as IMapType;
+end;
+
+end.
---
+++ u_MapTypeListGeneratorFromFullListBasic.pas
@@ -0,0 +1,62 @@
+unit u_MapTypeListGeneratorFromFullListBasic;
+
+interface
+
+uses
+  UMapType,
+  i_MapTypes;
+
+type
+  TMapTypeListGeneratorFromFullListBasic = class(TInterfacedObject, IMapTypeListFactory)
+  protected
+    FMainMap: Boolean;
+    FItemFactory: IMapTypeFactory;
+    function CheckIsAddMap(AMapType: TMapType): Boolean; virtual;
+    function CreateList: IMapTypeList; virtual;
+  public
+    constructor Create(AMainMapsList: Boolean; AFactory: IMapTypeFactory);
+  end;
+
+implementation
+
+uses
+  SysUtils,
+  u_GlobalState,
+  u_MapTypeList;
+
+{ TMapTypeListGeneratorFromFullListBasic }
+
+function TMapTypeListGeneratorFromFullListBasic.CheckIsAddMap(
+  AMapType: TMapType): Boolean;
+begin
+  if FMainMap then begin
+    Result := not AMapType.asLayer;
+  end else begin
+    Result := AMapType.asLayer;
+  end;
+end;
+
+constructor TMapTypeListGeneratorFromFullListBasic.Create(
+  AMainMapsList: Boolean; AFactory: IMapTypeFactory);
+begin
+  FMainMap := AMainMapsList;
+  FItemFactory := AFactory;
+end;
+
+function TMapTypeListGeneratorFromFullListBasic.CreateList: IMapTypeList;
+var
+  i: Integer;
+  VMapType: TMapType;
+  VList: TMapTypeList;
+begin
+  VList := TMapTypeList.Create;
+  Result := VList;
+  for i := 0 to Length(GState.MapType) - 1 do begin
+    VMapType := GState.MapType[i];
+    if CheckIsAddMap(VMapType) then begin
+      VList.Add(FItemFactory.CreateItem(VMapType));
+    end;
+  end;
+end;
+
+end.
