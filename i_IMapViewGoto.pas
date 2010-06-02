88abdadee6fcfaf24180218371a7b8a036b4780f
Viktor Demydov <vdemidov@gmail.com>
2010-06-03 01:09:16 +0300

    Юниты для вызова гео поиска.

---
+++ i_IMapViewGoto.pas
@@ -0,0 +1,17 @@
+unit i_IMapViewGoto;
+
+interface
+
+uses
+  t_GeoTypes;
+
+type
+  IMapViewGoto = interface
+    ['{33FDD537-B089-4ED6-8AB4-720E47B3C8B8}']
+    procedure GotoPos(ALonLat: TDoublePoint);
+  end;
+
+implementation
+
+end.
+ 
