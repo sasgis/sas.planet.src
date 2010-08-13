1b6744282e84958e69d84fd38d09a95cb6c2cac9
Viktor Demydov <vdemidov@gmail.com>
2010-08-13 16:41:18 +0300

    Новые лоадеры и сейверы с использованием Vampyre Imaging Library. Но их еще нужно тестить.

---
+++ BitmapTileSaveLoad/u_BitmapTileVampyreLoader.pas
@@ -0,0 +1,143 @@
+unit u_BitmapTileVampyreLoader;
+
+interface
+
+uses
+  Classes,
+  Imaging,
+  GR32,
+  i_BitmapTileSaveLoad;
+
+type
+  TVampyreBasicBitmapTileLoader = class(TInterfacedObject, IBitmapTileLoader)
+  private
+    FFormat: TImageFileFormat;
+  public
+    constructor Create(AFormat: TImageFileFormat);
+    procedure LoadFromFile(AFileName: string; ABtm: TCustomBitmap32);
+    procedure LoadFromStream(AStream: TStream; ABtm: TCustomBitmap32);
+  end;
+
+  TVampyreBasicBitmapTileLoaderPNG = class(TVampyreBasicBitmapTileLoader)
+  public
+    constructor Create;
+  end;
+
+  TVampyreBasicBitmapTileLoaderGIF = class(TVampyreBasicBitmapTileLoader)
+  public
+    constructor Create;
+  end;
+
+  TVampyreBasicBitmapTileLoaderBMP = class(TVampyreBasicBitmapTileLoader)
+  public
+    constructor Create;
+  end;
+
+implementation
+
+uses
+  SysUtils,
+  ImagingTypes,
+  ImagingGraphics32,
+  ImagingNetworkGraphics,
+  ImagingGif,
+  ImagingBitmap;
+
+{ TVampyreBasicBitmapTileLoader }
+
+constructor TVampyreBasicBitmapTileLoader.Create(AFormat: TImageFileFormat);
+begin
+  FFormat := AFormat;
+end;
+
+procedure TVampyreBasicBitmapTileLoader.LoadFromFile(AFileName: string;
+  ABtm: TCustomBitmap32);
+var
+  VFormat: TImageFileFormat;
+  VImage: TImageData;
+  IArray: TDynImageDataArray;
+  I: LongInt;
+begin
+  if FFormat <> nil then begin
+    VFormat := FFormat;
+  end else begin
+    VFormat := FindImageFileFormatByExt(DetermineFileFormat(AFileName));
+  end;
+  if VFormat = nil then begin
+    raise Exception.Create('Неизвестный формат файла');
+  end;
+  InitImage(VImage);
+  try
+    if not VFormat.LoadFromFile(AFileName, IArray, True) then begin
+      raise Exception.Create('Ошибка загрузки файла');
+    end;
+    if Length(IArray) = 0 then begin
+      raise Exception.Create('В файле не найдено изображений');
+    end;
+    VImage := IArray[0];
+    for I := 1 to Length(IArray) - 1 do begin
+      FreeImage(IArray[I]);
+    end;
+    ConvertImageDataToBitmap32(VImage, ABtm);
+  finally
+    FreeImage(VImage);
+  end;
+end;
+
+procedure TVampyreBasicBitmapTileLoader.LoadFromStream(AStream: TStream;
+  ABtm: TCustomBitmap32);
+var
+  VFormat: TImageFileFormat;
+  VImage: TImageData;
+  IArray: TDynImageDataArray;
+  I: LongInt;
+begin
+  if FFormat <> nil then begin
+    VFormat := FFormat;
+  end else begin
+    VFormat := FindImageFileFormatByExt(DetermineStreamFormat(AStream));
+  end;
+  if VFormat = nil then begin
+    raise Exception.Create('Неизвестный формат файла');
+  end;
+  InitImage(VImage);
+  try
+    if not VFormat.LoadFromStream(AStream, IArray, True) then begin
+      raise Exception.Create('Ошибка загрузки файла');
+    end;
+    if Length(IArray) = 0 then begin
+      raise Exception.Create('В файле не найдено изображений');
+    end;
+    VImage := IArray[0];
+    for I := 1 to Length(IArray) - 1 do begin
+      FreeImage(IArray[I]);
+    end;
+    ConvertImageDataToBitmap32(VImage, ABtm);
+  finally
+    FreeImage(VImage);
+  end;
+end;
+
+{ TVampyreBasicBitmapTileLoaderPNG }
+
+constructor TVampyreBasicBitmapTileLoaderPNG.Create;
+begin
+  inherited Create(FindImageFileFormatByClass(TPNGFileFormat))
+end;
+
+{ TVampyreBasicBitmapTileLoaderGIF }
+
+constructor TVampyreBasicBitmapTileLoaderGIF.Create;
+begin
+  inherited Create(FindImageFileFormatByClass(TGIFFileFormat))
+end;
+
+{ TVampyreBasicBitmapTileLoaderBMP }
+
+constructor TVampyreBasicBitmapTileLoaderBMP.Create;
+begin
+  inherited Create(FindImageFileFormatByClass(TBitmapFileFormat))
+end;
+
+end.
+
---
+++ BitmapTileSaveLoad/u_BitmapTileVampyreSaver.pas
@@ -0,0 +1,147 @@
+unit u_BitmapTileVampyreSaver;
+
+interface
+
+uses
+  Classes,
+  Imaging,
+  GR32,
+  i_BitmapTileSaveLoad;
+
+type
+  TVampyreBasicBitmapTileSaver = class(TInterfacedObject, IBitmapTileSaver)
+  private
+    FFormat: TImageFileFormat;
+  public
+    constructor Create(AFormat: TImageFileFormat);
+    destructor Destroy; override;
+    procedure SaveToFile(ABtm: TCustomBitmap32; AFileName: string);
+    procedure SaveToStream(ABtm: TCustomBitmap32; AStream: TStream);
+  end;
+
+  TVampyreBasicBitmapTileSaverPNG = class(TVampyreBasicBitmapTileSaver)
+  public
+    constructor Create;
+  end;
+
+  TVampyreBasicBitmapTileSaverGIF = class(TVampyreBasicBitmapTileSaver)
+  public
+    constructor Create;
+  end;
+
+  TVampyreBasicBitmapTileSaverBMP = class(TVampyreBasicBitmapTileSaver)
+  public
+    constructor Create;
+  end;
+
+implementation
+
+uses
+  SysUtils,
+  ImagingTypes,
+  ImagingGraphics32,
+  ImagingNetworkGraphics,
+  ImagingGif,
+  ImagingBitmap;
+
+{ TVampyreBasicBitmapTileSaver }
+
+constructor TVampyreBasicBitmapTileSaver.Create(AFormat: TImageFileFormat);
+begin
+  FFormat := AFormat;
+end;
+
+destructor TVampyreBasicBitmapTileSaver.Destroy;
+begin
+  FreeAndNil(FFormat);
+  inherited;
+end;
+
+procedure TVampyreBasicBitmapTileSaver.SaveToFile(ABtm: TCustomBitmap32;
+  AFileName: string);
+var
+  VFormat: TImageFileFormat;
+  VImage: TImageData;
+  IArray: TDynImageDataArray;
+begin
+  if FFormat <> nil then begin
+    VFormat := FFormat;
+  end else begin
+    VFormat := FindImageFileFormatByName(AFileName);
+  end;
+  if VFormat = nil then begin
+    raise Exception.Create('Неизвестный формат файла');
+  end;
+  InitImage(VImage);
+  try
+    ConvertBitmap32ToImageData(ABtm, VImage);
+    SetLength(IArray, 1);
+    IArray[0] := VImage;
+    if not VFormat.SaveToFile(AFileName, IArray, True) then begin
+      raise Exception.Create('Ошибка записи файла');
+    end;
+  finally
+    FreeImage(VImage);
+  end;
+end;
+
+procedure TVampyreBasicBitmapTileSaver.SaveToStream(ABtm: TCustomBitmap32;
+  AStream: TStream);
+var
+  VFormat: TImageFileFormat;
+  VImage: TImageData;
+  IArray: TDynImageDataArray;
+begin
+  if FFormat <> nil then begin
+    VFormat := FFormat;
+  end else begin
+    VFormat := FindImageFileFormatByExt('.bmp');
+  end;
+  if VFormat = nil then begin
+    raise Exception.Create('Неизвестный формат файла');
+  end;
+  InitImage(VImage);
+  try
+    ConvertBitmap32ToImageData(ABtm, VImage);
+    SetLength(IArray, 1);
+    IArray[0] := VImage;
+    if not VFormat.SaveToStream(AStream, IArray, True) then begin
+      raise Exception.Create('Ошибка записи файла');
+    end;
+  finally
+    FreeImage(VImage);
+  end;
+end;
+
+{ TVampyreBasicBitmapTileSaverPNG }
+
+constructor TVampyreBasicBitmapTileSaverPNG.Create;
+var
+  VFormat: TPNGFileFormat;
+begin
+  VFormat := TPNGFileFormat.Create();
+  inherited Create(VFormat);
+end;
+
+{ TVampyreBasicBitmapTileSaverGIF }
+
+constructor TVampyreBasicBitmapTileSaverGIF.Create;
+var
+  VFormat: TGIFFileFormat;
+begin
+  VFormat := TGIFFileFormat.Create();
+  inherited Create(VFormat);
+end;
+
+{ TVampyreBasicBitmapTileSaverBMP }
+
+constructor TVampyreBasicBitmapTileSaverBMP.Create;
+var
+  VFormat: TBitmapFileFormat;
+begin
+  VFormat := TBitmapFileFormat.Create();
+  inherited Create(VFormat);
+end;
+
+end.
+
