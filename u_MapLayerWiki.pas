f3c5a00100269cc2acdc291cf25a332d98c315ae
Viktor Demydov <vdemidov@gmail.com>
2010-08-17 14:06:45 +0300

    Удалил старую функцию отрисовки викимапии через канву.

--- u_MapLayerWiki.pas
+++ u_MapLayerWiki.pas
@@ -31,7 +31,6 @@ type
     FFixedPointArray: TArrayOfFixedPoint;
     FWikiLayerElments: array of TWikiLayerElement;
     procedure addWL(var AData: TKMLData);
-    procedure DrawWikiElement(var AData: TWikiLayerElement);
     procedure DrawWikiElementGR32(var AData: TWikiLayerElement);
     procedure DoRedraw; override;
     procedure Clear;
@@ -242,24 +241,6 @@ begin
     end;
   end;
   DrawWikiElementGR32(FWikiLayerElments[lenLay - 1]);
-end;
-
-procedure TWikiLayer.DrawWikiElement(var AData: TWikiLayerElement);
-begin
-  FLayer.Bitmap.Canvas.Pen.Width := 3;
-  FLayer.Bitmap.Canvas.Pen.Color := GState.WikiMapFonColor;
-  if length(AData.FPolygonOnBitmap) = 1 then begin
-    FLayer.Bitmap.Canvas.Ellipse(AData.FPolygonOnBitmap[0].x - 3, AData.FPolygonOnBitmap[0].y - 3, AData.FPolygonOnBitmap[0].x + 3, AData.FPolygonOnBitmap[0].y + 3);
-  end else begin
-    FLayer.Bitmap.Canvas.Polyline(AData.FPolygonOnBitmap);
-  end;
-  FLayer.Bitmap.Canvas.Pen.Width := 1;
-  FLayer.Bitmap.Canvas.Pen.Color := GState.WikiMapMainColor;
-  if length(AData.FPolygonOnBitmap) = 1 then begin
-    FLayer.Bitmap.Canvas.Ellipse(AData.FPolygonOnBitmap[0].x - 3, AData.FPolygonOnBitmap[0].y - 3, AData.FPolygonOnBitmap[0].x + 2, AData.FPolygonOnBitmap[0].y + 3);
-  end else begin
-    FLayer.Bitmap.Canvas.Polyline(AData.FPolygonOnBitmap);
-  end;
 end;
 
 procedure TWikiLayer.DrawWikiElementGR32(var AData: TWikiLayerElement);
