12313035007bf9280c631f653e3005bcdc5fabc2
Viktor Demydov <vdemidov@gmail.com>
2010-08-21 13:45:12 +0300

    Перевел отрисовку меток в новом провайдере отрендеренных меток на новый итератор.

--- u_MapMarksBitmapLayerProviderStuped.pas
+++ u_MapMarksBitmapLayerProviderStuped.pas
@@ -31,6 +31,7 @@ uses
   t_CommonTypes,
   u_GlobalState,
   Ugeofun,
+  u_MarksSimple,
   u_MarksReadWriteSimple,
   Unit1;
 
@@ -168,88 +169,75 @@ var
   VPointCount: Integer;
   VMarkName: string;
   VCategoryFilter: string;
+  VMarksIterator: TMarksIteratorVisibleInRectIgnoreEdit;
+  VMark: TMarkFull;
 begin
-  if (GState.show_point = mshNone)or(FMain.CDSmarks.State <> dsBrowse) then exit;
+  if (GState.show_point = mshNone) then exit;
   try
     LLRect := FGeoConvert.PixelRect2LonLatRect(FTargetRect, FZoom);
-    marksFilter:='';
-    if GState.show_point = mshChecked then begin
-      marksFilter:=marksFilter+'visible=1';
-      marksFilter:=marksFilter+' and ';
-      VCategoryFilter := GetMarksFileterByCategories(FZoom);
-      if Length(VCategoryFilter) > 0 then begin
-        marksFilter:=marksFilter + VCategoryFilter + ' and ';
-      end;
-    end;
+
     dLL:=ExtPoint((LLRect.Right-LLRect.Left)/2,(LLRect.Top-LLRect.Bottom)/2);
-    marksFilter:=marksFilter+'('+
-      ' LonR>'+floattostr(LLRect.Left-dLL.x)+' and'+
-      ' LonL<'+floattostr(LLRect.Right+dLL.x)+' and'+
-      ' LatB<'+floattostr(LLRect.Top+dLL.y)+' and'+
-      ' LatT>'+floattostr(LLRect.Bottom-dLL.y)+
-    ')';
-    FMain.CDSmarks.Filter:=marksFilter;
-    FMain.CDSmarks.Filtered:=true;
-    FMain.CDSmarks.First;
-    if FMain.CDSmarks.Eof then begin
-      FMain.CDSmarks.Filtered:=false;
-      exit;
-    end;
-//TODO: Сделать вывод подписей для меток.
-//    VBtmEx := TBitmap32.Create;
-//    VBtmEx.Font.Name:='Tahoma';
-//    VBtmEx.Font.Style:=[];
-//    VBtmEx.DrawMode := dmBlend;
-    btm:=TCustomBitmap32.Create;
+    LLRect.Left := LLRect.Left - dLL.X;
+    LLRect.Top := LLRect.Top - dLL.Y;
+    LLRect.Right := LLRect.Right + dLL.X;
+    LLRect.Bottom := LLRect.Bottom + dLL.Y;
+
+    VMarksIterator := TMarksIteratorVisibleInRectIgnoreEdit.Create(FZoom, LLRect);
     try
-      btm.DrawMode:=dmBlend;
-      btm.Resampler:=TLinearResampler.Create;
-      While not(FMain.CDSmarks.Eof) do begin
-        VScale1 := FMain.CDSmarksScale1.AsInteger;
-        VColor1 := TColor32(Fmain.CDSmarksColor1.AsInteger);
-        VColor2 := TColor32(Fmain.CDSmarksColor2.AsInteger);
-        VMarkName := FMain.CDSmarksname.AsString;
-        buf_line_arr := Blob2ExtArr(FMain.CDSmarks.FieldByName('lonlatarr'));
-        VPointCount := length(buf_line_arr);
-        if VPointCount>1 then begin
-          TestArrLenLonLatRect.Left := FMain.CDSmarksLonL.AsFloat;
-          TestArrLenLonLatRect.Top := FMain.CDSmarksLatT.AsFloat;
-          TestArrLenLonLatRect.Right := FMain.CDSmarksLonR.AsFloat;
-          TestArrLenLonLatRect.Bottom := FMain.CDSmarksLatB.AsFloat;
-          FGeoConvert.CheckLonLatRect(TestArrLenLonLatRect);
-          TestArrLenPixelRect := FGeoConvert.LonLatRect2PixelRect(TestArrLenLonLatRect, FZoom);
-          if (abs(TestArrLenPixelRect.Left-TestArrLenPixelRect.Right)>VScale1+2)or(abs(TestArrLenPixelRect.Top-TestArrLenPixelRect.Bottom)>VScale1+2) then begin
-            drawPath2Bitmap(buf_line_arr,VColor1,VColor2,VScale1,
-              (buf_line_arr[0].x=buf_line_arr[VPointCount-1].x)and(buf_line_arr[0].y=buf_line_arr[VPointCount-1].y));
-            SetLength(buf_line_arr,0);
-          end;
-        end else if VPointCount =1 then begin
-          xy:=FGeoConvert.LonLat2PixelPos(buf_line_arr[0],FZoom);
-          xy := MapPixel2BitmapPixel(xy);
-          imw:=FMain.CDSmarks.FieldByName('Scale2').AsInteger;
-          indexmi:=GState.MarkIcons.IndexOf(FMain.CDSmarks.FieldByName('picname').AsString);
-          if(indexmi=-1)and(GState.MarkIcons.Count>0) then begin
-            indexmi:=0;
-          end;
-          if(indexmi>-1)then begin
-            VIconSource := TCustomBitmap32(GState.MarkIcons.Objects[indexmi]);
-            btm.SetSize(VIconSource.Width, VIconSource.Height);
-            btm.Draw(0, 0, VIconSource);
-            FTargetBmp.Draw(bounds(xy.x-(imw div 2),xy.y-imw,imw,imw),bounds(0,0,btm.Width,btm.Height), btm);
-          end;
-          if VScale1>0 then begin
-//TODO: Сделать вывод подписей для меток.
-//            VBtmEx.Font.Size:=VScale1;
-//            texth:=VBtmEx.TextHeight(VMarkName) div 2;
-//            VBtmEx.RenderText(xy.x+(imw div 2)+2,xy.y-(imw div 2)-texth+1,VMarkName,1,VColor2);
-//            VBtmEx.RenderText(xy.x+(imw div 2)+1,xy.y-(imw div 2)-texth,VMarkName,1,VColor1);
+  //TODO: Сделать вывод подписей для меток.
+  //    VBtmEx := TBitmap32.Create;
+  //    VBtmEx.Font.Name:='Tahoma';
+  //    VBtmEx.Font.Style:=[];
+  //    VBtmEx.DrawMode := dmBlend;
+      btm:=TCustomBitmap32.Create;
+      try
+        btm.DrawMode:=dmBlend;
+        btm.Resampler:=TLinearResampler.Create;
+        While VMarksIterator.Next do begin
+          VMark := VMarksIterator.Current;
+          VScale1 := VMark.Scale1;
+          VColor1 := VMark.Color1;
+          VColor2 := VMark.Color2;
+          VMarkName := VMark.name;
+          buf_line_arr := VMark.Points;
+          VPointCount := length(buf_line_arr);
+          if VPointCount>1 then begin
+            TestArrLenLonLatRect := VMark.LLRect;
+            FGeoConvert.CheckLonLatRect(TestArrLenLonLatRect);
+            TestArrLenPixelRect := FGeoConvert.LonLatRect2PixelRect(TestArrLenLonLatRect, FZoom);
+            if (abs(TestArrLenPixelRect.Left-TestArrLenPixelRect.Right)>VScale1+2)or(abs(TestArrLenPixelRect.Top-TestArrLenPixelRect.Bottom)>VScale1+2) then begin
+              drawPath2Bitmap(buf_line_arr,VColor1,VColor2,VScale1,
+                (buf_line_arr[0].x=buf_line_arr[VPointCount-1].x)and(buf_line_arr[0].y=buf_line_arr[VPointCount-1].y));
+              SetLength(buf_line_arr,0);
+            end;
+          end else if VPointCount =1 then begin
+            xy:=FGeoConvert.LonLat2PixelPos(buf_line_arr[0],FZoom);
+            xy := MapPixel2BitmapPixel(xy);
+            imw:=VMark.Scale2;
+            indexmi:=GState.MarkIcons.IndexOf(VMark.PicName);
+            if(indexmi=-1)and(GState.MarkIcons.Count>0) then begin
+              indexmi:=0;
+            end;
+            if(indexmi>-1)then begin
+              VIconSource := TCustomBitmap32(GState.MarkIcons.Objects[indexmi]);
+              btm.SetSize(VIconSource.Width, VIconSource.Height);
+              btm.Draw(0, 0, VIconSource);
+              FTargetBmp.Draw(bounds(xy.x-(imw div 2),xy.y-imw,imw,imw),bounds(0,0,btm.Width,btm.Height), btm);
+            end;
+            if VScale1>0 then begin
+  //TODO: Сделать вывод подписей для меток.
+  //            VBtmEx.Font.Size:=VScale1;
+  //            texth:=VBtmEx.TextHeight(VMarkName) div 2;
+  //            VBtmEx.RenderText(xy.x+(imw div 2)+2,xy.y-(imw div 2)-texth+1,VMarkName,1,VColor2);
+  //            VBtmEx.RenderText(xy.x+(imw div 2)+1,xy.y-(imw div 2)-texth,VMarkName,1,VColor1);
+            end;
           end;
         end;
-        FMain.CDSmarks.Next;
+      finally
+        btm.Free;
       end;
-      FMain.CDSmarks.Filtered:=false;
     finally
-      btm.Free;
+      VMarksIterator.Free;
     end;
   finally
   end;
