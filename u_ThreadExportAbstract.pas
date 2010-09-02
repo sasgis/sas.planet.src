03607e682778e8a2a35b9db4cf56a198cbaad54a
Viktor Demydov <vdemidov@gmail.com>
2010-09-02 21:36:30 +0300

    Теперь список зумов для экспорта хранится как массив переменной длинны

--- u_ThreadExportAbstract.pas
+++ u_ThreadExportAbstract.pas
@@ -12,12 +12,14 @@ type
   TThreadExportAbstract = class(TThreadRegionProcessAbstract)
   protected
     FZoomArr: array [0..23] of boolean;
+    FZooms: array of Byte;
     procedure ProgressFormUpdateOnProgress; virtual;
   public
     constructor Create(
       APolygon: TExtendedPointArray;
       Azoomarr: array of boolean
     );
+    destructor Destroy; override;
   end;
 
 implementation
@@ -29,11 +31,25 @@ constructor TThreadExportAbstract.Create(APolygon: TExtendedPointArray;
   Azoomarr: array of boolean);
 var
   i: Integer;
+  VZoomCount: Integer;
 begin
   inherited Create(APolygon);
+  SetLength(FZooms, 24);
+  VZoomCount := 0;
   for i := 0 to 23 do begin
     FZoomArr[i] := Azoomarr[i];
+    if Azoomarr[i] then begin
+      FZooms[VZoomCount] := i;
+      Inc(VZoomCount);
+    end;
   end;
+  SetLength(FZooms, VZoomCount);
+end;
+
+destructor TThreadExportAbstract.Destroy;
+begin
+  inherited;
+  FZooms := nil;
 end;
 
 procedure TThreadExportAbstract.ProgressFormUpdateOnProgress;
