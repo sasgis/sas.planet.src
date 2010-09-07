ba775c479948a8a766b5ea992ca536e915bbb89f
Viktor Demydov <vdemidov@gmail.com>
2010-09-07 15:37:16 +0300

    Заменил при экспотре VCLZip на KaZip

--- u_ThreadExportToZip.pas
+++ u_ThreadExportToZip.pas
@@ -7,7 +7,7 @@ uses
   Forms,
   SysUtils,
   Classes,
-  VCLZIp,
+  KAZip,
   GR32,
   i_ITileFileNameGenerator,
   UMapType,
@@ -23,10 +23,9 @@ type
     FTileNameGen: ITileFileNameGenerator;
 
     FTargetFile: string;
-    FZip: TVCLZip;
+    FZip: TKaZip;
   protected
     procedure ProcessRegion; override;
-    procedure Terminate; override;
   public
     constructor Create(
       ATargetFile: string;
@@ -43,13 +42,8 @@ implementation
 uses
   u_TileIteratorAbstract,
   u_TileIteratorStuped,
+  u_TileStorageAbstract,
   u_GeoToStr;
-
-procedure TThreadExportToZip.Terminate;
-begin
-  inherited;
-  FZip.CancelTheOperation;
-end;
 
 constructor TThreadExportToZip.Create(
   ATargetFile: string;
@@ -62,7 +56,7 @@ begin
   FTargetFile := ATargetFile;
   FTileNameGen := ATileNameGen;
   FMapType := AMapType;
-  FZip := TVCLZip.Create(nil);
+  FZip := TKaZip.Create(nil);
 end;
 
 destructor TThreadExportToZip.Destroy;
@@ -75,12 +69,14 @@ procedure TThreadExportToZip.ProcessRegion;
 var
   i: integer;
   VZoom: Byte;
-  pathfrom: string;
   VExt: string;
   VPath: string;
   VTile: TPoint;
   VTileIterators: array of TTileIteratorAbstract;
   VTileIterator: TTileIteratorAbstract;
+  VTileStorage: TTileStorageAbstract;
+  VMemStream: TMemoryStream;
+  VFileTime: TDateTime;
 begin
   inherited;
   FTilesToProcess := 0;
@@ -95,42 +91,47 @@ begin
       SAS_STR_ExportTiles + ' ' + SAS_STR_CreateArhList,
       SAS_STR_AllSaves + ' ' + inttostr(FTilesToProcess) + ' ' + SAS_STR_Files
     );
-    FZip.Recurse := False;
-    FZip.StorePaths := true;
-    FZip.PackLevel := 0; // Уровень сжатия
-    FZip.ZipName := FTargetFile;
-    FTilesProcessed := 0;
-    ProgressFormUpdateOnProgress;
-    for i := 0 to Length(FZooms) - 1 do begin
-      VZoom := FZooms[i];
-        VExt := FMapType.TileStorage.TileFileExt;
-        VPath := IncludeTrailingPathDelimiter(IncludeTrailingPathDelimiter(FTargetFile) + FMapType.GetShortFolderName);
-        VTileIterator := VTileIterators[i];
-        while VTileIterator.Next do begin
-          if IsCancel then begin
-            exit;
+    VMemStream := TMemoryStream.Create;
+    try
+      VTileStorage := FMapType.TileStorage;
+      FZip.FileName := FTargetFile;
+      FZip.CreateZip(FTargetFile);
+      FZip.CompressionType := ctFast;
+      FZip.Active := true;
+
+      FTilesProcessed := 0;
+      ProgressFormUpdateOnProgress;
+      for i := 0 to Length(FZooms) - 1 do begin
+        VZoom := FZooms[i];
+          VExt := FMapType.TileStorage.TileFileExt;
+          VPath := IncludeTrailingPathDelimiter(IncludeTrailingPathDelimiter(FTargetFile) + FMapType.GetShortFolderName);
+          VTileIterator := VTileIterators[i];
+          while VTileIterator.Next do begin
+            if IsCancel then begin
+              exit;
+            end;
+            VTile := VTileIterator.Current;
+            if VTileStorage.ExistsTile(VTile, VZoom) then begin
+              VMemStream.Position := 0;
+              if VTileStorage.LoadTile(VTile, VZoom, VMemStream) then begin
+                VFileTime := VTileStorage.TileLoadDate(VTile, VZoom);
+                VMemStream.Position := 0;
+                FZip.AddStream(
+                  FTileNameGen.GetTileFileName(VTile, VZoom)+ VTileStorage.GetTileFileExt,
+                  faArchive,
+                  VFileTime,
+                  VMemStream
+                );
+              end;
+            end;
+            inc(FTilesProcessed);
+            if FTilesProcessed mod 100 = 0 then begin
+              ProgressFormUpdateOnProgress;
+            end;
           end;
-          VTile := VTileIterator.Current;
-          if FMapType.TileExists(VTile, VZoom) then begin
-//TODO: Разобраться и избавиться от путей. Нужно предусмотреть вариант, что тайлы хранятся не в файлах, а перед зипованием сохраняются в файлы.
-            pathfrom := FMapType.GetTileFileName(VTile, VZoom);
-            FZip.FilesList.Add(pathfrom);
-          end;
-          inc(FTilesProcessed);
-          if FTilesProcessed mod 100 = 0 then begin
-            ProgressFormUpdateOnProgress;
-          end;
-        end;
-    end;
-    ProgressFormUpdateCaption(
-      SAS_STR_Pack + ' ' + FTargetFile,
-      SAS_STR_AllSaves + ' ' + inttostr(FTilesToProcess) + ' ' + SAS_STR_Files
-    );
-    if FileExists(FZip.ZipName) then begin
-      DeleteFile(FZip.ZipName);
-    end;
-    If FZip.Zip = 0 then begin
-      Application.MessageBox(PChar(SAS_ERR_CreateArh), PChar(SAS_MSG_coution), 48);
+      end;
+    finally
+      VMemStream.Free;
     end;
   finally
     for i := 0 to Length(FZooms) - 1 do begin
