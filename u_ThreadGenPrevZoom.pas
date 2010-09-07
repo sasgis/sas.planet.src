4eea7dcc7fc303ca053baf158d33dc13c8abd765
Viktor Demydov <vdemidov@gmail.com>
2010-09-07 11:41:01 +0300

    Ковыряю генерилку верхних слоев

--- u_ThreadGenPrevZoom.pas
+++ u_ThreadGenPrevZoom.pas
@@ -71,9 +71,10 @@ procedure TThreadGenPrevZoom.ProcessRegion;
 var
   bmp_ex: TCustomBitmap32;
   bmp: TCustomBitmap32;
-  i, c_d, p_x, p_y, d2562, p_i, p_j, p_x_x, p_y_y: integer;
+  i, VSubTileCount, p_x, p_y, d2562, p_i, p_j, p_x_x, p_y_y: integer;
   save_len_tile: integer;
-  VZoom: Integer;
+  VZoomPrev: Byte;
+  VZoom: Byte;
   VTile: TPoint;
   VSubTile: TPoint;
   max, min: TPoint;
@@ -95,10 +96,11 @@ begin
     for i := 0 to length(FZooms) - 1 do begin
       polyg := FMapType.GeoConvert.LonLatArray2PixelArray(FPolygLL, FZooms[i]);
       if (not FGenFormPrevZoom) or (i = 0) then begin
-        inc(FTilesToProcess, GetDwnlNum(min, max, Polyg, true) * Round(IntPower(4, FSourceZoom - FZooms[i])));
+        VZoomDelta := FSourceZoom - FZooms[i];
       end else begin
-        inc(FTilesToProcess, GetDwnlNum(min, max, Polyg, true) * Round(IntPower(4, FZooms[i - 1] - FZooms[i])));
+        VZoomDelta := FZooms[i - 1] - FZooms[i];
       end;
+      inc(FTilesToProcess, GetDwnlNum(min, max, Polyg, true) * Round(IntPower(4, VZoomDelta)));
     end;
     ProgressFormUpdateCaption(
       '',
@@ -116,17 +118,15 @@ begin
         if Terminated then begin
           continue;
         end;
-        polyg := FMapType.GeoConvert.LonLatArray2PixelArray(FPolygLL, FZooms[i]);
         if (not FGenFormPrevZoom) or (i = 0) then begin
-          c_d := round(power(2, FSourceZoom - FZooms[i]));
+          VZoomPrev := FSourceZoom;
         end else begin
-          c_d := round(power(2, FZooms[i - 1] - FZooms[i]));
+          VZoomPrev := FZooms[i - 1];
         end;
-        if (not FGenFormPrevZoom) or (i = 0) then begin
-          VZoom := FSourceZoom;
-        end else begin
-          VZoom := FZooms[i - 1];
-        end;
+        VZoom := FZooms[i];
+        VZoomDelta := VZoomPrev - VZoom;
+        polyg := FMapType.GeoConvert.LonLatArray2PixelArray(FPolygLL, VZoom);
+        VSubTileCount := 1 shl VZoomDelta;
         GetDwnlNum(min, max, Polyg, false);
         p_x := min.x;
         while (p_x < max.X) and (not IsCancel) do begin
@@ -135,34 +135,34 @@ begin
           while (p_y < max.y) and (not IsCancel) do begin
             VTile.Y := p_y shr 8;
             if RgnAndRgn(Polyg, p_x, p_y, false) then begin
-              if FMapType.TileExists(VTile, FZooms[i]) then begin
+              if FMapType.TileExists(VTile, VZoom) then begin
                 if not (FIsReplace) then begin
                   ProgressFormUpdateOnProgress;
                   inc(p_y, 256);
                   continue;
                 end;
-                FMapType.LoadTile(bmp_Ex, VTile, FZooms[i], false);
+                FMapType.LoadTile(bmp_Ex, VTile, VZoom, false);
               end else begin
                 bmp_ex.SetSize(256, 256);
                 bmp_ex.Clear(Color32(GState.BGround));
               end;
-              d2562 := 256 div c_d;
+              d2562 := 256 div VSubTileCount;
               save_len_tile := 0;
-              for p_i := 1 to c_d do begin
+              for p_i := 1 to VSubTileCount do begin
                 if Terminated then begin
                   continue;
                 end;
-                for p_j := 1 to c_d do begin
+                for p_j := 1 to VSubTileCount do begin
                   if Terminated then begin
                     continue;
                   end;
-                  p_x_x := ((p_x - 128) * c_d) + ((p_i - 1) * 256);
-                  p_y_y := ((p_y - 128) * c_d) + ((p_j - 1) * 256);
+                  p_x_x := ((p_x - 128) * VSubTileCount) + ((p_i - 1) * 256);
+                  p_y_y := ((p_y - 128) * VSubTileCount) + ((p_j - 1) * 256);
                   VSubTile := Point(p_x_x shr 8, p_y_y shr 8);
 
-                  if FMapType.TileExists(VSubTile, VZoom) then begin
-                    if (FMapType.LoadTile(bmp, VSubTile, VZoom, false)) then begin
-                      bmp_ex.Draw(bounds((p_i - 1) * d2562, (p_j - 1) * d2562, 256 div c_d, 256 div c_d), bounds(0, 0, 256, 256), bmp);
+                  if FMapType.TileExists(VSubTile, VZoomPrev) then begin
+                    if (FMapType.LoadTile(bmp, VSubTile, VZoomPrev, false)) then begin
+                      bmp_ex.Draw(bounds((p_i - 1) * d2562, (p_j - 1) * d2562, 256 div VSubTileCount, 256 div VSubTileCount), bounds(0, 0, 256, 256), bmp);
                       inc(save_len_tile);
                     end else begin
                       Assert(False, 'Ошибка чтения тайла.');
@@ -177,8 +177,8 @@ begin
               if Terminated then begin
                 continue;
               end;
-              if ((not FIsSaveFullOnly) or (save_len_tile = c_d * c_d)) and (save_len_tile > 0) then begin
-                FMapType.SaveTileSimple(VTile, FZooms[i], bmp_ex);
+              if ((not FIsSaveFullOnly) or (save_len_tile = VSubTileCount * VSubTileCount)) and (save_len_tile > 0) then begin
+                FMapType.SaveTileSimple(VTile, VZoom, bmp_ex);
                 inc(FTileInProc);
               end;
             end;
