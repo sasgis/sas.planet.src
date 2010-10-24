8404d81452db3667b561bdc34398d3aa04ae4e4d
Viktor Demydov <vdemidov@gmail.com>
2010-10-24 12:44:42 +0300

    Пилю новый класс

--- u_MapLayerBase.pas
+++ u_MapLayerBase.pas
@@ -7,6 +7,7 @@ uses
   GR32_Image,
   i_JclNotify,
   i_ICoordConverter,
+  i_IPosChangeMessage,
   u_MapViewPortState,
   u_WindowLayerBasic;
 
@@ -16,8 +17,13 @@ type
     FScreenCenterPos: TPoint;
     FZoom: Byte;
     FGeoConvert: ICoordConverter;
+    FViewSize: TPoint;
 
     FMapPosChangeListener: IJclListener;
+    FViewScaleChangeListener: IJclListener;
+    procedure ProcessPosChange(AMessage: IPosChangeMessage);
+    procedure ProcessViewScaleChange(AMessage: IJclNotificationMessage);
+    
   public
     constructor Create(AParentMap: TImage32; AViewPortState: TMapViewPortState);
     destructor Destroy; override;
@@ -26,20 +32,85 @@ type
 
 implementation
 
+uses
+  u_JclNotify;
+
+{ TListenerOfMapLayer }
+
+type
+  TListenerOfMapLayer = class(TJclBaseListener)
+  protected
+    FMapLayer: TMapLayerBase;
+  public
+    constructor Create(AMapLayer: TMapLayerBase);
+  end;
+
+constructor TListenerOfMapLayer.Create(AMapLayer: TMapLayerBase);
+begin
+  FMapLayer := AMapLayer;
+end;
+
+{ TChangePosListener }
+
+type
+  TChangePosListener = class(TListenerOfMapLayer)
+  protected
+    procedure Notification(msg: IJclNotificationMessage); override;
+  end;
+
+procedure TChangePosListener.Notification(
+  msg: IJclNotificationMessage);
+var
+  VMessage: IPosChangeMessage;
+begin
+  VMessage := msg as IPosChangeMessage;
+  FMapLayer.ProcessPosChange(VMessage);
+end;
+
+{ TChangeViewScaleListener }
+
+type
+  TChangeViewScaleListener = class(TListenerOfMapLayer)
+  protected
+    procedure Notification(msg: IJclNotificationMessage); override;
+  end;
+
+procedure TChangeViewScaleListener.Notification(
+  msg: IJclNotificationMessage);
+begin
+  FMapLayer.ProcessViewScaleChange(msg);
+end;
+
 { TMapLayerBase }
 
 constructor TMapLayerBase.Create(AParentMap: TImage32;
   AViewPortState: TMapViewPortState);
 begin
   inherited;
-//  FMapPosChangeListener :=
+  FMapPosChangeListener := TChangePosListener.Create(Self);
+  FViewPortState.PosChangeNotifier.Add(FMapPosChangeListener);
+  FViewScaleChangeListener := TChangeViewScaleListener.Create(Self);
+  FViewPortState.ScaleChangeNotifier.Add(FViewScaleChangeListener);
 end;
 
 destructor TMapLayerBase.Destroy;
 begin
   FViewPortState.PosChangeNotifier.Remove(FMapPosChangeListener);
   FMapPosChangeListener := nil;
+  FViewPortState.PosChangeNotifier.Remove(FViewScaleChangeListener);
+  FViewScaleChangeListener := nil;
   inherited;
+end;
+
+procedure TMapLayerBase.ProcessPosChange(AMessage: IPosChangeMessage);
+begin
+
+end;
+
+procedure TMapLayerBase.ProcessViewScaleChange(
+  AMessage: IJclNotificationMessage);
+begin
+
 end;
 
 end.
