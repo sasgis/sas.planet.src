b585e682b0dbc59aac44951c2b359e4869586ac5
Viktor Demydov <vdemidov@gmail.com>
2010-12-06 16:53:13 +0200

    Ковыряю новые интерфейсы

--- i_IActiveMapsConfig.pas
+++ i_IActiveMapsConfig.pas
@@ -32,39 +32,46 @@ type
     property HybrChangeNotifier: IJclNotifier read GetHybrChangeNotifier;
   end;
 
-  IActiveMap = interface(IConfigDataElement)
-    ['{6BAD8743-D50B-4342-9A68-DA5FBDDFDB04}']
-    function GetSelectedGUID: TGUID;
-    function GetMapsList: IMapTypeList;
-
-    property SelectedMapGUID: TGUID read GetSelectedGUID;
-    property MapsList: IMapTypeList read GetMapsList;
-  end;
-
   IActiveMapSingle = interface(IConfigDataElement)
     ['{12F47503-E574-4F4F-A30C-7304D38410C7}']
     function GetMapType: IMapType;
     function GetIsActive: Boolean;
   end;
 
+  IActiveMap = interface(IConfigDataElement)
+    ['{6BAD8743-D50B-4342-9A68-DA5FBDDFDB04}']
+    function GetSelectedGUID: TGUID;
+    function GetMapSingle(AMapGUID: TGUID): IActiveMapSingle;
+    function GetMapsList: IMapTypeList;
+  end;
+
   IActiveMapsSet = interface(IConfigDataElement)
     ['{09F8FEE4-984C-4D1F-A240-BD8FF3333F85}']
     function IsGUIDSelected(AMapGUID: TGUID): Boolean;
+    function GetMapSingle(AMapGUID: TGUID): IActiveMapSingle;
     function GetSelectedMapsList: IMapTypeList;
     function GetMapsList: IMapTypeList;
-
-    property MapsList: IMapTypeList read GetMapsList;
   end;
 
   IActivMapWithLayers = interface(IConfigDataElement)
     ['{92B95280-7FD6-402A-8260-3FD83ED6BE36}']
-    function GetActiveMap: IActiveMap;
     procedure SelectMainByGUID(AMapGUID: TGUID);
 
     procedure SelectLayerByGUID(AMapGUID: TGUID);
     procedure UnSelectLayerByGUID(AMapGUID: TGUID);
+
+    function GetActiveMap: IActiveMap;
+    function GetLayers: IActiveMapsSet;
+    function GetAllActiveMapsSet: IActiveMapsSet;
+    function GetMapSingle(AMapGUID: TGUID): IActiveMapSingle;
   end;
 
+  IMainMapsConfig = interface(IActivMapWithLayers)
+    ['{8A8A42A5-9252-4E85-812C-6A5EEEF98443}']
+    function GetBitmapLayersSet: IActiveMapsSet;
+    function GetKmlLayersSet: IActiveMapsSet;
+  end;
+  
 const
   CGUID_Zero: TGUID = '{00000000-0000-0000-0000-000000000000}';
 
