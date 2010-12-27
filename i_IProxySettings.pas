a41755c277bbbe666787ccc881d178ec43562db8
Viktor Demydov <vdemidov@gmail.com>
2010-12-27 23:35:02 +0200

    Пара мелких правок новых интерфейсов

--- i_IProxySettings.pas
+++ i_IProxySettings.pas
@@ -22,22 +22,30 @@ type
     property Password: WideString read GetPassword;
   end;
 
-  IProxySettingsElement = interface(IConfigDataElement)
+  IProxyConfig = interface(IConfigDataElement)
   ['{0CE5A97E-471D-4A3E-93E3-D130DD1F50F5}']
-    function GetUseProxy(): boolean; safecall;
+    function GetUseIESettings: Boolean; safecall;
+    function GetUseProxy(): Boolean; safecall;
     function GetHost(): WideString; safecall;
     function GetUseLogin(): boolean; safecall;
     function GetLogin(): WideString; safecall;
     function GetPassword(): WideString; safecall;
+
+    procedure SetUseIESettings(AValue: Boolean);
+    procedure SetUseProxy(AValue: Boolean);
+    procedure SetHost(AValue: WideString);
+    procedure SetUseLogin(AValue: Boolean);
+    procedure SetLogin(AValue: WideString);
+    procedure SetPassword(AValue: WideString);
   end;
 
-  IInetSettings = interface(IConfigDataElement)
+  IInetConfig = interface(IConfigDataElement)
     ['{D025A3CE-2CC7-4DB3-BBF6-53DF14A2A2E7}']
-    function GetProxySettings: IProxySettings; safecall;
-    function GetTimeOut: cardinal; safecall;
+    function GetProxyConfig: IProxyConfig; safecall;
+    function GetTimeOut: Cardinal; safecall;
+    procedure SetTimeOut(AValue: Cardinal); safecall;
 
-    property ProxySettings: IProxySettings read GetProxySettings;
-    property TimeOut: cardinal read GetTimeOut;
+    property ProxyConfig: IProxyConfig read GetProxyConfig;
   end;
 
 implementation
