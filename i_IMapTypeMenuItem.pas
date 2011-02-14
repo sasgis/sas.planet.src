4d9fb57b3460b8434965d81a2ea7e209281c8c38
Viktor Demydov <vdemidov@gmail.com>
2011-02-14 09:53:28 +0200

    Вычистил пару кусков закоментированного кода

--- i_IMapTypeMenuItem.pas
+++ i_IMapTypeMenuItem.pas
@@ -1,19 +1,5 @@
 unit i_IMapTypeMenuItem;
-
 interface
-
-uses
-  TB2Item,
-  i_IActiveMapsConfig,
-  i_MapTypes;
-
-//type
-//  IMapTypeMenuItemFactory = interface
-//    ['{74E9D8BD-91D2-4A25-94D1-C707D068A7EC}']
-//    function CreateItem(AMapActive: IActiveMapSingle): TTBCustomItem;
-//  end;
-
 implementation
-
 end.
  
