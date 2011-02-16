9de07a7bc44884fd3cbc64f3f63fa983123859e6
Viktor Demydov <vdemidov@gmail.com>
2011-02-16 12:38:52 +0200

    Поделил пару старых интерфейсов

--- i_IActiveMapsConfig.pas
+++ i_IActiveMapsConfig.pas
@@ -8,30 +8,6 @@ uses
   i_IConfigDataElement;
 
 type
-  IActiveMapConfig = interface
-    ['{D33FE9FA-B243-4783-9D55-F15B813BADF9}']
-    procedure SelectMapByGUID(AMapGUID: TGUID);
-    function GetSelectedMapGUID: TGUID;
-    function GetMapsList: IMapTypeList;
-    function GetMapChangeNotifier: IJclNotifier;
-
-    property SelectedMapGUID: TGUID read GetSelectedMapGUID;
-    property MapsList: IMapTypeList read GetMapsList;
-    property MapChangeNotifier: IJclNotifier read GetMapChangeNotifier;
-  end;
-
-  IActiveMapWithHybrConfig = interface(IActiveMapConfig)
-    ['{74D5CD4A-75DC-4926-B9D9-5AA7FD13CA2C}']
-    procedure SelectHybrByGUID(AMapGUID: TGUID);
-    procedure UnSelectHybrByGUID(AMapGUID: TGUID);
-    function IsHybrGUIDSelected(AMapGUID: TGUID): Boolean;
-    function GetHybrList: IMapTypeList;
-    function GetHybrChangeNotifier: IJclNotifier;
-
-    property HybrList: IMapTypeList read GetHybrList;
-    property HybrChangeNotifier: IJclNotifier read GetHybrChangeNotifier;
-  end;
-
   IActiveMapSingle = interface(IConfigDataElement)
     ['{12F47503-E574-4F4F-A30C-7304D38410C7}']
     function GetMapType: IMapType;
