36df26a2055ce628f0b97e5a9652fc30b4ac04e8
Viktor Demydov <vdemidov@gmail.com>
2011-03-12 12:12:34 +0200

    Мелкая правка пока неиспользуемого кода.

--- i_Marks.pas
+++ i_Marks.pas
@@ -91,15 +91,18 @@ type
   IMarkCategory = interface
   ['{00226B68-9915-41AA-90B7-3F2348E53527}']
     function GetId: integer; stdcall;
-    function GetName: string; stdcall;
-    function GetVisible: boolean; stdcall;
-    function GetAfterScale: integer; stdcall;
-    function GetBeforeScale: integer; stdcall;
-
     property Id: integer read GetId;
+
+    function GetName: string; stdcall;
     property Name: string read GetName;
+
+    function GetVisible: boolean; stdcall;
     property Visible: boolean read GetVisible;
+
+    function GetAfterScale: integer; stdcall;
     property AfterScale: integer read GetAfterScale;
+
+    function GetBeforeScale: integer; stdcall;
     property BeforeScale: integer read GetBeforeScale;
   end;
 
