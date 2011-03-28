ccf5ba12ad9b20efd769089ad92f33b4a816b3d8
Viktor Demydov <vdemidov@gmail.com>
2011-03-28 22:04:06 +0300

    Некоторые мысли на тему унификации датчиков.

--- i_Sensor.pas
+++ i_Sensor.pas
@@ -3,6 +3,7 @@ unit i_Sensor;
 interface
 
 uses
+  GR32,
   i_ConfigDataElement,
   i_ConfigDataProvider,
   i_ConfigDataWriteProvider;
@@ -15,9 +16,10 @@ type
     function GetDescription: string;
     function CanReset: Boolean;
     function GetMenuItemName: string;
+    function GetSensorViewIID: TGUID;
   end;
 
-  ISensorConfig = interface(IConfigDataElement)
+  ISensorViewConfig = interface(IConfigDataElement)
     ['{ABA124E3-376F-495E-982C-F3D27F48F610}']
     function GetVisible: Boolean;
     procedure SetVisible(AValue: Boolean);
@@ -28,14 +30,25 @@ type
     property FontSize: Integer read GetFontSize write SetFontSize;
   end;
 
+  ISensorView = interface
+    ['{3D7823AF-17D9-495E-901C-BF6435E5C0E1}']
+    function GetConfig: ISensorViewConfig;
+  end;
+
+  ISensorViewText = interface(ISensorView)
+    ['{8345C7D4-BEAD-4242-9ACC-81FBEB571ADE}']
+    procedure SetText(AValue: string);
+  end;
+
+  ISensorViewBitmap = interface(ISensorView)
+    ['{B92A9CE8-82D0-4045-B931-4E85FA77070C}']
+    procedure SetBitmap(AValue: TCustomBitmap32);
+  end;
+
   ISensor = interface
     ['{F106BDDC-E596-47EE-99FC-C9A61C7868F4}']
-    procedure Show;
-    procedure Hide;
-    procedure Reset;
-    function GetVisible: Boolean;
-    procedure LoadConfig(AConfigProvider: IConfigDataProvider);
-    procedure SaveConfig(AConfigProvider: IConfigDataWriteProvider);
+    function GetInfo: ISensorInfo;
+    procedure SetView(AView: ISensorView);
   end;
 
 implementation
