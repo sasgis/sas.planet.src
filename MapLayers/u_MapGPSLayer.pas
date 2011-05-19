50a2e12e5680559d8ff5a18ca9e2fb933bc344bc
Viktor Demydov <vdemidov@gmail.com>
2011-05-19 13:56:45 +0300

    Переделываю слой GPS трека

--- MapLayers/u_MapGPSLayer.pas
+++ MapLayers/u_MapGPSLayer.pas
@@ -5,10 +5,12 @@ interface
 uses
   Types,
   GR32,
+  GR32_Polygons,
   GR32_Image,
   i_JclNotify,
   t_GeoTypes,
   t_CommonTypes,
+  i_LocalCoordConverter,
   i_GPSRecorder,
   i_MapLayerGPSTrackConfig,
   i_ViewPortState,
@@ -19,7 +21,17 @@ type
   private
     FConfig: IMapLayerGPSTrackConfig;
     FGPSRecorder: IGPSRecorder;
+    FPoints: TGPSTrackPointArray;
+    FPolygon: TPolygon32;
     procedure OnConfigChange(Sender: TObject);
+    procedure DrawPath(
+      AIsStop: TIsCancelChecker;
+      ALocalConverter: ILocalCoordConverter;
+      ATrackColorer: ITrackColorerStatic;
+      ALineWidth: Double;
+      APointsCount: Integer
+    );
+
   protected
     procedure DrawBitmap(AIsStop: TIsCancelChecker); override;
   public
@@ -32,6 +44,7 @@ type
       AConfig: IMapLayerGPSTrackConfig;
       AGPSRecorder: IGPSRecorder
     );
+    destructor Destroy; override;
   end;
 
 implementation
@@ -40,9 +53,7 @@ uses
   Classes,
   Graphics,
   SysUtils,
-  GR32_Polygons,
   u_GeoFun,
-  i_LocalCoordConverter,
   u_NotifyEventListener;
 
 { TMapGPSLayer }
@@ -62,6 +73,16 @@ begin
     TNotifyEventListener.Create(Self.OnConfigChange),
     FConfig.GetChangeNotifier
   );
+  FPolygon := TPolygon32.Create;
+  FPolygon.Antialiased := true;
+  FPolygon.AntialiasMode := am4times;
+  FPolygon.Closed := false;
+end;
+
+destructor TMapGPSLayer.Destroy;
+begin
+  FreeAndNil(FPolygon);
+  inherited;
 end;
 
 procedure TMapGPSLayer.DrawBitmap(AIsStop: TIsCancelChecker);
@@ -70,18 +91,6 @@ var
   VPointsCount: Integer;
   VLineWidth: Double;
   VLocalConverter: ILocalCoordConverter;
-  VPoints: TGPSTrackPointArray;
-  VPolygon: TPolygon32;
-  VMapPointPrev: TDoublePoint;
-  VPointPrevIsEmpty: Boolean;
-  VPointPrev: TDoublePoint;
-  i: Integer;
-  VMapPointCurr: TDoublePoint;
-  VPointCurrIsEmpty: Boolean;
-  VPointCurr: TDoublePoint;
-  VFixedPointsPair: array [0..1] of TFixedPoint;
-  VSegmentColor: TColor32;
-  VIsChangePrevPoint: Boolean;
 begin
   inherited;
   FConfig.LockRead;
@@ -95,73 +104,84 @@ begin
 
   if (VPointsCount > 1) then begin
     VLocalConverter := LayerCoordConverter;
-    VPoints := FGPSRecorder.LastPoints(VPointsCount);
-    VPointsCount := length(VPoints);
+    FPoints := FGPSRecorder.LastPoints(VPointsCount);
+    VPointsCount := length(FPoints);
     if (VPointsCount > 1) then begin
-      VPolygon := TPolygon32.Create;
-      try
-        VPolygon.Antialiased := true;
-        VPolygon.AntialiasMode := am4times;
-        VPolygon.Closed := false;
-        VMapPointPrev := VPoints[VPointsCount - 1].Point;
-        VPointPrevIsEmpty := PointIsEmpty(VMapPointPrev);
-        VPointPrev := VLocalConverter.LonLat2LocalPixelFloat(VMapPointPrev);
-        for i := VPointsCount - 2 downto 0 do begin
-          VMapPointCurr := VPoints[i].Point;
-          VPointCurrIsEmpty := PointIsEmpty(VMapPointCurr);
-          if not VPointCurrIsEmpty then begin
-            VPointCurr := VLocalConverter.LonLat2LocalPixelFloat(VMapPointCurr);
-            if not VPointPrevIsEmpty then begin
-              if (abs(VPointPrev.X - VPointCurr.X) > 1) or (Abs(VPointPrev.Y - VPointCurr.Y) > 1) then begin
-                if (VPointPrev.x < 32767) and (VPointPrev.x > -32767) and (VPointPrev.y < 32767) and (VPointPrev.y > -32767) then begin
-                  VFixedPointsPair[0] := FixedPoint(VPointPrev.X, VPointPrev.Y);
-                  VFixedPointsPair[1] := FixedPoint(VPointCurr.X, VPointCurr.Y);
-                  VPolygon.AddPoints(VFixedPointsPair[0], 2);
-                  with VPolygon.Outline do try
-                    with Grow(Fixed(VLineWidth / 2), 0.5) do try
-                      VSegmentColor := VTrackColorer.GetColorForSpeed(VPoints[i].Speed);
-                      Layer.Bitmap.Lock;
-                      try
-                        if not AIsStop then begin
-                          DrawFill(Layer.Bitmap, VSegmentColor);
-                        end;
-                      finally
-                        Layer.Bitmap.Unlock;
-                      end;
-                    finally
-                      free;
-                    end;
-                  finally
-                    free;
-                  end;
-                  VPolygon.Clear;
-                end;
-                VIsChangePrevPoint := True;
-              end else begin
-                VIsChangePrevPoint := False;
-              end;
-            end else begin
-              VIsChangePrevPoint := True;
-            end;
-          end else begin
-            VIsChangePrevPoint := True;
-          end;
-          if VIsChangePrevPoint then begin
-            VMapPointPrev := VMapPointCurr;
-            VPointPrev := VPointCurr;
-            VPointPrevIsEmpty := VPointCurrIsEmpty;
-          end;
-          if AIsStop then begin
-            Break;
-          end;
-        end;
-        if not AIsStop then begin
-          SetBitmapChanged;
-        end;
-      finally
-        VPolygon.Free;
-      end;
+      DrawPath(AIsStop, VLocalConverter, VTrackColorer, VLineWidth, VPointsCount);
     end;
+  end;
+end;
+
+procedure TMapGPSLayer.DrawPath(AIsStop: TIsCancelChecker;
+  ALocalConverter: ILocalCoordConverter; ATrackColorer: ITrackColorerStatic;
+  ALineWidth: Double; APointsCount: Integer);
+var
+  VMapPointPrev: TDoublePoint;
+  VPointPrevIsEmpty: Boolean;
+  VPointPrev: TDoublePoint;
+  i: Integer;
+  VMapPointCurr: TDoublePoint;
+  VPointCurrIsEmpty: Boolean;
+  VPointCurr: TDoublePoint;
+  VFixedPointsPair: array [0..1] of TFixedPoint;
+  VSegmentColor: TColor32;
+  VIsChangePrevPoint: Boolean;
+begin
+  VMapPointPrev := FPoints[APointsCount - 1].Point;
+  VPointPrevIsEmpty := PointIsEmpty(VMapPointPrev);
+  VPointPrev := ALocalConverter.LonLat2LocalPixelFloat(VMapPointPrev);
+  for i := APointsCount - 2 downto 0 do begin
+    VMapPointCurr := FPoints[i].Point;
+    VPointCurrIsEmpty := PointIsEmpty(VMapPointCurr);
+    if not VPointCurrIsEmpty then begin
+      VPointCurr := ALocalConverter.LonLat2LocalPixelFloat(VMapPointCurr);
+      if not VPointPrevIsEmpty then begin
+        if (abs(VPointPrev.X - VPointCurr.X) > 1) or (Abs(VPointPrev.Y - VPointCurr.Y) > 1) then begin
+          if (VPointPrev.x < 32767) and (VPointPrev.x > -32767) and (VPointPrev.y < 32767) and (VPointPrev.y > -32767) then begin
+            VFixedPointsPair[0] := FixedPoint(VPointPrev.X, VPointPrev.Y);
+            VFixedPointsPair[1] := FixedPoint(VPointCurr.X, VPointCurr.Y);
+            FPolygon.Clear;
+            FPolygon.AddPoints(VFixedPointsPair[0], 2);
+            with FPolygon.Outline do try
+              with Grow(Fixed(ALineWidth / 2), 0.5) do try
+                VSegmentColor := ATrackColorer.GetColorForSpeed(FPoints[i].Speed);
+                Layer.Bitmap.Lock;
+                try
+                  if not AIsStop then begin
+                    DrawFill(Layer.Bitmap, VSegmentColor);
+                  end;
+                finally
+                  Layer.Bitmap.Unlock;
+                end;
+              finally
+                free;
+              end;
+            finally
+              free;
+            end;
+            FPolygon.Clear;
+          end;
+          VIsChangePrevPoint := True;
+        end else begin
+          VIsChangePrevPoint := False;
+        end;
+      end else begin
+        VIsChangePrevPoint := True;
+      end;
+    end else begin
+      VIsChangePrevPoint := True;
+    end;
+    if VIsChangePrevPoint then begin
+      VMapPointPrev := VMapPointCurr;
+      VPointPrev := VPointCurr;
+      VPointPrevIsEmpty := VPointCurrIsEmpty;
+    end;
+    if AIsStop then begin
+      Break;
+    end;
+  end;
+  if not AIsStop then begin
+    SetBitmapChanged;
   end;
 end;
 
