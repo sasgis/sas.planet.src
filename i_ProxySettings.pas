75a592dafda2c2c6a700e622d3774db969819e18
Viktor Demydov <vdemidov@gmail.com>
2011-06-03 15:12:56 +0300

    Мелкое навдение красоты

--- i_ProxySettings.pas
+++ i_ProxySettings.pas
@@ -10,33 +10,46 @@ type
   IProxySettings = interface(IInterface)
     ['{17F31D40-FCA0-4525-9820-A14BB61AA08A}']
     function GetUseProxy(): boolean; safecall;
-    function GetHost(): WideString; safecall;
-    function GetUseLogin(): boolean; safecall;
-    function GetLogin(): WideString; safecall;
-    function GetPassword(): WideString; safecall;
-
     property UseProxy: boolean read GetUseProxy;
+
+    function GetHost(): WideString; safecall;
     property Host: WideString read GetHost;
+
+    function GetUseLogin(): boolean; safecall;
     property UseLogin: boolean read GetUseLogin;
+
+    function GetLogin(): WideString; safecall;
     property Login: WideString read GetLogin;
+
+    function GetPassword(): WideString; safecall;
     property Password: WideString read GetPassword;
   end;
 
   IProxyConfig = interface(IConfigDataElement)
   ['{0CE5A97E-471D-4A3E-93E3-D130DD1F50F5}']
     function GetUseIESettings: Boolean; safecall;
-    function GetUseProxy(): Boolean; safecall;
-    function GetHost(): WideString; safecall;
-    function GetUseLogin(): boolean; safecall;
-    function GetLogin(): WideString; safecall;
-    function GetPassword(): WideString; safecall;
-
     procedure SetUseIESettings(AValue: Boolean);
+    property UseIESettings: Boolean read GetUseIESettings write SetUseIESettings;
+
+    function GetUseProxy(): Boolean; safecall;
     procedure SetUseProxy(AValue: Boolean);
+    property UseProxy: boolean read GetUseProxy write SetUseProxy;
+
+    function GetHost(): WideString; safecall;
     procedure SetHost(AValue: WideString);
+    property Host: WideString read GetHost write SetHost;
+
+    function GetUseLogin(): boolean; safecall;
     procedure SetUseLogin(AValue: Boolean);
+    property UseLogin: boolean read GetUseLogin write SetUseLogin;
+
+    function GetLogin(): WideString; safecall;
     procedure SetLogin(AValue: WideString);
+    property Login: WideString read GetLogin write SetLogin;
+
+    function GetPassword(): WideString; safecall;
     procedure SetPassword(AValue: WideString);
+    property Password: WideString read GetPassword write SetPassword;
   end;
 
   IInetConfig = interface(IConfigDataElement)
