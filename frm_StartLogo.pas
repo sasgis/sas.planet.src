b46f568bdfc1859c3cc96024ed766a35a17b67ed
Viktor Demydov <vdemidov@gmail.com>
2011-06-14 17:05:25 +0300

    Мелкая правка лого, для случая, если в ресурсах нет картинки.

--- frm_StartLogo.pas
+++ frm_StartLogo.pas
@@ -31,6 +31,7 @@ type
 implementation
 
 uses
+  Types,
   c_SasVersion,
   u_GlobalState;
 
@@ -46,8 +47,20 @@ begin
 end;
 
 procedure TfrmStartLogo.FormShow(Sender: TObject);
+var
+  VBitmapSize: TPoint;
 begin
   GState.LoadBitmapFromJpegRes('LOGOI', imgLogo.Bitmap);
+  VBitmapSize.X := imgLogo.Bitmap.Width;
+  VBitmapSize.Y := imgLogo.Bitmap.Height;
+
+  if VBitmapSize.X < 100 then begin
+    VBitmapSize.X := 480;
+  end;
+  if VBitmapSize.Y < 100 then begin
+    VBitmapSize.Y := 276;
+  end;
+  imgLogo.Bitmap.SetSize(VBitmapSize.X, VBitmapSize.Y);
   lblVersion.Caption:='v '+SASVersion;
   FReadyToHide := False;
 end;
