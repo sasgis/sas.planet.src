9254edf996dd9b5aa93eff830983fd87ccb15122
feya <az@sasgis.ru>
2011-09-27 16:10:34 +0400

    Перевел шапку на англицкий.

--- i_DownloadResult.pas
+++ i_DownloadResult.pas
@@ -1,18 +1,20 @@
 {******************************************************************************}
-{* SAS.Планета (SAS.Planet)                                                   *}
-{* Copyright (C) 2007-2011, авторы программы SAS.Планета (SAS.Planet).        *}
-{* Это программа является свободным программным обеспечением. Вы можете       *}
-{* распространять и/или модифицировать её согласно условиям Стандартной       *}
-{* Общественной Лицензии GNU, опубликованной Фондом Свободного Программного   *}
-{* Обеспечения, версии 3. Эта программа распространяется в надежде, что она   *}
-{* будет полезной, но БЕЗ ВСЯКИХ ГАРАНТИЙ, в том числе подразумеваемых        *}
-{* гарантий ТОВАРНОГО СОСТОЯНИЯ ПРИ ПРОДАЖЕ и ГОДНОСТИ ДЛЯ ОПРЕДЕЛЁННОГО      *}
-{* ПРИМЕНЕНИЯ. Смотрите Стандартную Общественную Лицензию GNU версии 3, для   *}
-{* получения дополнительной информации. Вы должны были получить копию         *}
-{* Стандартной Общественной Лицензии GNU вместе с программой. В случае её     *}
-{* отсутствия, посмотрите http://www.gnu.org/licenses/.                       *}
+{* SAS.Planet (SAS.Планета)                                                   *}
+{* Copyright (C) 2007-2011, SAS.Planet development team.                      *}
+{* This program is free software: you can redistribute it and/or modify       *}
+{* it under the terms of the GNU General Public License as published by       *}
+{* the Free Software Foundation, either version 3 of the License, or          *}
+{* (at your option) any later version.                                        *}
 {*                                                                            *}
-{* http://sasgis.ru/sasplanet                                                 *}
+{* This program is distributed in the hope that it will be useful,            *}
+{* but WITHOUT ANY WARRANTY; without even the implied warranty of             *}
+{* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *}
+{* GNU General Public License for more details.                               *}
+{*                                                                            *}
+{* You should have received a copy of the GNU General Public License          *}
+{* along with this program.  If not, see <http://www.gnu.org/licenses/>.      *}
+{*                                                                            *}
+{* http://sasgis.ru                                                           *}
 {* az@sasgis.ru                                                               *}
 {******************************************************************************}
 
