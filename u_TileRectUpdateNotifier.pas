c9de52a5a319c6e11744d2b3f78f163b0e79c2d4
Viktor Demydov <vdemidov@gmail.com>
2011-11-01 16:39:51 +0200

    Новый интерфейс для уведомления об изменениях тайлов карты.

---
+++ u_TileRectUpdateNotifier.pas
@@ -0,0 +1,165 @@
+unit u_TileRectUpdateNotifier;
+
+interface
+
+uses
+  Types,
+  SysUtils,
+  i_JclNotify,
+  i_CoordConverter,
+  i_TileRectUpdateNotifier;
+
+type
+  TListenerRecord = record
+    Listener: IJclListener;
+    Rect: TRect;
+  end;
+
+  TTileRectUpdateNotifier = class(TInterfacedObject, ITileRectUpdateNotifier)
+  private
+    FGeoCoder: ICoordConverter;
+    FZoom: Byte;
+    FSynchronizer: TMultiReadExclusiveWriteSynchronizer;
+    FCount: Integer;
+    FList: array of TListenerRecord;
+    function CalcGrowSize(AOldSize: Integer): Integer;
+  protected
+    function GetGeoCoder: ICoordConverter; stdcall;
+    function GetZoom: Byte; stdcall;
+
+    procedure Add(AListener: IJclListener; ATileRect: TRect); stdcall;
+    procedure Remove(AListener: IJclListener); stdcall;
+
+    procedure TileUpdateNotify(ATile: TPoint); stdcall;
+  public
+    constructor Create(
+      AZoom: Byte;
+      AGeoCoder: ICoordConverter
+    );
+    destructor Destroy; override;
+  end;
+
+implementation
+
+uses
+  u_JclNotify;
+
+{ TTileRectUpdateNotifier }
+
+constructor TTileRectUpdateNotifier.Create(
+  AZoom: Byte;
+  AGeoCoder: ICoordConverter
+);
+begin
+  FZoom := AZoom;
+  FGeoCoder := AGeoCoder;
+  FSynchronizer := TMultiReadExclusiveWriteSynchronizer.Create;
+end;
+
+destructor TTileRectUpdateNotifier.Destroy;
+var
+  i: Integer;
+begin
+  for i := 0 to FCount - 1 do begin
+    FList[i].Listener := nil;
+  end;
+  inherited;
+end;
+
+function TTileRectUpdateNotifier.CalcGrowSize(AOldSize: Integer): Integer;
+begin
+  if AOldSize < 8 then begin
+    Result := 8;
+  end else begin
+    Result := AOldSize * 2;
+  end;
+end;
+
+procedure TTileRectUpdateNotifier.Add(AListener: IJclListener; ATileRect: TRect);
+var
+  i: Integer;
+  VIndex: Integer;
+begin
+  Assert(AListener <> nil);
+  FSynchronizer.BeginWrite;
+  try
+    VIndex := -1;
+    for i := 0 to FCount - 1 do begin
+      if FList[i].Listener = AListener then begin
+        VIndex := i;
+        Break;
+      end;
+    end;
+    if VIndex < 0 then begin
+      if FCount >= Length(FList) then begin
+        SetLength(FList, CalcGrowSize(Length(FList)));
+      end;
+      VIndex := FCount;
+      Inc(FCount);
+    end;
+    FList[VIndex].Listener := AListener;
+    FList[VIndex].Rect := ATileRect;
+  finally
+    FSynchronizer.EndWrite;
+  end;
+end;
+
+function TTileRectUpdateNotifier.GetGeoCoder: ICoordConverter;
+begin
+  Result := FGeoCoder;
+end;
+
+function TTileRectUpdateNotifier.GetZoom: Byte;
+begin
+  Result := FZoom;
+end;
+
+procedure TTileRectUpdateNotifier.Remove(AListener: IJclListener);
+var
+  i: Integer;
+  VIndex: Integer;
+begin
+  Assert(AListener <> nil);
+  FSynchronizer.BeginWrite;
+  try
+    VIndex := -1;
+    for i := 0 to FCount - 1 do begin
+      if FList[i].Listener = AListener then begin
+        VIndex := i;
+        Break;
+      end;
+    end;
+    if VIndex >= 0 then begin
+      FList[VIndex].Listener := nil;
+      Dec(FCount);
+      if VIndex < FCount then begin
+        System.Move(
+          FList[VIndex + 1],
+          FList[VIndex],
+          (FCount - VIndex) * SizeOf(TListenerRecord)
+        );
+        Pointer(FList[FCount].Listener) := nil;
+      end;
+    end;
+  finally
+    FSynchronizer.EndWrite;
+  end;
+end;
+
+procedure TTileRectUpdateNotifier.TileUpdateNotify(ATile: TPoint);
+var
+  i: Integer;
+begin
+  FSynchronizer.BeginRead;
+  try
+    for i := 0 to FCount - 1 do begin
+      if PtInRect(FList[i].Rect, ATile) then begin
+        FList[i].Listener.Notification(nil);
+      end;
+    end;
+  finally
+    FSynchronizer.EndRead;
+  end;
+end;
+
+end.
