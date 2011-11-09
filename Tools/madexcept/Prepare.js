7ca72df040eec76238b78b8f0b49cfaed9134a26
zed <starmen@tut.by>
2011-11-09 17:19:36 +0200

    В Tools добавил батник для компиляции отладочной версии при помощи madExcept

---
+++ Tools/madexcept/Prepare.js
@@ -0,0 +1,27 @@
+
+function ReadFile( fso, fileName){
+	var textFile = fso.OpenTextFile(fileName, 1, false);
+	var text = "";
+	if (!textFile.AtEndOfStream){
+		text = textFile.ReadAll();
+	};
+	textFile.Close();
+	return text;
+}
+
+var fso = WScript.CreateObject("Scripting.FileSystemObject");
+
+var VersionPostfix = ReadFile(fso, "VersionPostfix.inc");
+VersionPostfix = VersionPostfix + ' -= Debug =-';
+var fileVersionPostfix = fso.OpenTextFile("VersionPostfix.inc", 2, false);
+fileVersionPostfix.write(VersionPostfix);
+fileVersionPostfix.Close();
+
+var dpr = ReadFile(fso, "SASPlanet.dpr");
+var reg = /u_MadExcept/i;
+if (!reg.test(dpr)){
+	dpr = dpr.replace('uses\r\n', 'uses\r\n  u_MadExcept,\r\n');
+	var dprFile = fso.OpenTextFile("SASPlanet.dpr", 2, false);
+	dprFile.write(dpr);
+	dprFile.Close();
+}
---
+++ Tools/madexcept/u_MadExcept.pas
@@ -0,0 +1,17 @@
+unit u_MadExcept;
+
+interface
+
+uses
+  madExcept,
+  madLinkDisAsm,
+  madListHardware,
+  madListProcesses,
+  madListModules;
+  
+
+implementation
+
+// Make extended configuration madExcept here //
+
+end.
