50de7e0cbf5146907cf2e02f06f1eddb07c102da
Viktor Demydov <vdemidov@gmail.com>
2011-12-27 22:23:46 +0200

    Проект для юнит-тестов и один юнит-тест

---
+++ Test/SASPlanetTests.dpr
@@ -0,0 +1,33 @@
+program SASPlanetTests;
+{
+
+  Delphi DUnit Test Project
+  -------------------------
+  This project contains the DUnit test framework and the GUI/Console test runners.
+  Add "CONSOLE_TESTRUNNER" to the conditional defines entry in the project options 
+  to use the console test runner.  Otherwise the GUI test runner will be used by 
+  default.
+
+}
+
+{$IFDEF CONSOLE_TESTRUNNER}
+{$APPTYPE CONSOLE}
+{$ENDIF}
+
+uses
+  Forms,
+  TestFramework,
+  GUITestRunner,
+  TextTestRunner,
+  u_EnumDoublePointClosePoly_Test in '..\u_EnumDoublePointClosePoly_Test.pas';
+
+{$R *.RES}
+
+begin
+  Application.Initialize;
+  if IsConsole then
+    TextTestRunner.RunRegisteredTests
+  else
+    GUITestRunner.RunRegisteredTests;
+end.
+
