d7d603f42c0634f70a7f78ec8dfd8756dc4a6123
Viktor Demydov <vdemidov@gmail.com>
2012-02-07 16:01:58 +0200

    Пара новых юнитов

---
+++ i_RegionProcessProgressInfo.pas
@@ -0,0 +1,35 @@
+unit i_RegionProcessProgressInfo;
+
+interface
+
+type
+  IProgressInfo = interface
+    ['{B636F3D1-3F17-4BE5-8D8E-E161F945E42D}']
+    function GetProcessed: Double;
+    procedure SetProcessed(AValue: Double);
+    property Processed: Double read GetProcessed write SetProcessed;
+  end;
+  
+  IRegionProcessProgressInfo = interface(IProgressInfo)
+    ['{58559CEF-9233-4E25-87E0-F88E1A78C5AD}']
+    function GetFinished: Boolean;
+    property Finished: Boolean;
+
+    function GetCaption: string;
+    procedure SetCaption(AValue: string);
+    property Caption: string read GetCaption write SetCaption;
+
+    function GetFirstLine: string;
+    procedure SetFirstLine(AValue: string);
+    property FirstLine: string read GetFirstLine write SetFirstLine;
+
+    function GetSecondLine: string;
+    procedure SetSecondLine(AValue: string);
+    property SecondLine: string read GetSecondLine write SetSecondLine;
+
+    procedure Finish;
+  end;
+
+implementation
+
+end.
