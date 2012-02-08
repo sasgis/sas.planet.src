8fa62245b2524b908f3d3fa7b259fa3b5145c68c
Sergey Vasketsov <vasketsov@mail.ru>
2012-02-08 14:51:54 +0400

    Добавлена функция GetCount для упрощения работы со списком карт

--- u_MapTypeSet.pas
+++ u_MapTypeSet.pas
@@ -1,6 +1,6 @@
 {******************************************************************************}
 {* SAS.Planet (SAS.Планета)                                                   *}
-{* Copyright (C) 2007-2011, SAS.Planet development team.                      *}
+{* Copyright (C) 2007-2012, SAS.Planet development team.                      *}
 {* This program is free software: you can redistribute it and/or modify       *}
 {* it under the terms of the GNU General Public License as published by       *}
 {* the Free Software Foundation, either version 3 of the License, or          *}
@@ -33,6 +33,7 @@ type
     FList: IGUIDInterfaceSet;
     function GetMapTypeByGUID(AGUID: TGUID): IMapType;
     function GetIterator: IEnumGUID;
+    function GetCount: Integer;
   public
     procedure Add(AMap: IMapType);
     constructor Create(AAllowNil: Boolean);
@@ -68,6 +69,11 @@ destructor TMapTypeSet.Destroy;
 begin
   FList := nil;
   inherited;
+end;
+
+function TMapTypeSet.GetCount: Integer;
+begin
+  Result := FList.Count;
 end;
 
 function TMapTypeSet.GetIterator: IEnumGUID;
