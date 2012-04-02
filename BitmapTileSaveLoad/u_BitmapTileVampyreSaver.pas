92ba05e91230484b7dd33e736554db2ec9ddec83
Viktor Demydov <vdemidov@gmail.com>
2012-04-02 12:51:45 +0300

    Добавил в вампировскую сейвилку затычку как и в лоадер

--- BitmapTileSaveLoad/u_BitmapTileVampyreSaver.pas
+++ BitmapTileSaveLoad/u_BitmapTileVampyreSaver.pas
@@ -4,32 +4,50 @@ interface
 
 uses
   Classes,
+  SysUtils,
   Imaging,
   ImagingTypes,
   GR32,
   i_BinaryData,
   i_Bitmap32Static,
   i_ARGBToPaletteConverter,
+  i_InternalPerformanceCounter,
   i_BitmapTileSaveLoad;
 
 type
   TVampyreBasicBitmapTileSaver = class(TInterfacedObject, IBitmapTileSaver)
   private
+    FCS: IReadWriteSync;
+    FMetadata: TMetadata;
     FFormat: TImageFileFormat;
+    FPerfCounter: IInternalPerformanceCounter;
   protected
     procedure PrepareData(var AImage: TImageData); virtual;
   protected
     procedure SaveToStream(ABtm: TCustomBitmap32; AStream: TStream);
     function Save(ABitmap: IBitmap32Static): IBinaryData;
   public
-    constructor Create(AFormat: TImageFileFormat);
+    constructor Create(
+      AFormat: TImageFileFormat;
+      AMeta: TMetadata;
+      APerfCounterList: IInternalPerformanceCounterList
+    ); overload;
+    constructor Create(
+      AFormatClass: TImageFileFormatClass;
+      APerfCounterList: IInternalPerformanceCounterList
+    ); overload;
     destructor Destroy; override;
   end;
 
   TVampyreBasicBitmapTileSaverPNG = class(TVampyreBasicBitmapTileSaver)
   public
-    constructor Create(ACompressLevel: LongInt); overload;
-    constructor Create; overload;
+    constructor Create(
+      ACompressLevel: LongInt;
+      APerfCounterList: IInternalPerformanceCounterList = nil
+    ); overload;
+    constructor Create(
+      APerfCounterList: IInternalPerformanceCounterList = nil
+    ); overload;
   end;
 
   TVampyreBasicBitmapTileSaverPNGRGB = class(TVampyreBasicBitmapTileSaverPNG)
@@ -45,46 +63,77 @@ type
   public
     constructor Create(
       AConverter: IARGBToPaletteConverter;
-      ACompressLevel: LongInt
+      ACompressLevel: LongInt;
+      APerfCounterList: IInternalPerformanceCounterList = nil
     );
   end;
 
   TVampyreBasicBitmapTileSaverGIF = class(TVampyreBasicBitmapTileSaver)
   public
-    constructor Create;
+    constructor Create(
+      APerfCounterList: IInternalPerformanceCounterList = nil
+    );
   end;
 
   TVampyreBasicBitmapTileSaverBMP = class(TVampyreBasicBitmapTileSaver)
   public
-    constructor Create;
+    constructor Create(
+      APerfCounterList: IInternalPerformanceCounterList = nil
+    );
   end;
 
   TVampyreBasicBitmapTileSaverJPG = class(TVampyreBasicBitmapTileSaver)
   public
-    constructor Create(ACompressionQuality: Byte);
+    constructor Create(
+      ACompressionQuality: Byte;
+      APerfCounterList: IInternalPerformanceCounterList = nil
+    );
   end;
 
 implementation
 
 uses
-  SysUtils,
   ImagingGraphics32,
   ImagingNetworkGraphics,
   ImagingJpeg,
   ImagingGif,
   ImagingBitmap,
+  u_Synchronizer,
   u_BinaryDataByMemStream;
 
 { TVampyreBasicBitmapTileSaver }
 
-constructor TVampyreBasicBitmapTileSaver.Create(AFormat: TImageFileFormat);
+constructor TVampyreBasicBitmapTileSaver.Create(
+  AFormat: TImageFileFormat;
+  AMeta: TMetadata;
+  APerfCounterList: IInternalPerformanceCounterList
+);
 begin
+  FCS := MakeSyncObj(Self, TRUE);
+  FMetadata := AMeta;
   FFormat := AFormat;
+  if APerfCounterList = nil then begin
+    FPerfCounter := nil;
+  end else begin
+    FPerfCounter := APerfCounterList.CreateAndAddNewCounter('SaveStream');
+  end;
+end;
+
+constructor TVampyreBasicBitmapTileSaver.Create(
+  AFormatClass: TImageFileFormatClass;
+  APerfCounterList: IInternalPerformanceCounterList);
+var
+  VMeta: TMetadata;
+begin
+  VMeta := TMetadata.Create;
+  Create(AFormatClass.Create(VMeta), VMeta, APerfCounterList);
 end;
 
 destructor TVampyreBasicBitmapTileSaver.Destroy;
 begin
   FreeAndNil(FFormat);
+  FreeAndNil(FMetadata);
+  FCS := nil;
   inherited;
 end;
 
@@ -96,106 +145,124 @@ function TVampyreBasicBitmapTileSaver.Save(
   ABitmap: IBitmap32Static
 ): IBinaryData;
 var
-  VFormat: TImageFileFormat;
   VImage: TImageData;
   IArray: TDynImageDataArray;
   VMemStream: TMemoryStream;
+  VCounterContext: TInternalPerformanceCounterContext;
 begin
-  if FFormat <> nil then begin
-    VFormat := FFormat;
+  if FPerfCounter <> nil then begin
+    VCounterContext := FPerfCounter.StartOperation;
   end else begin
-    VFormat := FindImageFileFormatByExt('.bmp');
+    VCounterContext := 0;
   end;
-  if VFormat = nil then begin
-    raise Exception.Create('Неизвестный формат файла');
-  end;
-  InitImage(VImage);
   try
-    ConvertBitmap32ToImageData(ABitmap.Bitmap, VImage);
-    PrepareData(VImage);
-    SetLength(IArray, 1);
-    IArray[0] := VImage;
-    VMemStream := TMemoryStream.Create;
+    InitImage(VImage);
     try
-      if not VFormat.SaveToStream(VMemStream, IArray, True) then begin
-        raise Exception.Create('Ошибка записи файла');
+      ConvertBitmap32ToImageData(ABitmap.Bitmap, VImage);
+      PrepareData(VImage);
+      SetLength(IArray, 1);
+      IArray[0] := VImage;
+      VMemStream := TMemoryStream.Create;
+      try
+        FCS.BeginWrite;
+        try
+          if not FFormat.SaveToStream(VMemStream, IArray, True) then begin
+            raise Exception.Create('Ошибка записи файла');
+          end;
+        finally
+          FCS.EndWrite;
+        end;
+        Result := TBinaryDataByMemStream.CreateWithOwn(VMemStream);
+        VMemStream := nil;
+      finally
+        VMemStream.Free;
       end;
-      Result := TBinaryDataByMemStream.CreateWithOwn(VMemStream);
-      VMemStream := nil;
     finally
-      VMemStream.Free;
+      FreeImage(VImage);
     end;
   finally
-    FreeImage(VImage);
+    if FPerfCounter <> nil then begin
+      FPerfCounter.FinishOperation(VCounterContext);
+    end;
   end;
 end;
 
 procedure TVampyreBasicBitmapTileSaver.SaveToStream(ABtm: TCustomBitmap32;
   AStream: TStream);
 var
-  VFormat: TImageFileFormat;
   VImage: TImageData;
   IArray: TDynImageDataArray;
+  VCounterContext: TInternalPerformanceCounterContext;
 begin
-  if FFormat <> nil then begin
-    VFormat := FFormat;
+  if FPerfCounter <> nil then begin
+    VCounterContext := FPerfCounter.StartOperation;
   end else begin
-    VFormat := FindImageFileFormatByExt('.bmp');
+    VCounterContext := 0;
   end;
-  if VFormat = nil then begin
-    raise Exception.Create('Неизвестный формат файла');
-  end;
-  InitImage(VImage);
   try
-    ConvertBitmap32ToImageData(ABtm, VImage);
-    PrepareData(VImage);
-    SetLength(IArray, 1);
-    IArray[0] := VImage;
-    if not VFormat.SaveToStream(AStream, IArray, True) then begin
-      raise Exception.Create('Ошибка записи файла');
+    InitImage(VImage);
+    try
+      ConvertBitmap32ToImageData(ABtm, VImage);
+      PrepareData(VImage);
+      SetLength(IArray, 1);
+      IArray[0] := VImage;
+      FCS.BeginWrite;
+      try
+        if not FFormat.SaveToStream(AStream, IArray, True) then begin
+          raise Exception.Create('Ошибка записи файла');
+        end;
+      finally
+        FCS.EndWrite;
+      end;
+    finally
+      FreeImage(VImage);
     end;
   finally
-    FreeImage(VImage);
+    if FPerfCounter <> nil then begin
+      FPerfCounter.FinishOperation(VCounterContext);
+    end;
   end;
 end;
 
 { TVampyreBasicBitmapTileSaverPNG }
 
-constructor TVampyreBasicBitmapTileSaverPNG.Create(ACompressLevel: LongInt);
+constructor TVampyreBasicBitmapTileSaverPNG.Create(
+  ACompressLevel: LongInt;
+  APerfCounterList: IInternalPerformanceCounterList
+);
 var
   VFormat: TPNGFileFormat;
+  VMeta: TMetadata;
 begin
-  VFormat := TPNGFileFormat.Create();
+  VMeta := TMetadata.Create;
+  VFormat := TPNGFileFormat.Create(VMeta);
   VFormat.CompressLevel := ACompressLevel;
-  inherited Create(VFormat);
+  inherited Create(VFormat, VMeta, APerfCounterList);
 end;
 
-constructor TVampyreBasicBitmapTileSaverPNG.Create;
-var
-  VFormat: TPNGFileFormat;
+constructor TVampyreBasicBitmapTileSaverPNG.Create(
+  APerfCounterList: IInternalPerformanceCounterList
+);
 begin
-  VFormat := TPNGFileFormat.Create();
-  inherited Create(VFormat);
+  inherited Create(TPNGFileFormat, APerfCounterList);
 end;
 
 { TVampyreBasicBitmapTileSaverGIF }
 
-constructor TVampyreBasicBitmapTileSaverGIF.Create;
-var
-  VFormat: TGIFFileFormat;
+constructor TVampyreBasicBitmapTileSaverGIF.Create(
+  APerfCounterList: IInternalPerformanceCounterList = nil
+);
 begin
-  VFormat := TGIFFileFormat.Create();
-  inherited Create(VFormat);
+  inherited Create(TGIFFileFormat, APerfCounterList);
 end;
 
 { TVampyreBasicBitmapTileSaverBMP }
 
-constructor TVampyreBasicBitmapTileSaverBMP.Create;
-var
-  VFormat: TBitmapFileFormat;
+constructor TVampyreBasicBitmapTileSaverBMP.Create(
+  APerfCounterList: IInternalPerformanceCounterList = nil
+);
 begin
-  VFormat := TBitmapFileFormat.Create();
-  inherited Create(VFormat);
+  inherited Create(TBitmapFileFormat, APerfCounterList);
 end;
 
 { TVampyreBasicBitmapTileSaverPNGRGB }
@@ -208,9 +275,12 @@ end;
 { TVampyreBasicBitmapTileSaverPNGPalette }
 
 constructor TVampyreBasicBitmapTileSaverPNGPalette.Create(
-  AConverter: IARGBToPaletteConverter; ACompressLevel: Integer);
+  AConverter: IARGBToPaletteConverter;
+  ACompressLevel: Integer;
+  APerfCounterList: IInternalPerformanceCounterList = nil
+);
 begin
-  inherited Create(ACompressLevel);
+  inherited Create(ACompressLevel, APerfCounterList);
   FConverter := AConverter;
 end;
 
@@ -223,13 +293,18 @@ end;
 
 { TVampyreBasicBitmapTileSaverJPG }
 
-constructor TVampyreBasicBitmapTileSaverJPG.Create(ACompressionQuality: byte);
+constructor TVampyreBasicBitmapTileSaverJPG.Create(
+  ACompressionQuality: byte;
+  APerfCounterList: IInternalPerformanceCounterList = nil
+);
 var
   VFormat: TJpegFileFormat;
+  VMeta: TMetadata;
 begin
-  VFormat := TJpegFileFormat.Create();
+  VMeta := TMetadata.Create;
+  VFormat := TJpegFileFormat.Create(VMeta);
   VFormat.Quality := ACompressionQuality;
-  inherited Create(VFormat);
+  inherited Create(VFormat, VMeta, APerfCounterList);
 end;
 
 end.
