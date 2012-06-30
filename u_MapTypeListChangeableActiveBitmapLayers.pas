885f9857f853e3d8a266299f11c090f3401a1116
Viktor Demydov <vdemidov@gmail.com>
2012-06-30 16:18:58 +0300

    Добавил сортировку в новый класс списка слоев.

--- u_MapTypeListChangeableActiveBitmapLayers.pas
+++ u_MapTypeListChangeableActiveBitmapLayers.pas
@@ -76,8 +76,49 @@ begin
 end;
 
 function TMapTypeListChangeableActiveBitmapLayers.CreateStatic: IInterface;
+  procedure QuickSort(
+    var AMapsList: array of IMapType;
+    var AZList: array of Integer;
+    L, R: Integer
+  );
+  var
+    I, J: Integer;
+    P: Integer;
+    TI: Integer;
+    TM: IMapType;
+  begin
+    repeat
+      I := L;
+      J := R;
+      P := AZList[(L + R) shr 1];
+      repeat
+        while AZList[I] < P do begin
+          Inc(I);
+        end;
+        while AZList[J] > P do begin
+          Dec(J);
+        end;
+        if I <= J then begin
+          TI := AZList[I];
+          TM := AMapsList[I];
+
+          AZList[I] := AZList[J];
+          AMapsList[I] := AMapsList[J];
+          AZList[J] := TI;
+          AMapsList[J] := TM;
+          Inc(I);
+          Dec(J);
+        end;
+      until I > J;
+      if L < J then begin
+        QuickSort(AMapsList, AZList, L, J);
+      end;
+      L := I;
+    until I >= R;
+  end;
 var
   VLayers: array of IMapType;
+  VZArray: array of Integer;
   i: Integer;
   VEnum: IEnumGUID;
   VCnt: Cardinal;
@@ -100,8 +141,14 @@ begin
         end;
       end;
     end;
-    SetLength(VLayers, i);
-    Result := TMapTypeListStatic.Create(VLayers);
+    VCount := i;
+    SetLength(VLayers, VCount);
+    SetLength(VZArray, VCount);
+    for i := 0 to VCount - 1 do begin
+      VZArray[i] := VLayers[i].MapType.LayerDrawConfig.LayerZOrder;
+    end;
+    QuickSort(VLayers, VZArray, 0, VCount - 1);
+    Result := IMapTypeListStatic(TMapTypeListStatic.Create(VLayers));
   finally
     for i := 0 to Length(VLayers) - 1 do begin
       VLayers[i] := nil;
