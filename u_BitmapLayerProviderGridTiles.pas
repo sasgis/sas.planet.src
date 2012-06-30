8eaa6b387f99cf8995bc1922c4a1d0ee465c91c7
Viktor Demydov <vdemidov@gmail.com>
2012-06-30 19:36:43 +0300

    Ковыряю новый класс для отрисовки сеток

--- u_BitmapLayerProviderGridTiles.pas
+++ u_BitmapLayerProviderGridTiles.pas
@@ -64,6 +64,13 @@ var
   VMapRect: TDoubleRect;
   VRelativeRect: TDoubleRect;
   VTilesRect: TRect;
+  VTilesLineRect: TRect;
+  i, j: integer;
+  VTileRelativeRect: TDoubleRect;
+  VTileRect: TRect;
+  VTileScreenRect: TRect;
+  VLocalRect: TRect;
+  VBitmap: TCustomBitmap32;
 begin
   Result := nil;
   VCurrentZoom := ALocalConverter.GetZoom;
@@ -77,12 +84,71 @@ begin
     Exit;
   end;
 
+  VLocalRect := ALocalConverter.GetLocalRect;
   VMapRect := ALocalConverter.GetRectInMapPixelFloat;
   VGeoConvert.CheckPixelRectFloat(VMapRect, VCurrentZoom);
 
   VRelativeRect := VGeoConvert.PixelRectFloat2RelativeRect(VMapRect, VCurrentZoom);
   VTilesRect := VGeoConvert.RelativeRect2TileRect(VRelativeRect, VGridZoom);
 
+  VTilesLineRect.Left := VTilesRect.Left;
+  VTilesLineRect.Right := VTilesRect.Right;
+  VBitmap := TCustomBitmap32.Create;
+  try
+    for i := VTilesRect.Top to VTilesRect.Bottom do begin
+      VTilesLineRect.Top := i;
+      VTilesLineRect.Bottom := i;
+
+      VTileRelativeRect := VGeoConvert.TileRect2RelativeRect(VTilesLineRect, VGridZoom);
+      VTileRect := VGeoConvert.RelativeRect2PixelRect(VTileRelativeRect, VCurrentZoom);
+      VTileScreenRect := ALocalConverter.MapRect2LocalRect(VTileRect);
+
+      VTileScreenRect.Left := VLocalRect.Left;
+      VTileScreenRect.Right := VLocalRect.Right;
+
+      if VTileScreenRect.Top < VLocalRect.Top then begin
+        VTileScreenRect.Top := VLocalRect.Top;
+        VTileScreenRect.Bottom := VTileScreenRect.Top;
+      end;
+
+      if VTileScreenRect.Top > VLocalRect.Bottom then begin
+        VTileScreenRect.Top := VLocalRect.Bottom;
+        VTileScreenRect.Bottom := VTileScreenRect.Top;
+      end;
+
+      VBitmap.HorzLineTS(VTileScreenRect.Left, VTileScreenRect.Top,
+        VTileScreenRect.Right, FColor);
+    end;
+
+    VTilesLineRect.Top := VTilesRect.Top;
+    VTilesLineRect.Bottom := VTilesRect.Bottom;
+    for j := VTilesRect.Left to VTilesRect.Right do begin
+      VTilesLineRect.Left := j;
+      VTilesLineRect.Right := j;
+
+      VTileRelativeRect := VGeoConvert.TileRect2RelativeRect(VTilesLineRect, VGridZoom);
+      VTileRect := VGeoConvert.RelativeRect2PixelRect(VTileRelativeRect, VCurrentZoom);
+      VTileScreenRect := ALocalConverter.MapRect2LocalRect(VTileRect);
+
+      VTileScreenRect.Top := VLocalRect.Top;
+      VTileScreenRect.Bottom := VLocalRect.Bottom;
+
+      if VTileScreenRect.Left < VLocalRect.Left then begin
+        VTileScreenRect.Left := VLocalRect.Left;
+        VTileScreenRect.Right := VTileScreenRect.Left;
+      end;
+
+      if VTileScreenRect.Left > VLocalRect.Right then begin
+        VTileScreenRect.Left := VLocalRect.Right;
+        VTileScreenRect.Right := VTileScreenRect.Left;
+      end;
+
+      VBitmap.VertLineTS(VTileScreenRect.Left, VTileScreenRect.Top,
+        VTileScreenRect.Bottom, FColor);
+    end;
+  finally
+    VBitmap.Free;
+  end;
 end;
 
 end.
