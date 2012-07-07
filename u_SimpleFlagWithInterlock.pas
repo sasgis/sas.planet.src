05ef219722e559b074968a7bd83171dd8b7497d2
Viktor Demydov <vdemidov@gmail.com>
2012-07-07 15:20:39 +0300

    Новый класс флага

--- u_SimpleFlagWithInterlock.pas
+++ u_SimpleFlagWithInterlock.pas
@@ -18,6 +18,27 @@ type
     constructor Create;
   end;
 
+  TSimpleFlagWithParent = class(TInterfacedObject, ISimpleFlag)
+  private
+    FParent: ISimpleFlag;
+    FSetCount: Integer;
+  private
+    procedure SetFlag;
+    function CheckFlagAndReset: Boolean;
+    function CheckFlag: Boolean;
+  public
+    constructor Create(AParent: ISimpleFlag);
+  end;
+
+  TCounterInterlock = class(TInterfacedObject, ICounter)
+  private
+    FCount: Integer;
+  private
+    function Inc: Integer;
+    function Dec: Integer;
+  public
+    constructor Create;
+  end;
 
 implementation
 
@@ -41,6 +62,49 @@ end;
 
 procedure TSimpleFlagWithInterlock.SetFlag;
 begin
+  InterlockedIncrement(FSetCount);
+end;
+
+{ TCounterInterlock }
+
+constructor TCounterInterlock.Create;
+begin
+  inherited Create;
+  FCount := 0;
+end;
+
+function TCounterInterlock.Dec: Integer;
+begin
+  Result := InterlockedDecrement(FCount);
+end;
+
+function TCounterInterlock.Inc: Integer;
+begin
+  Result := InterlockedIncrement(FCount);
+end;
+
+{ TSimpleFlagWithParent }
+
+constructor TSimpleFlagWithParent.Create(AParent: ISimpleFlag);
+begin
+  inherited Create;
+  FParent := AParent;
+  FSetCount := 0;
+end;
+
+function TSimpleFlagWithParent.CheckFlag: Boolean;
+begin
+  Result := InterlockedCompareExchange(FSetCount, 0, 0) > 0;
+end;
+
+function TSimpleFlagWithParent.CheckFlagAndReset: Boolean;
+begin
+  Result := InterlockedExchange(FSetCount, 0) > 0;
+end;
+
+procedure TSimpleFlagWithParent.SetFlag;
+begin
+  FParent.SetFlag;
   InterlockedIncrement(FSetCount);
 end;
 
