3426cc035c472bf341c8413abc108003fa54a6c5
Viktor Demydov <vdemidov@gmail.com>
2012-07-27 19:49:01 +0300

    Вынес класс в отдельный юнит

---
+++ u_ActiveMapSingleSet.pas
@@ -0,0 +1,36 @@
+unit u_ActiveMapSingleSet;
+
+interface
+
+uses
+  i_GUIDSet,
+  i_ActiveMapsConfig;
+
+type
+  TActiveMapSingleSet = class(TInterfacedObject, IActiveMapSingleSet)
+  private
+    FSet: IGUIDInterfaceSet;
+  private
+    function GetMapSingle(const AMapGUID: TGUID): IActiveMapSingle;
+  public
+    constructor Create(ASet: IGUIDInterfaceSet);
+  end;
+
+implementation
+
+{ TActiveMapSingleSet }
+
+constructor TActiveMapSingleSet.Create(ASet: IGUIDInterfaceSet);
+begin
+  inherited Create;
+  FSet := ASet;
+end;
+
+function TActiveMapSingleSet.GetMapSingle(
+  const AMapGUID: TGUID): IActiveMapSingle;
+begin
+  Result := IActiveMapSingle(FSet.GetByGUID(AMapGUID));
+end;
+
+
+end.
