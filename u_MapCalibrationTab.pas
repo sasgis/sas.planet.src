e8ba4e8ba22709e0d8a2c6b1a8ca1aba5887280a
Viktor Demydov <vdemidov@gmail.com>
2012-08-30 11:05:08 +0300

    Переделываю сохранение файлов привязки склеенных карт

--- u_MapCalibrationTab.pas
+++ u_MapCalibrationTab.pas
@@ -29,7 +29,7 @@ uses
 
 type
   TMapCalibrationTab = class(TInterfacedObject, IMapCalibration)
-  public
+  private
     // Имя для вывода в листбоксе для выбора при экспорте.
     function GetName: WideString; safecall;
     // Более детальное описание привязки
@@ -46,6 +46,7 @@ type
 implementation
 
 uses
+  Classes,
   SysUtils,
   t_GeoTypes,
   u_GeoToStr;
@@ -69,52 +70,59 @@ procedure TMapCalibrationTab.SaveCalibrationInfo(
   const AConverter: ICoordConverter
 );
 var
-  f: TextFile;
   xy: TPoint;
-  lat, lon: array[1..3] of real;
+  lat, lon: array[1..3] of Double;
   VLL1, VLL2: TDoublePoint;
   VLL: TDoublePoint;
   VLocalRect: TRect;
+  VFileName: string;
+  VFileStream: TFileStream;
+  VText: AnsiString;
 begin
-  assignfile(f, ChangeFileExt(AFileName, '.tab'));
-  rewrite(f);
-  writeln(f, '!table');
-  writeln(f, '!version 300');
-  writeln(f, '!charset WindowsCyrillic' + #13#10);
-  writeln(f, 'Definition Table');
-  writeln(f, 'File "' + ExtractFileName(AFileName) + '"');
-  writeln(f, 'Type "RASTER"');
+  VFileName := ChangeFileExt(AFileName, '.tab');
+  VFileStream := TFileStream.Create(VFileName, fmCreate);
+  try
+    VText := '';
+    VText := VText + '!table' + #13#10;
+    VText := VText + '!version 300' + #13#10;
+    VText := VText + '!charset WindowsCyrillic' + #13#10 + #13#10;
+    VText := VText + 'Definition Table'  + #13#10;
+    VText := VText + 'File "' + ExtractFileName(AFileName) + '"' + #13#10;
+    VText := VText + 'Type "RASTER"' + #13#10;
 
-  VLL1 := AConverter.PixelPos2LonLat(xy1, AZoom);
-  VLL2 := AConverter.PixelPos2LonLat(xy2, AZoom);
-  xy.Y := (xy2.y - ((xy2.Y - xy1.Y) div 2));
-  xy.X := (xy2.x - ((xy2.x - xy1.x) div 2));
-  VLL := AConverter.PixelPos2LonLat(xy, AZoom);
+    VLL1 := AConverter.PixelPos2LonLat(xy1, AZoom);
+    VLL2 := AConverter.PixelPos2LonLat(xy2, AZoom);
+    xy.Y := (xy2.y - ((xy2.Y - xy1.Y) div 2));
+    xy.X := (xy2.x - ((xy2.x - xy1.x) div 2));
+    VLL := AConverter.PixelPos2LonLat(xy, AZoom);
 
-  lon[1] := VLL1.X;
-  lat[1] := VLL1.Y;
-  lon[2] := VLL.X;
-  lat[2] := VLL.Y;
-  lon[3] := VLL2.X;
-  lat[3] := VLL2.Y;
+    lon[1] := VLL1.X;
+    lat[1] := VLL1.Y;
+    lon[2] := VLL.X;
+    lat[2] := VLL.Y;
+    lon[3] := VLL2.X;
+    lat[3] := VLL2.Y;
 
-  VLocalRect.TopLeft := Point(0, 0);
-  VLocalRect.BottomRight := Point(xy2.X - xy1.X, xy2.y - xy1.y);
+    VLocalRect.TopLeft := Point(0, 0);
+    VLocalRect.BottomRight := Point(xy2.X - xy1.X, xy2.y - xy1.y);
 
-  writeln(f, '(' + R2StrPoint(lon[1]) + ',' + R2StrPoint(lat[1]) + ') (' + inttostr(VLocalRect.Left) + ', ' + inttostr(VLocalRect.Top) + ') Label "Точка 1",');
-  writeln(f, '(' + R2StrPoint(lon[3]) + ',' + R2StrPoint(lat[3]) + ') (' + inttostr(VLocalRect.Right) + ', ' + inttostr(VLocalRect.Bottom) + ') Label "Точка 2",');
-  writeln(f, '(' + R2StrPoint(lon[1]) + ',' + R2StrPoint(lat[3]) + ') (' + inttostr(VLocalRect.Left) + ', ' + inttostr(VLocalRect.Bottom) + ') Label "Точка 3",');
-  writeln(f, '(' + R2StrPoint(lon[3]) + ',' + R2StrPoint(lat[1]) + ') (' + inttostr(VLocalRect.Right) + ', ' + inttostr(VLocalRect.Top) + ') Label "Точка 4",');
-  writeln(f, '(' + R2StrPoint(lon[2]) + ',' + R2StrPoint(lat[2]) + ') (' + inttostr((VLocalRect.Right - VLocalRect.Left) div 2) + ', ' + inttostr((VLocalRect.Bottom - VLocalRect.Top) div 2) + ') Label "Точка 5",');
-  writeln(f, '(' + R2StrPoint(lon[2]) + ',' + R2StrPoint(lat[1]) + ') (' + inttostr((VLocalRect.Right - VLocalRect.Left) div 2) + ', ' + inttostr(VLocalRect.Top) + ') Label "Точка 6",');
-  writeln(f, '(' + R2StrPoint(lon[1]) + ',' + R2StrPoint(lat[2]) + ') (' + inttostr(VLocalRect.Left) + ', ' + inttostr((VLocalRect.Bottom - VLocalRect.Top) div 2) + ') Label "Точка 7",');
-  writeln(f, '(' + R2StrPoint(lon[3]) + ',' + R2StrPoint(lat[2]) + ') (' + inttostr(VLocalRect.Right) + ', ' + inttostr((VLocalRect.Bottom - VLocalRect.Top) div 2) + ') Label "Точка 8",');
-  writeln(f, '(' + R2StrPoint(lon[2]) + ',' + R2StrPoint(lat[3]) + ') (' + inttostr((VLocalRect.Right - VLocalRect.Left) div 2) + ', ' + inttostr(VLocalRect.Bottom) + ') Label "Точка 9"');
+    VText := VText + '(' + R2StrPoint(lon[1]) + ',' + R2StrPoint(lat[1]) + ') (' + inttostr(VLocalRect.Left) + ', ' + inttostr(VLocalRect.Top) + ') Label "Точка 1",' + #13#10;
+    VText := VText + '(' + R2StrPoint(lon[3]) + ',' + R2StrPoint(lat[3]) + ') (' + inttostr(VLocalRect.Right) + ', ' + inttostr(VLocalRect.Bottom) + ') Label "Точка 2",' + #13#10;
+    VText := VText + '(' + R2StrPoint(lon[1]) + ',' + R2StrPoint(lat[3]) + ') (' + inttostr(VLocalRect.Left) + ', ' + inttostr(VLocalRect.Bottom) + ') Label "Точка 3",' + #13#10;
+    VText := VText + '(' + R2StrPoint(lon[3]) + ',' + R2StrPoint(lat[1]) + ') (' + inttostr(VLocalRect.Right) + ', ' + inttostr(VLocalRect.Top) + ') Label "Точка 4",' + #13#10;
+    VText := VText + '(' + R2StrPoint(lon[2]) + ',' + R2StrPoint(lat[2]) + ') (' + inttostr((VLocalRect.Right - VLocalRect.Left) div 2) + ', ' + inttostr((VLocalRect.Bottom - VLocalRect.Top) div 2) + ') Label "Точка 5",' + #13#10;
+    VText := VText + '(' + R2StrPoint(lon[2]) + ',' + R2StrPoint(lat[1]) + ') (' + inttostr((VLocalRect.Right - VLocalRect.Left) div 2) + ', ' + inttostr(VLocalRect.Top) + ') Label "Точка 6",' + #13#10;
+    VText := VText + '(' + R2StrPoint(lon[1]) + ',' + R2StrPoint(lat[2]) + ') (' + inttostr(VLocalRect.Left) + ', ' + inttostr((VLocalRect.Bottom - VLocalRect.Top) div 2) + ') Label "Точка 7",' + #13#10;
+    VText := VText + '(' + R2StrPoint(lon[3]) + ',' + R2StrPoint(lat[2]) + ') (' + inttostr(VLocalRect.Right) + ', ' + inttostr((VLocalRect.Bottom - VLocalRect.Top) div 2) + ') Label "Точка 8",' + #13#10;
+    VText := VText + '(' + R2StrPoint(lon[2]) + ',' + R2StrPoint(lat[3]) + ') (' + inttostr((VLocalRect.Right - VLocalRect.Left) div 2) + ', ' + inttostr(VLocalRect.Bottom) + ') Label "Точка 9"' + #13#10;
 
-  writeln(f, 'CoordSys Earth Projection 1, 104');
-  writeln(f, 'Units "degree"');
-  closefile(f);
+    VText := VText + 'CoordSys Earth Projection 1, 104' + #13#10;
+    VText := VText + 'Units "degree"' + #13#10;
+
+    VFileStream.WriteBuffer(VText[1], Length(VText));
+  finally
+    VFileStream.Free;
+  end;
 end;
 
 end.
- 
