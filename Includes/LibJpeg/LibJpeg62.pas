072aef1d00ae94bd34800902557b9479561ac0f9
zed <starmen@tut.by>
2012-08-31 22:44:13 +0300

    Исправления в LibJpeg для совместимости с XE2

--- Includes/LibJpeg/LibJpeg62.pas
+++ Includes/LibJpeg/LibJpeg62.pas
@@ -775,7 +775,7 @@ jpeg_error_mgr = record
   { Routine that actually outputs a trace or error message }
   output_message: procedure(cinfo: j_common_ptr); cdecl;
   { Format a message string for the most recent JPEG error or message }
-  format_message: procedure(cinfo: j_common_ptr; buffer: pAnsichar); cdecl;
+  format_message: procedure(cinfo: j_common_ptr; buffer: PAnsiChar); cdecl;
 //#define JMSG_LENGTH_MAX  200	{ recommended size of format_message buffer }
   { Reset error state variables at start of a new image }
   reset_error_mgr: procedure(cinfo: j_common_ptr); cdecl;
@@ -786,7 +786,7 @@ jpeg_error_mgr = record
   msg_parm: record
     case integer of
       1: (i: array [0..7] of integer);
-      2: (s: array [0..79] of char);
+      2: (s: array [0..79] of AnsiChar);
     end;
   
   { Standard state variables for error facility }
--- Includes/LibJpeg/LibJpegErrorHandler.pas
+++ Includes/LibJpeg/LibJpegErrorHandler.pas
@@ -38,26 +38,26 @@ implementation
 {$IFDEF LIB_JPEG_62_SUPPORT}
 procedure libjpeg_error_exit(cinfo: LibJpeg62.j_common_ptr); cdecl;
 var
-  Msg: String;
+  Msg: AnsiString;
 begin
   SetLength(Msg, 256);
   cinfo^.err^.format_message(cinfo, PAnsiChar(Msg));
   cinfo^.global_state := 0;
   LibJpeg62.jpeg_abort(cinfo);
   raise ELibJpegException.Create(
-    'ERROR [' + IntToStr(cinfo^.err^.msg_code) + '] ' + PAnsiChar(Msg)
+    'ERROR [' + IntToStr(cinfo^.err^.msg_code) + '] ' + string(PAnsiChar(Msg))
   );
 end;
 
 procedure libjpeg_output_message(cinfo: LibJpeg62.j_common_ptr); cdecl;
 var
-  Msg: String;
+  Msg: AnsiString;
 begin
   SetLength(Msg, 256);
   cinfo^.err^.format_message(cinfo, PAnsiChar(Msg));
   cinfo^.global_state := 0;
   raise ELibJpegException.Create(
-    'OUTPUT [' + IntToStr(cinfo^.err^.msg_code) + '] ' + PAnsiChar(Msg)
+    'OUTPUT [' + IntToStr(cinfo^.err^.msg_code) + '] ' + string(PAnsiChar(Msg))
   );
 end;
 {$ENDIF}
@@ -65,26 +65,26 @@ end;
 {$IFDEF LIB_JPEG_8_SUPPORT}
 procedure libjpeg_error_exit(cinfo: LibJpeg8.j_common_ptr); cdecl;
 var
-  Msg: String;
+  Msg: AnsiString;
 begin
   SetLength(Msg, 256);
   cinfo^.err^.format_message(cinfo, PAnsiChar(Msg));
   cinfo^.global_state := 0;
   LibJpeg8.jpeg_abort(cinfo);
   raise ELibJpegException.Create(
-    'ERROR [' + IntToStr(cinfo^.err^.msg_code) + '] ' + PAnsiChar(Msg)
+    'ERROR [' + IntToStr(cinfo^.err^.msg_code) + '] ' + string(PAnsiChar(Msg))
   );
 end;
 
 procedure libjpeg_output_message(cinfo: LibJpeg8.j_common_ptr); cdecl;
 var
-  Msg: String;
+  Msg: AnsiString;
 begin
   SetLength(Msg, 256);
   cinfo^.err^.format_message(cinfo, PAnsiChar(Msg));
   cinfo^.global_state := 0;
   raise ELibJpegException.Create(
-    'OUTPUT [' + IntToStr(cinfo^.err^.msg_code) + '] ' + PAnsiChar(Msg)
+    'OUTPUT [' + IntToStr(cinfo^.err^.msg_code) + '] ' + string(PAnsiChar(Msg))
   );
 end;
 {$ENDIF}
