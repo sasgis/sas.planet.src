7a12d714649145ddeb23f1cba913e87f8a6899ee
Viktor Demydov <vdemidov@gmail.com>
2012-11-24 14:25:22 +0200

    Ковыряю фабрики синхропримитивов

--- Syncronize/u_ReadWriteSyncAbstract.pas
+++ Syncronize/u_ReadWriteSyncAbstract.pas
@@ -10,7 +10,7 @@ uses
 type
   TReadWriteSyncAbstract = class(TInterfacedObject)
   private
-    FName: ShortString;
+    FName: AnsiString;
   public
     constructor Create(const AName: AnsiString);
   end;
@@ -18,12 +18,10 @@ type
   TReadWriteSyncDebugWrapper = class(TInterfacedObject, IReadWriteSync)
   private
     FLock: IReadWriteSync;
-    FLockClassName: ShortString;
+    FLockClassName: AnsiString;
     FName: ShortString;
-    FReadCounter: IInternalPerformanceCounter;
-    FWriteCounter: IInternalPerformanceCounter;
   protected
-    procedure DoDebugGlobalLocks(const AProcedure, AEvent: String);
+    procedure DoDebugGlobalLocks(const AProcedure, AEvent: AnsiString);
   private
     procedure BeginRead;
     procedure EndRead;
@@ -33,7 +31,23 @@ type
     constructor Create(
       const ALock: IReadWriteSync;
       const ALockClassName: AnsiString;
-      const AName: AnsiString;
+      const AName: AnsiString
+    );
+  end;
+
+  TReadWriteSyncCounterWrapper = class(TInterfacedObject, IReadWriteSync)
+  private
+    FLock: IReadWriteSync;
+    FReadCounter: IInternalPerformanceCounter;
+    FWriteCounter: IInternalPerformanceCounter;
+  private
+    procedure BeginRead;
+    procedure EndRead;
+    function BeginWrite: Boolean;
+    procedure EndWrite;
+  public
+    constructor Create(
+      const ALock: IReadWriteSync;
       const AReadCounter: IInternalPerformanceCounter;
       const AWriteCounter: IInternalPerformanceCounter
     );
@@ -51,6 +65,48 @@ type
   private
     function Make(const AName: AnsiString): IReadWriteSync;
   end;
+
+  TSynchronizerFactoryWithDebug = class(TInterfacedObject, IReadWriteSyncFactory)
+  private
+    FFactory: IReadWriteSyncFactory;
+    FLockClassName: AnsiString;
+  private
+    function Make(const AName: AnsiString): IReadWriteSync;
+  public
+    constructor Create(
+      const AFactory: IReadWriteSyncFactory;
+      const ALockClassName: AnsiString
+    );
+  end;
+
+  TSynchronizerFactoryWithCounters = class(TInterfacedObject, IReadWriteSyncFactory)
+  private
+    FFactory: IReadWriteSyncFactory;
+    FReadCounter: IInternalPerformanceCounter;
+    FWriteCounter: IInternalPerformanceCounter;
+  private
+    function Make(const AName: AnsiString): IReadWriteSync;
+  public
+    constructor Create(
+      const AFactory: IReadWriteSyncFactory;
+      const AReadCounter: IInternalPerformanceCounter;
+      const AWriteCounter: IInternalPerformanceCounter
+    );
+  end;
+
+  TSynchronizerFactoryWithMakeCounter = class(TInterfacedObject, IReadWriteSyncFactory)
+  private
+    FFactory: IReadWriteSyncFactory;
+    FCounter: IInternalPerformanceCounter;
+  private
+    function Make(const AName: AnsiString): IReadWriteSync;
+  public
+    constructor Create(
+      const AFactory: IReadWriteSyncFactory;
+      const ACounter: IInternalPerformanceCounter
+    );
+  end;
+
 
 var
   DebugGlobalLocks_Enabled: Boolean;
@@ -103,8 +159,7 @@ end;
 constructor TReadWriteSyncDebugWrapper.Create(
   const ALock: IReadWriteSync;
   const ALockClassName: AnsiString;
-  const AName: AnsiString;
-  const AReadCounter, AWriteCounter: IInternalPerformanceCounter
+  const AName: AnsiString
 );
 begin
   Assert(ALock <> nil);
@@ -112,41 +167,19 @@ begin
   FLock := ALock;
   FLockClassName := ALockClassName;
   FName := AName;
-  FReadCounter := AReadCounter;
-  FWriteCounter := AWriteCounter;
 end;
 
 procedure TReadWriteSyncDebugWrapper.BeginRead;
-var
-  VCounter: Pointer;
-  VContext: TInternalPerformanceCounterContext;
 begin
   DoDebugGlobalLocks('BeginRead', 'IN');
-  VCounter := Pointer(FReadCounter);
-  if VCounter <> nil then begin
-    VContext := IInternalPerformanceCounter(VCounter).StartOperation;
-    FLock.BeginRead;
-    IInternalPerformanceCounter(VCounter).FinishOperation(VContext);
-  end else begin
-    FLock.BeginRead;
-  end;
+  FLock.BeginRead;
   DoDebugGlobalLocks('BeginRead', 'OUT');
 end;
 
 function TReadWriteSyncDebugWrapper.BeginWrite: Boolean;
-var
-  VCounter: Pointer;
-  VContext: TInternalPerformanceCounterContext;
 begin
   DoDebugGlobalLocks('BeginWrite', 'IN');
-  VCounter := Pointer(FWriteCounter);
-  if VCounter <> nil then begin
-    VContext := IInternalPerformanceCounter(VCounter).StartOperation;
-    Result := FLock.BeginWrite;
-    IInternalPerformanceCounter(VCounter).FinishOperation(VContext);
-  end else begin
-    Result := FLock.BeginWrite;
-  end;
+  Result := FLock.BeginWrite;
   DoDebugGlobalLocks('BeginWrite', 'OUT');
 end;
 
@@ -175,6 +208,135 @@ begin
   DoDebugGlobalLocks('EndWrite', 'IN');
   FLock.EndWrite;
   DoDebugGlobalLocks('EndWrite', 'OUT');
+end;
+
+{ TReadWriteSyncCounterWrapper }
+
+constructor TReadWriteSyncCounterWrapper.Create(
+  const ALock: IReadWriteSync;
+  const AReadCounter, AWriteCounter: IInternalPerformanceCounter
+);
+begin
+  Assert(ALock <> nil);
+  inherited Create;
+  FLock := ALock;
+  FReadCounter := AReadCounter;
+  FWriteCounter := AWriteCounter;
+end;
+
+procedure TReadWriteSyncCounterWrapper.BeginRead;
+var
+  VCounter: Pointer;
+  VContext: TInternalPerformanceCounterContext;
+begin
+  VCounter := Pointer(FReadCounter);
+  if VCounter <> nil then begin
+    VContext := IInternalPerformanceCounter(VCounter).StartOperation;
+    FLock.BeginRead;
+    IInternalPerformanceCounter(VCounter).FinishOperation(VContext);
+  end else begin
+    FLock.BeginRead;
+  end;
+end;
+
+function TReadWriteSyncCounterWrapper.BeginWrite: Boolean;
+var
+  VCounter: Pointer;
+  VContext: TInternalPerformanceCounterContext;
+begin
+  VCounter := Pointer(FWriteCounter);
+  if VCounter <> nil then begin
+    VContext := IInternalPerformanceCounter(VCounter).StartOperation;
+    Result := FLock.BeginWrite;
+    IInternalPerformanceCounter(VCounter).FinishOperation(VContext);
+  end else begin
+    Result := FLock.BeginWrite;
+  end;
+end;
+
+procedure TReadWriteSyncCounterWrapper.EndRead;
+begin
+  FLock.EndRead;
+end;
+
+procedure TReadWriteSyncCounterWrapper.EndWrite;
+begin
+  FLock.EndWrite;
+end;
+
+{ TSynchronizerFactoryWithDebug }
+
+constructor TSynchronizerFactoryWithDebug.Create(
+  const AFactory: IReadWriteSyncFactory; const ALockClassName: AnsiString);
+begin
+  Assert(AFactory <> nil);
+  inherited Create;
+  FFactory := AFactory;
+  FLockClassName := ALockClassName;
+end;
+
+function TSynchronizerFactoryWithDebug.Make(
+  const AName: AnsiString
+): IReadWriteSync;
+begin
+  Result :=
+    TReadWriteSyncDebugWrapper.Create(
+      FFactory.Make(AName),
+      FLockClassName,
+      AName
+    );
+end;
+
+{ TSynchronizerFactoryWithCounters }
+
+constructor TSynchronizerFactoryWithCounters.Create(
+  const AFactory: IReadWriteSyncFactory;
+  const AReadCounter, AWriteCounter: IInternalPerformanceCounter
+);
+begin
+  Assert(AFactory <> nil);
+  inherited Create;
+  FFactory := AFactory;
+  FReadCounter := AReadCounter;
+  FWriteCounter := AWriteCounter;
+end;
+
+function TSynchronizerFactoryWithCounters.Make(
+  const AName: AnsiString): IReadWriteSync;
+begin
+  Result :=
+    TReadWriteSyncCounterWrapper.Create(
+      FFactory.Make(AName),
+      FReadCounter,
+      FWriteCounter
+    );
+end;
+
+{ TSynchronizerFactoryWithMakeCounter }
+
+constructor TSynchronizerFactoryWithMakeCounter.Create(
+  const AFactory: IReadWriteSyncFactory;
+  const ACounter: IInternalPerformanceCounter
+);
+begin
+  Assert(AFactory <> nil);
+  Assert(ACounter <> nil);
+  inherited Create;
+  FFactory := AFactory;
+  FCounter := ACounter;
+end;
+
+function TSynchronizerFactoryWithMakeCounter.Make(
+  const AName: AnsiString): IReadWriteSync;
+var
+  VContext: TInternalPerformanceCounterContext;
+begin
+  VContext := FCounter.StartOperation;
+  try
+    Result := FFactory.Make(AName);
+  finally
+    FCounter.FinishOperation(VContext);
+  end;
 end;
 
 initialization
