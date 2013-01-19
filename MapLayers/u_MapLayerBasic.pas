091a546bf4aec09e6b22318f854179ab3a52453f
Viktor Demydov <vdemidov@gmail.com>
2013-01-19 23:05:34 +0200

    Объединил два класса в один

--- MapLayers/u_MapLayerBasic.pas
+++ MapLayers/u_MapLayerBasic.pas
@@ -14,7 +14,7 @@ uses
   u_WindowLayerBasic;
 
 type
-  TWindowLayerBasic = class(TWindowLayerAbstract)
+  TMapLayerBasicNoBitmap = class(TWindowLayerAbstract)
   private
     FView: ILocalCoordConverterChangeable;
     FVisible: Boolean;
@@ -22,6 +22,12 @@ type
 
     FNeedRedrawFlag: ISimpleFlag;
     FRedrawCounter: IInternalPerformanceCounter;
+    FOnPaintCounter: IInternalPerformanceCounter;
+    procedure OnPaintLayer(
+      Sender: TObject;
+      Buffer: TBitmap32
+    );
+    procedure OnViewChange;
   protected
     property View: ILocalCoordConverterChangeable read FView;
     function GetVisible: Boolean; virtual;
@@ -33,41 +39,20 @@ type
     procedure DoShow; virtual;
     procedure Hide;
     procedure DoHide; virtual;
-    procedure DoRedraw; virtual; abstract;
+    procedure DoRedraw; virtual;
     procedure RedrawIfNeed;
 
-    property Layer: TCustomLayer read FLayer;
-    property Visible: Boolean read GetVisible write SetVisible;
-  protected
-    procedure DoViewUpdate; override;
-    procedure Redraw;
-  public
-    constructor Create(
-      const APerfList: IInternalPerformanceCounterList;
-      const AAppStartedNotifier: INotifierOneOperation;
-      const AAppClosingNotifier: INotifierOneOperation;
-      ALayer: TCustomLayer;
-      const AView: ILocalCoordConverterChangeable
-    );
-    destructor Destroy; override;
-  end;
-
-  TMapLayerBasicNoBitmap = class(TWindowLayerBasic)
-  private
-    FOnPaintCounter: IInternalPerformanceCounter;
-    procedure OnPaintLayer(
-      Sender: TObject;
-      Buffer: TBitmap32
-    );
-    procedure OnViewChange;
-  protected
     procedure PaintLayer(
       ABuffer: TBitmap32;
       const ALocalConverter: ILocalCoordConverter
     ); virtual; abstract;
+
+    property Layer: TCustomLayer read FLayer;
+    property Visible: Boolean read GetVisible write SetVisible;
   protected
-    procedure DoRedraw; override;
     procedure StartThreads; override;
+    procedure DoViewUpdate; override;
+    procedure Redraw;
   public
     constructor Create(
       const APerfList: IInternalPerformanceCounterList;
@@ -76,6 +61,7 @@ type
       AParentMap: TImage32;
       const AView: ILocalCoordConverterChangeable
     );
+    destructor Destroy; override;
   end;
 
 implementation
@@ -83,123 +69,6 @@ implementation
 uses
   u_SimpleFlagWithInterlock,
   u_ListenerByEvent;
-
-{ TWindowLayerBasic }
-
-constructor TWindowLayerBasic.Create(
-  const APerfList: IInternalPerformanceCounterList;
-  const AAppStartedNotifier: INotifierOneOperation;
-  const AAppClosingNotifier: INotifierOneOperation;
-  ALayer: TCustomLayer;
-  const AView: ILocalCoordConverterChangeable
-);
-begin
-  inherited Create(
-    APerfList,
-    AAppStartedNotifier,
-    AAppClosingNotifier
-  );
-
-  FView := AView;
-  FLayer := ALayer;
-  FRedrawCounter := PerfList.CreateAndAddNewCounter('Redraw');
-
-  FLayer.MouseEvents := false;
-  FLayer.Visible := false;
-  FVisible := False;
-  FNeedRedrawFlag := TSimpleFlagWithInterlock.Create;
-end;
-
-destructor TWindowLayerBasic.Destroy;
-begin
-  FLayer := nil;
-  inherited;
-end;
-
-procedure TWindowLayerBasic.DoViewUpdate;
-begin
-  inherited;
-  RedrawIfNeed;
-end;
-
-procedure TWindowLayerBasic.Hide;
-begin
-  ViewUpdateLock;
-  try
-    if FVisible then begin
-      DoHide;
-    end;
-  finally
-    ViewUpdateUnlock;
-  end;
-end;
-
-procedure TWindowLayerBasic.DoHide;
-begin
-  FVisible := False;
-  FLayer.Visible := False;
-  SetNeedRedraw;
-end;
-
-procedure TWindowLayerBasic.Show;
-begin
-  ViewUpdateLock;
-  try
-    if not Visible then begin
-      DoShow;
-    end;
-  finally
-    ViewUpdateUnlock;
-  end;
-end;
-
-procedure TWindowLayerBasic.DoShow;
-begin
-  FVisible := True;
-  FLayer.Visible := True;
-  SetNeedRedraw;
-end;
-
-function TWindowLayerBasic.GetVisible: Boolean;
-begin
-  Result := FVisible;
-end;
-
-procedure TWindowLayerBasic.Redraw;
-var
-  VCounterContext: TInternalPerformanceCounterContext;
-begin
-  if FVisible then begin
-    FNeedRedrawFlag.CheckFlagAndReset;
-    VCounterContext := FRedrawCounter.StartOperation;
-    try
-      DoRedraw;
-    finally
-      FRedrawCounter.FinishOperation(VCounterContext);
-    end;
-  end;
-end;
-
-procedure TWindowLayerBasic.RedrawIfNeed;
-begin
-  if FNeedRedrawFlag.CheckFlagAndReset then begin
-    Redraw;
-  end;
-end;
-
-procedure TWindowLayerBasic.SetNeedRedraw;
-begin
-  FNeedRedrawFlag.SetFlag;
-end;
-
-procedure TWindowLayerBasic.SetVisible(const Value: Boolean);
-begin
-  if Value then begin
-    Show;
-  end else begin
-    Hide;
-  end;
-end;
 
 { TMapLayerBasicNoBitmap }
 
@@ -214,10 +83,17 @@ begin
   inherited Create(
     APerfList,
     AAppStartedNotifier,
-    AAppClosingNotifier,
-    TCustomLayer.Create(AParentMap.Layers),
-    AView
+    AAppClosingNotifier
   );
+
+  FView := AView;
+  FLayer := TCustomLayer.Create(AParentMap.Layers);
+  FRedrawCounter := PerfList.CreateAndAddNewCounter('Redraw');
+
+  FLayer.MouseEvents := false;
+  FLayer.Visible := false;
+  FVisible := False;
+  FNeedRedrawFlag := TSimpleFlagWithInterlock.Create;
   FOnPaintCounter := PerfList.CreateAndAddNewCounter('OnPaint');
   LinksList.Add(
     TNotifyNoMmgEventListener.Create(Self.OnViewChange),
@@ -229,6 +105,12 @@ procedure TMapLayerBasicNoBitmap.DoRedraw;
 begin
   inherited;
   Layer.Changed;
+end;
+
+procedure TMapLayerBasicNoBitmap.DoViewUpdate;
+begin
+  inherited;
+  RedrawIfNeed;
 end;
 
 procedure TMapLayerBasicNoBitmap.OnPaintLayer(
@@ -259,6 +141,91 @@ procedure TMapLayerBasicNoBitmap.StartThreads;
 begin
   inherited;
   Layer.OnPaint := OnPaintLayer;
+end;
+
+procedure TMapLayerBasicNoBitmap.Hide;
+begin
+  ViewUpdateLock;
+  try
+    if FVisible then begin
+      DoHide;
+    end;
+  finally
+    ViewUpdateUnlock;
+  end;
+end;
+
+destructor TMapLayerBasicNoBitmap.Destroy;
+begin
+  FLayer := nil;
+  inherited;
+end;
+
+procedure TMapLayerBasicNoBitmap.DoHide;
+begin
+  FVisible := False;
+  FLayer.Visible := False;
+  SetNeedRedraw;
+end;
+
+procedure TMapLayerBasicNoBitmap.Show;
+begin
+  ViewUpdateLock;
+  try
+    if not Visible then begin
+      DoShow;
+    end;
+  finally
+    ViewUpdateUnlock;
+  end;
+end;
+
+procedure TMapLayerBasicNoBitmap.DoShow;
+begin
+  FVisible := True;
+  FLayer.Visible := True;
+  SetNeedRedraw;
+end;
+
+function TMapLayerBasicNoBitmap.GetVisible: Boolean;
+begin
+  Result := FVisible;
+end;
+
+procedure TMapLayerBasicNoBitmap.Redraw;
+var
+  VCounterContext: TInternalPerformanceCounterContext;
+begin
+  if FVisible then begin
+    FNeedRedrawFlag.CheckFlagAndReset;
+    VCounterContext := FRedrawCounter.StartOperation;
+    try
+      DoRedraw;
+    finally
+      FRedrawCounter.FinishOperation(VCounterContext);
+    end;
+  end;
+end;
+
+procedure TMapLayerBasicNoBitmap.RedrawIfNeed;
+begin
+  if FNeedRedrawFlag.CheckFlagAndReset then begin
+    Redraw;
+  end;
+end;
+
+procedure TMapLayerBasicNoBitmap.SetNeedRedraw;
+begin
+  FNeedRedrawFlag.SetFlag;
+end;
+
+procedure TMapLayerBasicNoBitmap.SetVisible(const Value: Boolean);
+begin
+  if Value then begin
+    Show;
+  end else begin
+    Hide;
+  end;
 end;
 
 end.
