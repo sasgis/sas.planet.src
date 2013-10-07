c739279d9a6b5b583597fecf8817b846eb22b973
zed <starmen@tut.by>
2013-10-07 21:13:54 +0300

    2189: Навёл порядок в коде и уменьшил излишнюю точность при сохранении координат

--- u_MapCalibrationOzi.pas
+++ u_MapCalibrationOzi.pas
@@ -1,6 +1,6 @@
 {******************************************************************************}
 {* SAS.Planet (SAS.Планета)                                                   *}
-{* Copyright (C) 2007-2012, SAS.Planet development team.                      *}
+{* Copyright (C) 2007-2013, SAS.Planet development team.                      *}
 {* This program is free software: you can redistribute it and/or modify       *}
 {* it under the terms of the GNU General Public License as published by       *}
 {* the Free Software Foundation, either version 3 of the License, or          *}
@@ -24,6 +24,7 @@ interface
 
 uses
   Types,
+  ALfcnString,
   i_CoordConverter,
   i_MapCalibration,
   u_BaseInterfacedObject;
@@ -31,17 +32,18 @@ uses
 type
   TMapCalibrationOzi = class(TBaseInterfacedObject, IMapCalibration)
   private
-    // Имя для вывода в листбоксе для выбора при экспорте.
+    FFormatSettings: TALFormatSettings;
+  private
     function GetName: WideString; safecall;
-    // Более детальное описание привязки
     function GetDescription: WideString; safecall;
-    // Генерирует привязку для склеенной карты.
     procedure SaveCalibrationInfo(
       const AFileName: WideString;
-      const xy1, xy2: TPoint;
-      AZoom: byte;
+      const XY1, XY2: TPoint;
+      AZoom: Byte;
       const AConverter: ICoordConverter
     ); safecall;
+  public
+    constructor Create;
   end;
 
 implementation
@@ -49,49 +51,115 @@ implementation
 uses
   Classes,
   SysUtils,
-  ALfcnString,
   t_GeoTypes,
-  u_GeoToStr;
+  c_CoordConverter;
+
+const
+  cOziFileExt = '.map';
+  cCalibrationStrFormat: AnsiString = 'Point%.2d,xy,    %d, %d,in, deg, %s, %s, grid,   ,           ,           ,N' + #13#10;
+  cCalibrationEmptyStrFormat: AnsiString = 'Point%.2d,xy,     ,     ,in, deg,    ,        ,N,    ,        ,W, grid,   ,           ,           ,N' + #13#10;
+
+resourcestring
+  rsOziMapCalibrationDescription = 'Calibration for OziExplorer programm (*.map)';
 
 { TMapCalibrationOzi }
 
+constructor TMapCalibrationOzi.Create;
+begin
+  inherited Create;
+  FFormatSettings.DecimalSeparator := '.';
+end;
+
 function TMapCalibrationOzi.GetDescription: WideString;
 begin
-  Result := 'Привязка *.map для программы OziExplorer';
+  Result := rsOziMapCalibrationDescription;
 end;
 
 function TMapCalibrationOzi.GetName: WideString;
 begin
-  Result := '.map';
+  Result := cOziFileExt;
 end;
 
 procedure TMapCalibrationOzi.SaveCalibrationInfo(
   const AFileName: WideString;
-  const xy1, xy2: TPoint;
-  AZoom: byte;
+  const XY1, XY2: TPoint;
+  AZoom: Byte;
   const AConverter: ICoordConverter
 );
+
+  function GetDegrees(const ACoord: Double): AnsiString;
+  begin
+    Result := ALIntToStr(Trunc(Abs(ACoord)));
+  end;
+
+  function GetMinutes(const ACoord: Double): AnsiString;
+  begin
+     Result := ALFormat('%.4f', [Frac(Abs(ACoord)) * 60], FFormatSettings);
+  end;
+
+  function DoubleToAnsiStr(const AValue: Double): AnsiString;
+  begin
+    Result := ALFormat('%.6f', [AValue], FFormatSettings);
+  end;
+
+  function GetPointCalibrationStr(const APointID, X, Y: Integer; const ALon, ALat: Double): AnsiString;
+  var
+    VLonStr, VLatStr: AnsiString;
+  begin
+    VLonStr := GetDegrees(ALon) + ', ' + GetMinutes(ALon);
+    if ALon < 0 then begin
+      VLonStr := VLonStr + ',W';
+    end else begin
+      VLonStr := VLonStr + ',E';
+    end;
+
+    VLatStr := GetDegrees(ALat) + ', ' + GetMinutes(ALat);
+    if ALat < 0 then begin
+      VLatStr := VLatStr + ',S';
+    end else begin
+      VLatStr := VLatStr + ',N';
+    end;
+
+    Result := ALFormat(cCalibrationStrFormat, [APointID, X, Y, VLatStr, VLonStr], FFormatSettings);
+  end;
+
 const
-  D2R: Double = 0.017453292519943295769236907684886;// Константа для преобразования градусов в радианы
+  cDegreeToRadCoeff: Double = 0.017453292519943295769236907684886;
 var
-  xy: TPoint;
-  rad: Double;
-  lat, lon: array[1..3] of Double;
-  i: integer;
-  lats, lons: array[1..3] of AnsiString;
-  VFileName: String;
-  VLL1, VLL2: TDoublePoint;
-  VLL: TDoublePoint;
+  I: Integer;
+  VXY: TPoint;
+  VRadius: Double;
+  VFileName: string;
+  VLL, VLL1, VLL2: TDoublePoint;
   VLocalRect: TRect;
   VFileStream: TFileStream;
   VText: AnsiString;
   VMapName: AnsiString;
+  VProjection: AnsiString;
 begin
-  VFileName := ChangeFileExt(AFileName, '.map');
+  VLL1 := AConverter.PixelPos2LonLat(XY1, AZoom);
+  VLL2 := AConverter.PixelPos2LonLat(XY2, AZoom);
+
+  VXY.Y := (XY2.y - ((XY2.Y - XY1.Y) div 2));
+  VXY.X := (XY2.x - ((XY2.x - XY1.x) div 2));
+  VLL := AConverter.PixelPos2LonLat(VXY, AZoom);
+
+  VLocalRect.TopLeft := Point(0, 0);
+  VLocalRect.BottomRight := Point(XY2.X - XY1.X, XY2.y - XY1.y);
+
+  VRadius := AConverter.Datum.GetSpheroidRadiusA;
+
+  if AConverter.ProjectionEPSG = CGELonLatProjectionEPSG then begin
+    VProjection := 'Latitude/Longitude';
+  end else begin
+    VProjection := 'Mercator';
+  end;
+
+  VFileName := ChangeFileExt(AFileName, cOziFileExt);
+  VMapName := UTF8Encode(ExtractFileName(AFileName));
+
   VFileStream := TFileStream.Create(VFileName, fmCreate);
   try
-    VMapName := UTF8Encode(ExtractFileName(AFileName));
-    
     VText :=
       'OziExplorer Map Data File Version 2.2' + #13#10 +
       VMapName + #13#10 +
@@ -101,74 +169,39 @@ begin
       'Reserved 1' + #13#10 +
       'Reserved 2' + #13#10 +
       'Magnetic Variation,,,E' + #13#10 +
-      'Map Projection,Mercator,PolyCal,No,AutoCalOnly,No,BSBUseWPX,No' + #13#10; 
+      'Map Projection,' + VProjection + ',PolyCal,No,AutoCalOnly,No,BSBUseWPX,No' + #13#10 +
+      GetPointCalibrationStr(1, VLocalRect.Left, VLocalRect.Top, VLL1.X, VLL1.Y) +
+      GetPointCalibrationStr(2, ((VLocalRect.Right - VLocalRect.Left) div 2), VLocalRect.Top, VLL.X, VLL1.Y) +
+      GetPointCalibrationStr(3, VLocalRect.Right, VLocalRect.Top, VLL2.X, VLL1.Y) +
+      GetPointCalibrationStr(4, VLocalRect.Left, ((VLocalRect.Bottom - VLocalRect.Top) div 2), VLL1.X, VLL.Y) +
+      GetPointCalibrationStr(5, ((VLocalRect.Right - VLocalRect.Left) div 2), ((VLocalRect.Bottom - VLocalRect.Top) div 2), VLL.X, VLL.Y) +
+      GetPointCalibrationStr(6, VLocalRect.Right, ((VLocalRect.Bottom - VLocalRect.Top) div 2), VLL2.X, VLL.Y) +
+      GetPointCalibrationStr(7, VLocalRect.Left, VLocalRect.Bottom, VLL1.X, VLL2.Y) +
+      GetPointCalibrationStr(8, ((VLocalRect.Right - VLocalRect.Left) div 2), VLocalRect.Bottom, VLL.X, VLL2.Y) +
+      GetPointCalibrationStr(9, VLocalRect.Right, VLocalRect.Bottom, VLL2.X, VLL2.Y);
 
-    VLL1 := AConverter.PixelPos2LonLat(xy1, AZoom);
-    VLL2 := AConverter.PixelPos2LonLat(xy2, AZoom);
-    xy.Y := (xy2.y - ((xy2.Y - xy1.Y) div 2));
-    xy.X := (xy2.x - ((xy2.x - xy1.x) div 2));
-    VLL := AConverter.PixelPos2LonLat(xy, AZoom);
-
-    lon[1] := VLL1.X;
-    lat[1] := VLL1.Y;
-    lon[2] := VLL.X;
-    lat[2] := VLL.Y;
-    lon[3] := VLL2.X;
-    lat[3] := VLL2.Y;
-
-    for i := 1 to 3 do begin
-      lons[i] := ALIntToStr(trunc(abs(lon[i]))) + ', ' + R2AnsiStrPoint(Frac(abs(lon[i])) * 60);
-      if lon[i] < 0 then begin
-        lons[i] := lons[i] + ',W';
-      end else begin
-        lons[i] := lons[i] + ',E';
-      end;
-    end;
-    for i := 1 to 3 do begin
-      lats[i] := ALinttostr(trunc(abs(lat[i]))) + ', ' + R2AnsiStrPoint(Frac(abs(lat[i])) * 60);
-      if lat[i] < 0 then begin
-        lats[i] := lats[i] + ',S';
-      end else begin
-        lats[i] := lats[i] + ',N';
-      end;
-    end;
-    VLocalRect.TopLeft := Point(0, 0);
-    VLocalRect.BottomRight := Point(xy2.X - xy1.X, xy2.y - xy1.y);
-
-    VText := VText + 'Point01,xy,    ' + ALinttostr(VLocalRect.Left) + ', ' + ALinttostr(VLocalRect.Top) + ',in, deg, ' + lats[1] + ', ' + lons[1] + ', grid,   ,           ,           ,N' + #13#10;
-    VText := VText + 'Point02,xy,    ' + ALinttostr(VLocalRect.Right) + ', ' + ALinttostr(VLocalRect.Bottom) + ',in, deg, ' + lats[3] + ', ' + lons[3] + ', grid,   ,           ,           ,N' + #13#10;
-    VText := VText + 'Point03,xy,    ' + ALinttostr(VLocalRect.Left) + ', ' + ALinttostr(VLocalRect.Bottom) + ',in, deg, ' + lats[3] + ', ' + lons[1] + ', grid,   ,           ,           ,N' + #13#10;
-    VText := VText + 'Point04,xy,    ' + ALinttostr(VLocalRect.Right) + ', ' + ALinttostr(VLocalRect.Top) + ',in, deg, ' + lats[1] + ', ' + lons[3] + ', grid,   ,           ,           ,N' + #13#10;
-    VText := VText + 'Point05,xy,    ' + ALinttostr((VLocalRect.Right - VLocalRect.Left) div 2) + ', ' + ALinttostr((VLocalRect.Bottom - VLocalRect.Top) div 2) + ',in, deg, ' + lats[2] + ', ' + lons[2] + ', grid,   ,           ,           ,N' + #13#10;
-    VText := VText + 'Point06,xy,    ' + ALinttostr((VLocalRect.Right - VLocalRect.Left) div 2) + ', ' + ALinttostr(VLocalRect.Top) + ',in, deg, ' + lats[1] + ', ' + lons[2] + ', grid,   ,           ,           ,N' + #13#10;
-    VText := VText + 'Point07,xy,    ' + ALinttostr(VLocalRect.Left) + ', ' + ALinttostr((VLocalRect.Bottom - VLocalRect.Top) div 2) + ',in, deg, ' + lats[2] + ', ' + lons[1] + ', grid,   ,           ,           ,N' + #13#10;
-    VText := VText + 'Point08,xy,    ' + ALinttostr(VLocalRect.Right) + ', ' + ALinttostr((VLocalRect.Bottom - VLocalRect.Top) div 2) + ',in, deg, ' + lats[2] + ', ' + lons[3] + ', grid,   ,           ,           ,N' + #13#10;
-    VText := VText + 'Point09,xy,    ' + ALinttostr((VLocalRect.Right - VLocalRect.Left) div 2) + ', ' + ALinttostr(VLocalRect.Bottom) + ',in, deg, ' + lats[3] + ', ' + lons[2] + ', grid,   ,           ,           ,N' + #13#10;
-    for i := 10 to 30 do begin
-      VText := VText + 'Point' + ALinttostr(i) + ',xy,     ,     ,in, deg,    ,        ,N,    ,        ,W, grid,   ,           ,           ,N' + #13#10;
+    for I := 10 to 30 do begin
+      VText := VText + ALFormat(cCalibrationEmptyStrFormat, [I], FFormatSettings);
     end;
 
-    VText := VText + 'Projection Setup,,,,,,,,,,' + #13#10;
-    VText := VText + 'Map Feature = MF ; Map Comment = MC     These follow if they exist' + #13#10;
-    VText := VText + 'Track File = TF      These follow if they exist' + #13#10;
-    VText := VText + 'Moving Map Parameters = MM?    These follow if they exist' + #13#10;
-    VText := VText + 'MM0,Yes' + #13#10;
-    VText := VText + 'MMPNUM,4' + #13#10;
-    VText := VText + 'MMPXY,1,' + ALinttostr(VLocalRect.Left) + ',' + ALinttostr(VLocalRect.Top) + #13#10;
-    VText := VText + 'MMPXY,2,' + ALinttostr(VLocalRect.Right) + ',' + ALinttostr(VLocalRect.Top) + #13#10;
-    VText := VText + 'MMPXY,3,' + ALinttostr(VLocalRect.Right) + ',' + ALinttostr(VLocalRect.Bottom) + #13#10;
-    VText := VText + 'MMPXY,4,' + ALinttostr(VLocalRect.Left) + ',' + ALinttostr(VLocalRect.Bottom) + #13#10;
-
-    VText := VText + 'MMPLL,1, ' + R2AnsiStrPoint(lon[1]) + ', ' + R2AnsiStrPoint(lat[1]) + #13#10;
-    VText := VText + 'MMPLL,2, ' + R2AnsiStrPoint(lon[3]) + ', ' + R2AnsiStrPoint(lat[1]) + #13#10;
-    VText := VText + 'MMPLL,3, ' + R2AnsiStrPoint(lon[3]) + ', ' + R2AnsiStrPoint(lat[3]) + #13#10;
-    VText := VText + 'MMPLL,4, ' + R2AnsiStrPoint(lon[1]) + ', ' + R2AnsiStrPoint(lat[3]) + #13#10;
-
-    rad := AConverter.Datum.GetSpheroidRadiusA;
-
-    VText := VText + 'MM1B,' + R2AnsiStrPoint(1 / ((AConverter.PixelsAtZoomFloat(AZoom) / (2 * PI)) / (rad * cos(lat[2] * D2R)))) + #13#10;
-    VText := VText + 'MOP,Map Open Position,0,0' + #13#10;
-    VText := VText + 'IWH,Map Image Width/Height,' + ALinttostr(VLocalRect.Right) + ',' + ALinttostr(VLocalRect.Bottom) + #13#10;
+    VText := VText +
+      'Projection Setup,,,,,,,,,,' + #13#10 +
+      'Map Feature = MF ; Map Comment = MC     These follow if they exist' + #13#10 +
+      'Track File = TF      These follow if they exist' + #13#10 +
+      'Moving Map Parameters = MM?    These follow if they exist' + #13#10 +
+      'MM0,Yes' + #13#10 +
+      'MMPNUM,4' + #13#10 +
+      'MMPXY,1,' + ALIntToStr(VLocalRect.Left) + ',' + ALIntToStr(VLocalRect.Top) + #13#10 +
+      'MMPXY,2,' + ALIntToStr(VLocalRect.Right) + ',' + ALIntToStr(VLocalRect.Top) + #13#10 +
+      'MMPXY,3,' + ALIntToStr(VLocalRect.Right) + ',' + ALIntToStr(VLocalRect.Bottom) + #13#10 +
+      'MMPXY,4,' + ALIntToStr(VLocalRect.Left) + ',' + ALIntToStr(VLocalRect.Bottom) + #13#10 +
+      'MMPLL,1, ' + DoubleToAnsiStr(VLL1.X) + ', ' + DoubleToAnsiStr(VLL1.Y) + #13#10 +
+      'MMPLL,2, ' + DoubleToAnsiStr(VLL2.X) + ', ' + DoubleToAnsiStr(VLL1.Y) + #13#10 +
+      'MMPLL,3, ' + DoubleToAnsiStr(VLL2.X) + ', ' + DoubleToAnsiStr(VLL2.Y) + #13#10 +
+      'MMPLL,4, ' + DoubleToAnsiStr(VLL1.X) + ', ' + DoubleToAnsiStr(VLL2.Y) + #13#10 +
+      'MM1B,' + DoubleToAnsiStr(1 / ((AConverter.PixelsAtZoomFloat(AZoom) / (2 * PI)) / (VRadius * Cos(VLL.Y * cDegreeToRadCoeff)))) + #13#10 +
+      'MOP,Map Open Position,0,0' + #13#10 +
+      'IWH,Map Image Width/Height,' + ALIntToStr(VLocalRect.Right) + ',' + ALIntToStr(VLocalRect.Bottom) + #13#10;
 
     VFileStream.WriteBuffer(VText[1], Length(VText));
   finally
