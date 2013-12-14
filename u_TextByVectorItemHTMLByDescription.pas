07bda98f20b1b1e862427bf39ccabf697ed8877e
Viktor Demydov <vdemidov@gmail.com>
2013-12-14 18:22:50 +0200

    Для меток без описание не выводим ошибку

--- u_TextByVectorItemHTMLByDescription.pas
+++ u_TextByVectorItemHTMLByDescription.pas
@@ -20,18 +20,15 @@ implementation
 function TTextByVectorItemHTMLByDescription.GetText(
   const AItem: IVectorDataItemSimple): string;
 begin
-  Result := AItem.Desc;
-  if Result <> '' then begin
-    Result :=
-      '<html>'#13#10 +
-        '<head>'#13#10 +
-          '<title>' + AItem.GetInfoCaption + '</title>'#13#10 +
-        '</head>'#13#10 +
-        '<body>'#13#10 +
-          Result + #13#10 +
-        '</body>'#13#10 +
-        '</html>';
-  end;
+  Result :=
+    '<html>'#13#10 +
+      '<head>'#13#10 +
+        '<title>' + AItem.GetInfoCaption + '</title>'#13#10 +
+      '</head>'#13#10 +
+      '<body>'#13#10 +
+        AItem.Desc + #13#10 +
+      '</body>'#13#10 +
+      '</html>';
 end;
 
 end.
