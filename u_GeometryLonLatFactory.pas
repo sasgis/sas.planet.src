12095cb2bafbe0882a71eae88c138170b2dd5fac
Viktor Demydov <vdemidov@gmail.com>
2013-12-15 14:38:40 +0200

    Проба использования построителя мульти-линий

--- u_GeometryLonLatFactory.pas
+++ u_GeometryLonLatFactory.pas
@@ -450,39 +450,19 @@ var
   i: Integer;
   VStart: PDoublePointArray;
   VLineLen: Integer;
-  VLineCount: Integer;
-  VList: IInterfaceListSimple;
   VPoint: TDoublePoint;
   VLineBounds: TDoubleRect;
-  VBounds: TDoubleRect;
-  VRect: ILonLatRect;
-  VLinesetHash: THashValue;
+  VBuilder: IGeometryLonLatMultiLineBuilder;
 begin
-  VLineCount := 0;
+  VBuilder := MakeGeometryLonLatMultiLineBuilder;
   VStart := APoints;
   VLineLen := 0;
-  VLinesetHash := 0;
   for i := 0 to ACount - 1 do begin
     VPoint := APoints[i];
     if PointIsEmpty(VPoint) then begin
       if VLineLen > 0 then begin
-        if VLineCount > 0 then begin
-          if VLineCount = 1 then begin
-            VList := TInterfaceListSimple.Create;
-            VLinesetHash := VLine.Hash;
-          end else begin
-            FHashFunction.UpdateHashByHash(VLinesetHash, VLine.Hash);
-          end;
-          VList.Add(VLine);
-          VLine := nil;
-        end;
         VLine := CreateLonLatLineInternal(VLineBounds, VStart, VLineLen);
-        if VLineCount > 0 then begin
-          VBounds := UnionLonLatRects(VBounds, VLineBounds);
-        end else begin
-          VBounds := VLineBounds;
-        end;
-        Inc(VLineCount);
+        VBuilder.Add(VLine);
         VLineLen := 0;
       end;
     end else begin
@@ -508,34 +488,10 @@ begin
     end;
   end;
   if VLineLen > 0 then begin
-    if VLineCount > 0 then begin
-      if VLineCount = 1 then begin
-        VList := TInterfaceListSimple.Create;
-        VLinesetHash := VLine.Hash;
-      end else begin
-        FHashFunction.UpdateHashByHash(VLinesetHash, VLine.Hash);
-      end;
-      VList.Add(VLine);
-      VLine := nil;
-    end;
     VLine := CreateLonLatLineInternal(VLineBounds, VStart, VLineLen);
-    if VLineCount > 0 then begin
-      VBounds := UnionLonLatRects(VBounds, VLineBounds);
-    end else begin
-      VBounds := VLineBounds;
-    end;
-    Inc(VLineCount);
+    VBuilder.Add(VLine);
   end;
-  if VLineCount = 0 then begin
-    Result := FEmptyLonLatPath;
-  end else if VLineCount = 1 then begin
-    Result := TLonLatPathOneLine.Create(VLine);
-  end else begin
-    VList.Add(VLine);
-    FHashFunction.UpdateHashByHash(VLinesetHash, VLine.Hash);
-    VRect := TLonLatRect.Create(VBounds);
-    Result := TGeometryLonLatMultiLine.Create(VRect, VLinesetHash, VList.MakeStaticAndClear);
-  end;
+  Result := VBuilder.MakeStaticAndClear;
 end;
 
 function TGeometryLonLatFactory.CreateLonLatMultiLineByEnum(
