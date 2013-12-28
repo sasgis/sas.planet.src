9a3ccf4147350f2e4c519b4d1e26662b1797be11
zed <starmen@tut.by>
2013-12-28 18:46:48 +0300

    2293: Поиск через официальный API Яндекс.Карт

--- GeoCoder/u_GeoCoderByYandex.pas
+++ GeoCoder/u_GeoCoderByYandex.pas
@@ -53,6 +53,7 @@ implementation
 uses
   SysUtils,
   ALfcnString,
+  superobject,
   t_GeoTypes,
   i_GeoCoder,
   i_CoordConverter,
@@ -70,179 +71,96 @@ function TGeoCoderByYandex.ParseResultToPlacemarksList(
   const ASearch: WideString;
   const ALocalConverter: ILocalCoordConverter
 ): IInterfaceListSimple;
+
+  function _PosToPoint(const APos: AnsiString): TDoublePoint; // "27.560573 53.865552"
+  var
+    I: Integer;
+    VLat, VLon: AnsiString;
+    VFormatSettings: TALFormatSettings;
+  begin
+    I := ALPos(' ', APos);
+    if I > 0 then begin
+      VLon := ALCopyStr(APos, 0, I - 1);
+      VLat := ALCopyStr(APos, I + 1, Length(APos) - I);
+    end else begin
+      raise EParserError.CreateFmt('Can''t parse coordinates from string: "%s"', [APos]);
+    end;
+    VFormatSettings.DecimalSeparator := '.';
+    try
+      Result.Y := ALStrToFloat(VLat, VFormatSettings);
+      Result.X := ALStrToFloat(VLon, VFormatSettings);
+    except
+      raise EParserError.CreateFmt(SAS_ERR_CoordParseError, [VLat, VLon]);
+    end;
+  end;
+
 var
-  slat, slon: AnsiString;
-  sname, sdesc, sfulldesc: string;
-  i, j: integer;
+  I: Integer;
+  VTmpBuf: UTF8String;
   VPoint: TDoublePoint;
   VPlace: IVectorDataItemPoint;
   VList: IInterfaceListSimple;
-  VFormatSettings: TALFormatSettings;
-  CurPos:integer;// позиция текущего символа
-  BrLevel:integer;//глубина вложенности {=плюс один   }=минус один
-  Buffer:AnsiString; // сюда складываем временно считанную строку
-  CurChar:AnsiString; // текущий символ
-  err:boolean;// признак окончания поиска когда уже всё нашли...
-  ParseErr:boolean; // признак ошибки разбора строки
-  Vstr2Find: AnsiString;
+  VJsonObject: ISuperObject;
+  VJsonArray: TSuperArray;
+  VResultItem: ISuperObject;
+  VName, VDescription: string;
 begin
-  Buffer := '';
-  BrLevel := 0;
-  err := false;
   if AResult.Data.Size <= 0 then begin
     raise EParserError.Create(SAS_ERR_EmptyServerResponse);
   end;
-  VFormatSettings.DecimalSeparator := '.';
+
   VList := TInterfaceListSimple.Create;
-  SetLength(Vstr2Find, AResult.Data.Size);
-  Move(AResult.Data.Buffer^, Vstr2Find[1], AResult.Data.Size);
-  Vstr2Find := ALStringReplace(Vstr2Find,'\/','/', [rfReplaceAll]); // разделители
-  Vstr2Find := ALStringReplace(Vstr2Find,'\"','"', [rfReplaceAll]); // разделители
-  CurPos:=ALPosEx('"features":[', Vstr2Find, 1)-1;
-  while (CurPos<length(Vstr2Find)) and (not err)do begin
-   inc (CurPos);
-   CurChar:=copy(Vstr2Find,CurPos,1);
-   Buffer:=Buffer+CurChar;
 
-   if CurChar='{' then inc(BrLevel);
-   if CurChar='}' then begin
-    dec(BrLevel);
-    if BrLevel=0 then  begin
-   // подстрока готова. теперь будем её парсит и дёргатьнужные нам значения
-     sdesc:='';
-     sname:='';
-     sfulldesc:='';
-     ParseErr:=False;
-     j:=1;
-     // если есть адресная информация - добавляем её
-     // это же является признаком того что точку нужно обрабатывать
-     i := ALPosEx('{"AddressLine":"', Buffer, 1);
-     if i>0  then begin
-      if i>j then begin
-       j := ALPosEx('",', Buffer, i + 16);
-       sdesc:=Utf8ToAnsi(Copy(Buffer, i + 16, j - (i + 16)));
-      end;
-     end
-    else
-    if ALPosEx('"Address":{"locality":"', Buffer, 1)>1 then begin
-     i := ALPosEx('"Address":{"locality":"', Buffer, 1);
-     j := ALPosEx('",', Buffer, i + 23);
-     sdesc:=Utf8ToAnsi(Copy(Buffer, i + 23, j - (i + 23)));
-    end else
-    // у НЯК всё совсем по другому. проверяем няковские поля
-    if ALPosEx('PSearchMetaData', Buffer, 1)>1 then begin
-     i:=ALPosEx('"PSearchMetaData":{"id":"', Buffer, 1);
-     j := ALPosEx('",', Buffer, i + 25);
-     sfulldesc:='http://n.maps.yandex.ru/?l=wmap&oid='+ string(Copy(Buffer, i + 25, j - (i + 25)));
-   end
-   else ParseErr:=true; // дальше строку не разбираем ибо отсутствует наименование.
+  SetLength(VTmpBuf, AResult.Data.Size);
+  Move(AResult.Data.Buffer^, VTmpBuf[1], AResult.Data.Size);
 
-   if not ParseErr then begin // если нашли признак валидности данных
-    // делаем ссылку на описание если оно есть.
-    j:=1;
-    i:= ALPosEx('"CompanyMetaData":{"id":"', Buffer, 1);
-    if i>j then begin
-     j := ALPosEx('",', Buffer, i + 25);
-     sfulldesc:='http://maps.yandex.ru/sprav/'+string(Copy(Buffer, i + 25, j - (i + 25)))+'/';
-    end;
+  VJsonObject := SO(VTmpBuf);
 
-     // достаём наименование
-     i := ALPosEx(',"name":"', Buffer, 1);
-     j := ALPosEx('",', Buffer, i + 9);
-     sname:= Utf8ToAnsi(Copy(Buffer, i + 9, j - (i + 9)));
-     // прибавляем статусную часть (улица,посёлок..) если она идёт сразу за наименованием
-     if Copy(Buffer,j,8)='","type"' then begin
-      i := ALPosEx('"type":"', Buffer, j);
-      j := ALPosEx('",', Buffer, i + 8);
-      sname:= sname+' '+Utf8ToAnsi(Copy(Buffer, i + 8, j - (i + 8)));
-     end;
-
-     // Достаём координаты
-     i := ALPosEx('"coordinates":[', Buffer, j);
-     j := ALPosEx(',', Buffer, i + 15);
-     slon := Copy(Buffer, i + 15, j - (i + 15));
-     i := ALPosEx(']', Buffer, j);
-     slat := Copy(Buffer, j + 1, i - (j + 1));
-     if slat[1] = '\' then delete(slat, 1, 1);
-     if slon[1] = '\' then delete(slon, 1, 1);
-
-
-     i:=1;
-     if ALPosEx('"Categories":', Buffer, i)>i then begin// значит есть категория
-      sdesc:=sdesc+' (';
-      while ALPosEx('{"name":"', Buffer, i)>i do begin // названий категорий может быть несколько поэтому будем их сцеплять (банки-банкоматы)
-       i := ALPosEx('{"name":"', Buffer, i);
-       j := ALPosEx('","', Buffer, i);
-       sdesc:=sdesc+' '+Utf8ToAnsi(Copy(Buffer, i + 9, j - (i + 9)));
-       i:=j;
-       end;
-      sdesc:=sdesc+')';
-      end;
-
-     // описание из НЯК
-     j:=1;
-     i:= ALPosEx('{"locality":"', Buffer, 1);
-     if i>j then begin
-      j := ALPosEx('"', Buffer, i + 13);
-      sdesc:='(НЯК) '+Utf8ToAnsi(Copy(Buffer, i + 13, j - (i + 13)));
-      end;
-
-     // описание из НЯК
-     j:=1;
-     i:= ALPosEx('"category_name":"', Buffer, 1);
-     if i>j then begin
-      j := ALPosEx('"', Buffer, i + 17);
-      if sdesc='' then sdesc:='(НЯК) ';
-      sdesc:=sdesc+' ('+Utf8ToAnsi(Copy(Buffer, i + 17, j - (i + 17)))+')';
-      end;
-
-     try
-      VPoint.Y := ALStrToFloat(slat, VFormatSettings);
-      VPoint.X := ALStrToFloat(slon, VFormatSettings);
-     except
-      raise EParserError.CreateFmt(SAS_ERR_CoordParseError, [slat, slon]);
-     end;
-
-      // если нашли что-нибудь - тогда добавляем точку.
-     if sdesc<>'' then begin
-      VPlace := PlacemarkFactory.Build(VPoint, sname, sdesc, sfulldesc, 4);
-      VList.Add(VPlace);
-     end;
-    end;
-    Buffer:='';
-   end;
-   if (BrLevel=1) and (curpos>= length(Vstr2Find)) then err:=true;//  Хватит. Довольно. Выходим.
-   end;
+  if not Assigned(VJsonObject) then begin
+    raise EParserError.Create('JSON parser error');
   end;
+
+  VJsonArray := VJsonObject.A['response.GeoObjectCollection.featureMember'];
+  if Assigned(VJsonArray) then begin
+    for I := 0 to VJsonArray.Length - 1 do begin
+      VResultItem := VJsonArray.O[I];
+      VName := Utf8ToAnsi(VResultItem.S['GeoObject.name']);
+      VDescription := Utf8ToAnsi(VResultItem.S['GeoObject.description']);
+      VPoint := _PosToPoint(Utf8ToAnsi(VResultItem.S['GeoObject.Point.pos']));
+      VPlace := PlacemarkFactory.Build(VPoint, VName, VDescription, '', 4);
+      VList.Add(VPlace);
+    end;
+  end;
+
   Result := VList;
 end;
 
-function TGeoCoderByYandex.PrepareRequest(const ASearch: WideString;
-  const ALocalConverter: ILocalCoordConverter): IDownloadRequest;
+function TGeoCoderByYandex.PrepareRequest(
+  const ASearch: WideString;
+  const ALocalConverter: ILocalCoordConverter
+): IDownloadRequest;
 var
-  VSearch: String;
   VConverter: ICoordConverter;
   VZoom: Byte;
   VMapRect: TDoubleRect;
   VLonLatRect: TDoubleRect;
 begin
-  VSearch := ASearch;
   VConverter:=ALocalConverter.GetGeoConverter;
   VZoom := ALocalConverter.GetZoom;
   VMapRect := ALocalConverter.GetRectInMapPixelFloat;
   VConverter.CheckPixelRectFloat(VMapRect, VZoom);
   VLonLatRect := VConverter.PixelRectFloat2LonLatRect(VMapRect, VZoom);
+
+  // http://api.yandex.ru/maps/doc/geocoder/desc/concepts/input_params.xml
   Result :=
     PrepareRequestByURL(
-            'http://maps.yandex.ru/?text='+URLEncode(AnsiToUtf8(VSearch))+
-            '&sll='+R2AnsiStrPoint(ALocalConverter.GetCenterLonLat.x)+','+R2AnsiStrPoint(ALocalConverter.GetCenterLonLat.y)+
-            '&sspn='+R2AnsiStrPoint(VLonLatRect.Right-VLonLatRect.Left)+','+R2AnsiStrPoint(VLonLatRect.Top-VLonLatRect.Bottom)+
-            '&z='+ALIntToStr(VZoom)+'&source=form&output=json'
+      'http://geocode-maps.yandex.ru/1.x/' +
+      '?geocode=' + URLEncode(Utf8Encode(ASearch)) +
+      '&ll=' + R2AnsiStrPoint(ALocalConverter.GetCenterLonLat.x) + ',' + R2AnsiStrPoint(ALocalConverter.GetCenterLonLat.y) +
+      '&spn=' + R2AnsiStrPoint(VLonLatRect.Right-VLonLatRect.Left) + ',' + R2AnsiStrPoint(VLonLatRect.Top-VLonLatRect.Bottom) +
+      '&format=json' +
+      '&results=15'
     );
 end;
-end.
-// examples
-//  http://maps.yandex.ru/?text=%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F%2C%20%D0%A2%D1%8E%D0%BC%D0%B5%D0%BD%D1%81%D0%BA%D0%B0%D1%8F%20%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C%2C%20%D0%A2%D1%8E%D0%BC%D0%B5%D0%BD%D1%8C&sll=65.558412%2C57.182627&ll=38.975277%2C45.035407&spn=0.469666%2C0.261446&z=11&l=map
-//  http://maps.yandex.ru/?text=%D0%B1%D0%B0%D0%BB%D0%BE%D1%87%D0%BA%D0%B0&sll=38.975276999999984%2C45.03540700001939&sspn=0.469666%2C0.261446&z=11&source=form&output=json
 
-// http://maps.yandex.ru/?text=%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0&sll=37.4969343220393,55.7374581159436&sspn=0.211658477783203,0.0711842917507042&z=13&source=form&output=json
+end.
