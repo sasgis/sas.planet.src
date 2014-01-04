ee0d4cd6310981c18e51dc8e33685d51c9d82575
zed <starmen@tut.by>
2014-01-04 15:21:43 +0300

    При поиске через гугл теперь используется json вместо xml

--- GeoCoder/u_GeoCoderByGoogle.pas
+++ GeoCoder/u_GeoCoderByGoogle.pas
@@ -1,6 +1,6 @@
 {******************************************************************************}
 {* SAS.Planet (SAS.Планета)                                                   *}
-{* Copyright (C) 2007-2012, SAS.Planet development team.                      *}
+{* Copyright (C) 2007-2014, SAS.Planet development team.                      *}
 {* This program is free software: you can redistribute it and/or modify       *}
 {* it under the terms of the GNU General Public License as published by       *}
 {* the Free Software Foundation, either version 3 of the License, or          *}
@@ -52,9 +52,7 @@ implementation
 
 uses
   SysUtils,
-  forms,
-  XMLIntf,
-  XMLDoc,
+  superobject,
   t_GeoTypes,
   i_GeoCoder,
   i_VectorDataItemSimple,
@@ -73,63 +71,86 @@ function TGeoCoderByGoogle.ParseResultToPlacemarksList(
   const ALocalConverter: ILocalCoordConverter
 ): IInterfaceListSimple;
 var
-  Stream: TMemoryStream;
-  Node: IXMLNode;
-  PlacemarkNode, PointNode, CoordNode, AddressNode: IXMLNode;
-  i: Integer;
+  I: Integer;
+  VTmpBuf: UTF8String;
+  VJsonObject: ISuperObject;
+  VJsonArray: TSuperArray;
+  VResultItem: ISuperObject;
   VPoint: TDoublePoint;
   VPlace: IVectorDataItemPoint;
   VList: IInterfaceListSimple;
   VFormatSettings: TFormatSettings;
-  XMLDocument: TXMLDocument;
-  VLon, VLat: string;
+  VStatus, VName, VLon, VLat: string;
 begin
   if AResult.Data.Size <= 0 then begin
     raise EParserError.Create(SAS_ERR_EmptyServerResponse);
   end;
+
   VFormatSettings.DecimalSeparator := '.';
+
   VList := TInterfaceListSimple.Create;
-  Stream := TMemoryStream.Create;
-  XMLDocument := TXMLDocument.Create(application);
-  try
-    Stream.Write(AResult.Data.Buffer^, AResult.Data.Size);
-    XMLDocument.LoadFromStream(Stream);
-    Node := XMLDocument.DocumentElement;
-    if (Node <> nil) and (Node.ChildNodes.Count > 0) then begin
-      for i := 0 to Node.ChildNodes.Count - 1 do begin
-        if Node.ChildNodes[i].NodeName = 'status' then begin
-          if Node.ChildNodes[i].Text <> 'OK' then begin
-            Break;
-          end;
-        end else if Node.ChildNodes[i].NodeName = 'result' then begin
-          PlacemarkNode := Node.ChildNodes[i];
-          AddressNode := PlacemarkNode.ChildNodes.FindNode('formatted_address');
-          PointNode := PlacemarkNode.ChildNodes.FindNode('geometry');
-          PointNode := PointNode.ChildNodes.FindNode('location');
-          if (AddressNode <> nil) and (PointNode <> nil) then begin
-            CoordNode := PointNode.ChildNodes.FindNode('lng');
-            Assert(CoordNode <> nil);
-            VLon := CoordNode.Text;
-            CoordNode := PointNode.ChildNodes.FindNode('lat');
-            Assert(CoordNode <> nil);
-            VLat := CoordNode.Text;
-            try
-              VPoint.X := StrToFloat(VLon, VFormatSettings);
-              VPoint.Y := StrToFloat(VLat, VFormatSettings);
-            except
-              raise EParserError.CreateFmt(SAS_ERR_CoordParseError, [VLon, VLat]);
-            end;
-            VPlace := PlacemarkFactory.Build(VPoint, AddressNode.Text, '', '', 4);
-            VList.Add(VPlace);
-          end;
-        end;
-      end;
-    end;
-    Result := VList;
-  finally
-    XMLDocument.Free;
-    Stream.Free;
+
+  SetLength(VTmpBuf, AResult.Data.Size);
+  Move(AResult.Data.Buffer^, VTmpBuf[1], AResult.Data.Size);
+
+  VJsonObject := SO(VTmpBuf);
+
+  if not Assigned(VJsonObject) then begin
+    raise EParserError.Create('JSON parser error');
   end;
+
+  VStatus := Utf8ToAnsi(VJsonObject.S['status']);
+
+  (*
+    https://developers.google.com/maps/documentation/geocoding/index#StatusCodes
+
+    "OK" indicates that no errors occurred; the address was successfully parsed
+    and at least one geocode was returned.
+
+    "ZERO_RESULTS" indicates that the geocode was successful but returned no
+    results. This may occur if the geocode was passed a non-existent address or
+    a latlng in a remote location.
+
+    "OVER_QUERY_LIMIT" indicates that you are over your quota.
+
+    "REQUEST_DENIED" indicates that your request was denied, generally because
+    of lack of a sensor parameter.
+
+    "INVALID_REQUEST" generally indicates that the query (address or latlng)
+    is missing.
+
+    UNKNOWN_ERROR indicates that the request could not be processed due to a
+    server error. The request may succeed if you try again.
+  *)
+
+  if VStatus <> 'OK' then begin
+    if VStatus = 'ZERO_RESULTS' then begin
+      Exit;
+    end else begin
+      raise Exception.CreateFmt('Unexpected status value: %s', [VStatus]);
+    end;
+  end;
+
+  VJsonArray := VJsonObject.A['results'];
+  Assert(VJsonArray <> nil);
+
+  for I := 0 to VJsonArray.Length - 1 do begin
+    VResultItem := VJsonArray.O[I];
+    Assert(VResultItem <> nil);
+    VName := Utf8ToAnsi(VResultItem.S['formatted_address']);
+    VLat := Utf8ToAnsi(VResultItem.S['geometry.location.lat']);
+    VLon := Utf8ToAnsi(VResultItem.S['geometry.location.lng']);
+    try
+      VPoint.X := StrToFloat(VLon, VFormatSettings);
+      VPoint.Y := StrToFloat(VLat, VFormatSettings);
+    except
+      raise EParserError.CreateFmt(SAS_ERR_CoordParseError, [VLon, VLat]);
+    end;
+    VPlace := PlacemarkFactory.Build(VPoint, VName, '', '', 4);
+    VList.Add(VPlace);
+  end;
+
+  Result := VList;
 end;
 
 function TGeoCoderByGoogle.PrepareRequest(
@@ -137,27 +158,23 @@ function TGeoCoderByGoogle.PrepareRequest(
   const ALocalConverter: ILocalCoordConverter
 ): IDownloadRequest;
 var
-  VSearch: String;
+  VSearch: string;
   VConverter: ICoordConverter;
   VZoom: Byte;
   VMapRect: TDoubleRect;
   VLonLatRect: TDoubleRect;
-  i: integer;
 begin
-  VSearch := ASearch;
-  for i := 1 to length(VSearch) do begin
-    if VSearch[i] = ' ' then begin
-      VSearch[i] := '+';
-    end;
-  end;
+  VSearch := StringReplace(ASearch, ' ', '+', [rfReplaceAll]);
   VConverter := ALocalConverter.GetGeoConverter;
   VZoom := ALocalConverter.GetZoom;
   VMapRect := ALocalConverter.GetRectInMapPixelFloat;
   VConverter.CheckPixelRectFloat(VMapRect, VZoom);
   VLonLatRect := VConverter.PixelRectFloat2LonLatRect(VMapRect, VZoom);
+
+  // https://developers.google.com/maps/documentation/geocoding/index
   Result :=
     PrepareRequestByURL(
-      'http://maps.googleapis.com/maps/api/geocode/xml?address=' +
+      'http://maps.googleapis.com/maps/api/geocode/json?address=' +
       URLEncode(AnsiToUtf8(VSearch)) +
       '&sensor=false' +
       '&language=' + StringReplace(SAS_STR_GoogleSearchLanguage, '&hl=', '', [rfIgnoreCase]) +
