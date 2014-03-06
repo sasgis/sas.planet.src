967292c38f628528b00b0385fbfb26bc2d9812c7
Sergey Gavrilenko <garl@mail.ru>
2014-03-06 15:11:46 +0400

    sasgis.ru -> sasgis.org (GeoCoder)

--- GeoCoder/u_GeoCoderByGoogle.pas
+++ GeoCoder/u_GeoCoderByGoogle.pas
@@ -14,8 +14,8 @@
 {* You should have received a copy of the GNU General Public License          *}
 {* along with this program.  If not, see <http://www.gnu.org/licenses/>.      *}
 {*                                                                            *}
-{* http://sasgis.ru                                                           *}
-{* az@sasgis.ru                                                               *}
+{* http://sasgis.org                                                          *}
+{* info@sasgis.org                                                            *}
 {******************************************************************************}
 
 unit u_GeoCoderByGoogle;
