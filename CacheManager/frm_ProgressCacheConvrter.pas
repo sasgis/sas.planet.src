bb18cbda0302e154fa278db13de0680f14f80dae
Sergey Gavrilenko <garl@mail.ru>
2014-03-06 13:15:06 +0400

    sasgis.ru -> sasgis.org ( CacheManager, GeoCalc )

--- CacheManager/frm_ProgressCacheConvrter.pas
+++ CacheManager/frm_ProgressCacheConvrter.pas
@@ -1,6 +1,6 @@
 {******************************************************************************}
 {* SAS.Planet (SAS.Планета)                                                   *}
-{* Copyright (C) 2007-2012, SAS.Planet development team.                      *}
+{* Copyright (C) 2007-2014, SAS.Planet development team.                      *}
 {* This program is free software: you can redistribute it and/or modify       *}
 {* it under the terms of the GNU General Public License as published by       *}
 {* the Free Software Foundation, either version 3 of the License, or          *}
@@ -14,8 +14,8 @@
 {* You should have received a copy of the GNU General Public License          *}
 {* along with this program.  If not, see <http://www.gnu.org/licenses/>.      *}
 {*                                                                            *}
-{* http://sasgis.ru                                                           *}
-{* az@sasgis.ru                                                               *}
+{* http://sasgis.org                                                          *}
+{* info@sasgis.org                                                            *}
 {******************************************************************************}
 
 unit frm_ProgressCacheConvrter;
@@ -181,7 +181,7 @@ end;
 
 procedure TfrmProgressCacheConverter.btnPauseClick(Sender: TObject);
 begin
-  if not FFinished then begin    
+  if not FFinished then begin
     if FThreadPaused then begin
       FConverterThread.Resume;
       FThreadPaused := False;
@@ -221,7 +221,7 @@ begin
     lblProcessedValue.Caption := FloatToStrF(FProgressInfo.TilesProcessed, ffNumber, 12, 0);
     lblSkippedValue.Caption := FloatToStrF(FProgressInfo.TilesSkipped, ffNumber, 12, 0);
     lblSizeValue.Caption := VValueConverter.DataSizeConvert(FProgressInfo.TilesSize / 1024);
-    lblLastTileValue.Caption := FProgressInfo.LastTileName;     
+    lblLastTileValue.Caption := FProgressInfo.LastTileName;
     if FProgressInfo.Finished then begin
       Self.Caption := SAS_STR_Finished;
       FFinished := True;
