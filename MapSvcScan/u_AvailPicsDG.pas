b0ea08c2bb2dd650ebe16b0fca98dda4d2a572c5
Sergey Gavrilenko <garl@mail.ru>
2014-03-07 09:30:28 +0400

    sasgis.ru -> sasgis.org ( MapSvcScan )

--- MapSvcScan/u_AvailPicsDG.pas
+++ MapSvcScan/u_AvailPicsDG.pas
@@ -1,6 +1,6 @@
 {******************************************************************************}
 {* SAS.Planet (SAS.Планета)                                                   *}
-{* Copyright (C) 2007-2012, SAS.Planet development team.                      *}
+{* Copyright (C) 2007-2014, SAS.Planet development team.                      *}
 {* This program is free software: you can redistribute it and/or modify       *}
 {* it under the terms of the GNU General Public License as published by       *}
 {* the Free Software Foundation, either version 3 of the License, or          *}
@@ -14,8 +14,8 @@
 {* You should have received a copy of the GNU General Public License          *}
 {* along with this program.  If not, see <http://www.gnu.org/licenses/>.      *}
 {*                                                                            *}
-{* http://sasgis.ru                                                           *}
-{* az@sasgis.ru                                                               *}
+{* http://sasgis.org                                                          *}
+{* info@sasgis.org                                                            *}
 {******************************************************************************}
 
 unit u_AvailPicsDG;
@@ -240,7 +240,7 @@ begin
       Result := Result + Codes64[x + 1];
     end;
   end;
-  
+
   if a>0 then begin
     Result := Result + Codes64[(b shl (6-a))+1];
   end;
@@ -267,7 +267,7 @@ begin
     end
     else SWord := VLine;
   end;
-  
+
   if AWordNmbr <= N then
     Result := SWord
   else
