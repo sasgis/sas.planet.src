ef994d92037f5541ea9bd2c8564faab868cca626
Viktor Demydov <vdemidov@gmail.com>
2014-03-08 13:30:28 +0200

    02078: Подготовка к разделению понятий растрового буфера, растровой статичной картинки и растрового холста, который может делать простые операции рисования и может создавать растровые картинки.

--- i_Bitmap32Static.pas
+++ i_Bitmap32Static.pas
@@ -7,13 +7,41 @@ uses
   i_Changeable;
 
 type
-  IBitmap32Static = interface
+  IBitmap32Buffer = interface
     ['{CE710076-F0B6-43BF-A70F-15B40555DBFA}']
     function GetSize: TPoint;
     property Size: TPoint read GetSize;
 
     function GetData: PColor32Array;
     property Data: PColor32Array read GetData;
+  end;
+
+  // TODO: IBitmap32Buffer should be simple buffer of bitmap data but IBitmap32Static should have hash and should be immutable
+  IBitmap32Static = IBitmap32Buffer;
+
+  //TODO for future use
+  IBitmap32Surface = interface
+    ['{90C9BDF0-E2CC-4369-BA0B-96C46A70A19C}']
+    function GetSize: TPoint;
+    property Size: TPoint read GetSize;
+
+    function GetData: PColor32Array;
+    property Data: PColor32Array read GetData;
+
+    function IsEmpty: Boolean;
+
+    procedure InitByBitmap32Static(const ASource: IBitmap32Static);
+    procedure InitByData(const ASize: TPoint; const AData: PColor32Array);
+    procedure SetSizeAndClear(const ASize: TPoint; const AFillColor: TColor32);
+
+    procedure Clear;
+    procedure FullFill(const AFillColor: TColor32);
+
+    procedure FillRect(const ARect: TRect; const AValue: TColor32);
+    procedure DrawBitmapStatic(const ASource: IBitmap32Static);
+    // TODO: Add other simple draw methods
+
+    function MakeAndClear: IBitmap32Static;
   end;
 
   IBitmapChangeable = interface(IChangeable)
