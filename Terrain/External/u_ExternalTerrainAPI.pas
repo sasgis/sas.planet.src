e7b6c5f7b2511bb2da90deafb57679b0d099d53d
Sergey Gavrilenko <garl@mail.ru>
2014-03-11 15:03:44 +0400

    sasgis.ru -> sasgis.org ( Synchronize, Terrain )

--- Terrain/External/u_ExternalTerrainAPI.pas
+++ Terrain/External/u_ExternalTerrainAPI.pas
@@ -1,3 +1,23 @@
+{******************************************************************************}
+{* SAS.Planet (SAS.Планета)                                                   *}
+{* Copyright (C) 2007-2014, SAS.Planet development team.                      *}
+{* This program is free software: you can redistribute it and/or modify       *}
+{* it under the terms of the GNU General Public License as published by       *}
+{* the Free Software Foundation, either version 3 of the License, or          *}
+{* (at your option) any later version.                                        *}
+{*                                                                            *}
+{* This program is distributed in the hope that it will be useful,            *}
+{* but WITHOUT ANY WARRANTY; without even the implied warranty of             *}
+{* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *}
+{* GNU General Public License for more details.                               *}
+{*                                                                            *}
+{* You should have received a copy of the GNU General Public License          *}
+{* along with this program.  If not, see <http://www.gnu.org/licenses/>.      *}
+{*                                                                            *}
+{* http://sasgis.org                                                          *}
+{* info@sasgis.org                                                            *}
+{******************************************************************************}
+
 unit u_ExternalTerrainAPI;
 
 interface
@@ -89,7 +109,7 @@ const
   // for both 16-bit and 32-bit integers. This is called big-endian byte order
   c_42_MM = $2A00; // 42
 
-type  
+type
   // 4.6.2 IFD Structure
   // The IFD used in this standard consists of:
   // a 2-byte count (number of fields),
--- Terrain/GoogleEarth/u_GoogleEarthTerrainTileStorage.pas
+++ Terrain/GoogleEarth/u_GoogleEarthTerrainTileStorage.pas
@@ -1,6 +1,6 @@
 {******************************************************************************}
 {* SAS.Planet (SAS.Планета)                                                   *}
-{* Copyright (C) 2007-2013, SAS.Planet development team.                      *}
+{* Copyright (C) 2007-2014, SAS.Planet development team.                      *}
 {* This program is free software: you can redistribute it and/or modify       *}
 {* it under the terms of the GNU General Public License as published by       *}
 {* the Free Software Foundation, either version 3 of the License, or          *}
@@ -14,8 +14,8 @@
 {* You should have received a copy of the GNU General Public License          *}
 {* along with this program.  If not, see <http://www.gnu.org/licenses/>.      *}
 {*                                                                            *}
-{* http://sasgis.ru                                                           *}
-{* az@sasgis.ru                                                               *}
+{* http://sasgis.org                                                          *}
+{* info@sasgis.org                                                            *}
 {******************************************************************************}
 
 unit u_GoogleEarthTerrainTileStorage;
@@ -129,7 +129,7 @@ var
   VAvailable: Boolean;
 begin
   Result := FMemCache.Get(AXY, AZoom, VIsTne);
-  
+
   if (Result = nil) and not VIsTne then begin
 
     FSync.BeginRead;
@@ -148,7 +148,7 @@ begin
     end;
 
     FSync.BeginRead;
-    try   
+    try
       if (FCacheProvider <> nil) then begin
         VResult := FCacheProvider.GetTileInfo(
           AXY,
