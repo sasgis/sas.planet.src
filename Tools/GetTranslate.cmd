abd16a08d7df9fdde6a1dcaba01698f6d106c01b
zed <starmen@tut.by>
2015-01-24 15:31:18 +0300

    Мелкие фиксы скрипта и конфига gnugettext, чnобы он обрабатывал только содержимое папки Src

--- Tools/GetTranslate.cmd
+++ Tools/GetTranslate.cmd
@@ -1,9 +1,10 @@
-cd ..\
-copy ".\Tools\dxgettext\*.*" ".\" /Y
-dxgettext -r --delphi --nonascii --useignorepo -o:msgid -o ..\SAS.Translate
+cd ..\Src\
+copy "..\Tools\dxgettext\*.*" ".\" /Y
+dxgettext -r --delphi --nonascii --useignorepo -o:msgid -o ..\..\SAS.Translate
 del /F /Q ggexclude.cfg dxgettext.ini ignore.po
-cd ..\SAS.Translate
+cd ..\..\SAS.Translate
 msgattrib --output-file=default.po --no-location default.po
 msgremove default.po -i ignore.po -o output.po
 del default.po
 rename output.po default.po
+pause
