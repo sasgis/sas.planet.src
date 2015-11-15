97eb0c4bfcbfe42cf70b4a9540bb5d40ce5314f8
Viktor Demydov <vdemidov@gmail.com>
2015-11-15 23:34:15 +0200

    Функции для безопасного конвертирования строк в/из ASCII

--- Src/u_StrFunc.pas
+++ Src/u_StrFunc.pas
@@ -32,6 +32,15 @@ function GetBetween(const Str, After, Before: AnsiString): AnsiString;
 function SetHeaderValue(const AHeaders, AName, AValue: AnsiString): AnsiString;
 function GetHeaderValue(const AHeaders, AName: AnsiString): AnsiString;
 
+{$IFDef UNICODE}
+function IsAscii(const P: PChar; const Len: Integer): Boolean; overload; inline;
+function IsAscii(const s: string): Boolean; overload; inline;
+{$ENDIF}
+function IsAscii(const P: PAnsiChar; const Len: Integer): Boolean; overload; inline;
+function IsAscii(const s: AnsiString): Boolean; overload; inline;
+function StringToAsciiSafe(const s: string): AnsiString; inline;
+function AsciiToStringSafe(const s: AnsiString): string; inline;
+
 implementation
 
 uses
@@ -100,6 +109,81 @@ begin
     end;
   end else begin
     Result := '';
+  end;
+end;
+
+{$IFDef UNICODE}
+function IsAscii(const s: string): Boolean; overload;
+var
+  VLen: Integer;
+begin
+  VLen := Length(s);
+  if VLen > 0 then begin
+    Result := IsAscii(PChar(s), VLen);
+  end else begin
+    Result := True;
+  end;
+end;
+
+function IsAscii(const P: PChar; const Len: Integer): Boolean;
+var
+  I: Integer;
+begin
+  Result := False;
+  if P <> nil then begin
+    for I := 0 to Len - 1 do begin
+      if Ord(P[I]) > 127 then begin
+        Exit;
+      end;
+    end;
+  end;
+  Result := True;
+end;
+{$ENDIF}
+
+function IsAscii(const P: PAnsiChar; const Len: Integer): Boolean;
+var
+  I: Integer;
+begin
+  Result := False;
+  if P <> nil then begin
+    for I := 0 to Len - 1 do begin
+      if Ord(P[I]) > 127 then begin
+        Exit;
+      end;
+    end;
+  end;
+  Result := True;
+end;
+
+function IsAscii(const s: AnsiString): Boolean;
+var
+  VLen: Integer;
+begin
+  VLen := Length(s);
+  if VLen > 0 then begin
+    Result := IsAscii(PAnsiChar(s), VLen);
+  end else begin
+    Result := True;
+  end;
+end;
+
+
+function StringToAsciiSafe(const s: string): AnsiString;
+begin
+  if IsAscii(s) then begin
+    Result := AnsiString(s);
+  end else begin
+    raise Exception.CreateFmt('String "%s" contain non-ascii characters!', [s]);
+  end;
+end;
+
+function AsciiToStringSafe(const s: AnsiString): string;
+begin
+  if IsAscii(s) then begin
+    Result := string(s);
+  end else begin
+    raise Exception.CreateFmt('String "%s" contain non-ascii characters!', [s]);
   end;
 end;
 
