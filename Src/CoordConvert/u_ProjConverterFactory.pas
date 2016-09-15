069385ad3efacde5bc6d8f1380b6b02ea6e7cf5a
zed <starmen@tut.by>
2016-09-15 23:45:20 +0300

    Рефакторинг использования proj4

--- Src/CoordConvert/u_ProjConverterFactory.pas
+++ Src/CoordConvert/u_ProjConverterFactory.pas
@@ -31,14 +31,12 @@ uses
 type
   TProjConverterFactory = class(TBaseInterfacedObject, IProjConverterFactory)
   private
-    FSync: IReadWriteSync;
-    FDllFailed: Boolean;
-    FDllHolder: IInterface;
+    FProj4Status: Integer; // 0 - not loaded; 1 - ok; 2 - error
   private
-    procedure InitDll;
-    function GetArgsByEpsg(const AEPSG: Integer): AnsiString;
-    function GetByInitStringInternal(const AArgs: AnsiString): IProjConverter;
+    function _GetArgsByEpsg(const AEPSG: Integer): AnsiString;
+    function _GetByInitString(const AArgs: AnsiString): IProjConverter;
   private
+    { IProjConverterFactory }
     function GetByEPSG(const AEPSG: Integer): IProjConverter;
     function GetByInitString(const AArgs: AnsiString): IProjConverter;
   public
@@ -48,221 +46,126 @@ type
 implementation
 
 uses
+  Proj4,
   ALString,
-  t_GeoTypes,
-  u_Synchronizer;
+  t_GeoTypes;
+
+const
+  cProj4NotLoaded = 0;
+  cProj4LoadedOK = 1;
+  cProj4LoadedError = 2;
+
+resourcestring
+  rsProjectionInitError = 'Can''t initialize proj4 with string: %s';
 
 type
-  // proj4 routines (as types)
-  TProjXY = TDoublePoint;
-  TProjLP = TDoublePoint;
-  PProjPJ = Pointer;
+  EProjConverterByDllError = class(Exception);
 
-type
-  IProjDLLHolder = interface
-    function ProjInit(
-      const Args: PAnsiChar
-    ): PProjPJ;
-    function ProjFree(
-      projPJ: PProjPJ
-    ): Integer;
-
-    function ProjForward(
-      ProjLP: TProjLP;
-      const projPJ: PProjPJ
-    ): TProjXY;
-    function ProjInverse(
-      ProjXY: TProjXY;
-      const projPJ: PProjPJ
-    ): TProjLP;
-  end;
-
-type
-  T_pj_init_plus = function(
-    const Args: PAnsiChar
-  ): PProjPJ; cdecl;
-
-  { forward projection normally Longitude Latitude to plain xy Coordinates }
-  T_pj_fwd = function(
-    ProjLP: TProjLP;
-    const projPJ: PProjPJ
-  ): TProjXY; cdecl;
-
-  { inverse projection normally plain xy coordinates to longitude latitude coordinates }
-  T_pj_inv = function(
-    ProjXY: TProjXY;
-    const projPJ: PProjPJ
-  ): TProjLP; cdecl;
-
-  { Free the allocated projections }
-  T_pj_free = function(
-    projPJ: PProjPJ
-  ): Integer; cdecl;
-
-type
-  TProjDLLHolder = class(TBaseInterfacedObject, IProjDLLHolder)
-  private
-    FDLLHandle: THandle;
-    F_pj_init_plus: T_pj_init_plus;
-    F_pj_fwd: T_pj_fwd;
-    F_pj_inv: T_pj_inv;
-    F_pj_free: T_pj_free;
-  private
-    function ProjInit(
-      const Args: PAnsiChar
-    ): PProjPJ;
-    function ProjFree(
-      projPJ: PProjPJ
-    ): Integer;
-
-    function ProjForward(
-      ProjLP: TProjLP;
-      const projPJ: PProjPJ
-    ): TProjXY;
-    function ProjInverse(
-      ProjXY: TProjXY;
-      const projPJ: PProjPJ
-    ): TProjLP;
-  public
-    constructor Create(const ADllFullName: string);
-    destructor Destroy; override;
-  end;
-
-{ TProjDLLHolder }
-
-constructor TProjDLLHolder.Create(const ADllFullName: string);
-begin
-  inherited Create;
-  // try to load library
-  FDLLHandle := LoadLibrary(PChar(ADllFullName));
-  if (FDLLHandle <> 0) then begin
-    try
-      F_pj_init_plus := GetProcAddress(FDLLHandle, 'pj_init_plus');
-      if Addr(F_pj_init_plus) = nil then begin
-        RaiseLastOSError;
-      end;
-
-      F_pj_fwd := GetProcAddress(FDLLHandle, 'pj_fwd');
-      if Addr(F_pj_fwd) = nil then begin
-        RaiseLastOSError;
-      end;
-      F_pj_inv := GetProcAddress(FDLLHandle, 'pj_inv');
-      if Addr(F_pj_inv) = nil then begin
-        RaiseLastOSError;
-      end;
-      F_pj_free := GetProcAddress(FDLLHandle, 'pj_free');
-      if Addr(F_pj_free) = nil then begin
-        RaiseLastOSError;
-      end;
-    except
-      FreeLibrary(FDLLHandle);
-      FDLLHandle := 0;
-      raise;
-    end;
-  end else begin
-    RaiseLastOSError;
-  end;
-end;
-
-destructor TProjDLLHolder.Destroy;
-begin
-  if (FDLLHandle <> 0) then begin
-    FreeLibrary(FDLLHandle);
-    FDLLHandle := 0;
-  end;
-  inherited;
-end;
-
-function TProjDLLHolder.ProjForward(
-  ProjLP: TProjLP;
-  const projPJ: PProjPJ
-): TProjXY;
-begin
-  Result := F_pj_fwd(ProjLP, projPJ);
-end;
-
-function TProjDLLHolder.ProjFree(projPJ: PProjPJ): Integer;
-begin
-  Result := F_pj_free(projPJ);
-end;
-
-function TProjDLLHolder.ProjInit(const Args: PAnsiChar): PProjPJ;
-begin
-  Result := F_pj_init_plus(Args);
-end;
-
-function TProjDLLHolder.ProjInverse(
-  ProjXY: TProjXY;
-  const projPJ: PProjPJ
-): TProjLP;
-begin
-  Result := F_pj_inv(ProjXY, projPJ);
-end;
-
-type
   TProjConverterByDll = class(TBaseInterfacedObject, IProjConverter)
   private
-    FDllHolder: IProjDLLHolder;
-    FprojPJ: PProjPJ;
+    FGeoPJ: projPJ;
+    FProjPJ: projPJ;
+    FProj4InitStr: AnsiString;
   private
-    function LonLat2XY(const AProjLP: TDoublePoint): TDoublePoint;
-    function XY2LonLat(const AProjXY: TDoublePoint): TDoublePoint;
+    function LonLat2XY(const ALonLat: TDoublePoint): TDoublePoint;
+    function XY2LonLat(const AXY: TDoublePoint): TDoublePoint;
   public
-    constructor Create(
-      const ADllHolder: IProjDLLHolder;
-      AprojPJ: PProjPJ
-    );
+    constructor Create(const AProj4InitStr: AnsiString);
     destructor Destroy; override;
+    procedure AfterConstruction; override;
   end;
 
 { TProjConverterByDll }
 
-constructor TProjConverterByDll.Create(
-  const ADllHolder: IProjDLLHolder;
-  AprojPJ: PProjPJ
-);
+constructor TProjConverterByDll.Create(const AProj4InitStr: AnsiString);
 begin
+  Assert(AProj4InitStr <> '');
   inherited Create;
-  Assert(ADllHolder <> nil);
-  FDllHolder := ADllHolder;
-  Assert(AprojPJ <> nil);
-  FprojPJ := AprojPJ;
+  FProj4InitStr := AProj4InitStr;
+  FProjPJ := nil;
+  FGeoPJ := nil;
+end;
+
+procedure TProjConverterByDll.AfterConstruction;
+begin
+  inherited;
+
+  FProjPJ := pj_init_plus(PAnsiChar(FProj4InitStr));
+  if FProjPJ = nil then begin
+    raise EProjConverterByDllError.CreateFmt(rsProjectionInitError, [FProj4InitStr]);
+  end;
+
+  FGeoPJ := pj_init_plus(PAnsiChar(wgs84));
+  if FGeoPJ = nil then begin
+    raise EProjConverterByDllError.CreateFmt(rsProjectionInitError, [wgs84]);
+  end;
 end;
 
 destructor TProjConverterByDll.Destroy;
 begin
-  if Assigned(FDllHolder) then begin
-    FDllHolder.ProjFree(FprojPJ);
+  if FProjPJ <> nil then begin
+    pj_free(FProjPJ);
+    FProjPJ := nil;
   end;
-  FprojPJ := nil;
-  FDllHolder := nil;
+
+  if FGeoPJ <> nil then begin
+    pj_free(FGeoPJ);
+    FGeoPJ := nil;
+  end;
+
   inherited;
 end;
 
 function TProjConverterByDll.LonLat2XY(
-  const AProjLP: TDoublePoint): TDoublePoint;
-const
- { Scaling value decimal degree to radiant }
-  DEG_TO_RAD = 0.0174532925199432958;
+  const ALonLat: TDoublePoint
+): TDoublePoint;
 var
-  VProjLP: TProjLP;
+  err: Integer;
+  x, y, z: Double;
 begin
-  VProjLP.X := AProjLP.X * DEG_TO_RAD;
-  VProjLP.Y := AProjLP.Y * DEG_TO_RAD;
-  Result := FDllHolder.ProjForward(VProjLP, FprojPJ);
+  Assert(FProjPJ <> nil);
+  Assert(FGeoPJ <> nil);
+
+  x := ALonLat.X * DEG_TO_RAD;
+  y := ALonLat.Y * DEG_TO_RAD;
+  z := 0;
+
+  err := pj_transform(FGeoPJ, FProjPJ, 1, 0, x, y, z); // Geo -> Proj
+
+  if err <> 0 then begin
+    raise EProjConverterByDllError.CreateFmt(
+      'LonLat2XY(%.6f; %.6f) failed: %s', [ALonLat.X, ALonLat.Y, pj_strerrno(err)]
+    );
+  end;
+
+  Result.X := x;
+  Result.Y := y;
 end;
 
 function TProjConverterByDll.XY2LonLat(
-  const AProjXY: TDoublePoint): TDoublePoint;
-const
- { Scaling value radiant to decimal degree }
-  RAD_TO_DEG = 57.29577951308232;
+  const AXY: TDoublePoint
+): TDoublePoint;
 var
-  VProjLP: TProjLP;
+  err: Integer;
+  x, y, z: Double;
 begin
-  VProjLP := FDllHolder.ProjInverse(AProjXY, FprojPJ);
-  Result.X := VProjLP.X * RAD_TO_DEG;
-  Result.Y := VProjLP.Y * RAD_TO_DEG;
+  Assert(FProjPJ <> nil);
+  Assert(FGeoPJ <> nil);
+
+  x := AXY.X;
+  y := AXY.Y;
+  z := 0;
+
+  err := pj_transform(FProjPJ, FGeoPJ, 1, 0, x, y, z); // Proj -> Geo
+
+  if err <> 0 then begin
+    raise EProjConverterByDllError.CreateFmt(
+      'XY2LonLat(%.6f; %.6f) failed: %s', [AXY.X, AXY.Y, pj_strerrno(err)]
+    );
+  end;
+
+  Result.X := x * RAD_TO_DEG;
+  Result.Y := y * RAD_TO_DEG;
 end;
 
 { TProjConverterFactory }
@@ -270,48 +173,78 @@ end;
 constructor TProjConverterFactory.Create;
 begin
   inherited Create;
-  FSync := GSync.SyncStd.Make(Self.ClassName);
-  FDllFailed := False;
+  FProj4Status := cProj4NotLoaded;
 end;
 
-function TProjConverterFactory.GetArgsByEpsg(const AEPSG: Integer): AnsiString;
+function TProjConverterFactory._GetArgsByEpsg(const AEPSG: Integer): AnsiString;
 var
-  i: Integer;
+  I: Integer;
 begin
   Result := '';
   // known EPSGs
-  if (AEPSG = 53004) then begin
+  if AEPSG = 53004 then begin
     // Sphere Mercator ESRI:53004
     Result := '+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +a=6371000 +b=6371000 +units=m +no_defs';
-  end else if (AEPSG = 3785) then begin
+  end else if AEPSG = 3785 then begin
     // Popular Visualisation CRS / Mercator
     Result := '+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +a=6378137 +b=6378137 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs';
-  end else if (AEPSG = 3395) then begin
+  end else if AEPSG = 3395 then begin
     // WGS 84 / World Mercator
     Result := '+proj=merc +lon_0=0 +k=1 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs';
-  end else if (AEPSG = 4269) then begin
+  end else if AEPSG = 4269 then begin
     // NAD83
     Result := '+proj=longlat +ellps=GRS80 +datum=NAD83 +no_defs';
-  end else if (AEPSG = 4326) then begin
+  end else if AEPSG = 4326 then begin
     // WGS 84
-    Result := '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs';
+    Result := wgs84;
   end else if (AEPSG >= 2463) and (AEPSG <= 2491) then begin
     // 2463-2491 = Pulkovo 1995 / Gauss-Kruger CM
-    i := 21 + (AEPSG - 2463) * 6;
-    if (i > 180) then begin
-      i := i - 360;
+    I := 21 + (AEPSG - 2463) * 6;
+    if I > 180 then begin
+      I := I - 360;
     end;
-    Result := '+proj=tmerc +lat_0=0 +lon_0=' + ALIntToStr(i) + ' +k=1 +x_0=500000 +y_0=0 +ellps=krass +units=m +no_defs';
+    Result := '+proj=tmerc +lat_0=0 +lon_0=' + ALIntToStr(I) + ' +k=1 +x_0=500000 +y_0=0 +ellps=krass +units=m +no_defs';
   end else if (AEPSG >= 2492) and (AEPSG <= 2522) then begin
     // 2492-2522 = Pulkovo 1942 / Gauss-Kruger CM
-    i := 9 + (AEPSG - 2492) * 6;
-    if (i > 180) then begin
-      i := i - 360;
+    I := 9 + (AEPSG - 2492) * 6;
+    if I > 180 then begin
+      I := I - 360;
     end;
-    Result := '+proj=tmerc +lat_0=0 +lon_0=' + ALIntToStr(i) + ' +k=1 +x_0=500000 +y_0=0 +ellps=krass +units=m +no_defs';
+    Result := '+proj=tmerc +lat_0=0 +lon_0=' + ALIntToStr(I) + ' +k=1 +x_0=500000 +y_0=0 +ellps=krass +units=m +no_defs';
   end else if (AEPSG >= 32601) and (AEPSG <= 32660) then begin
     // 32601-32660 = WGS 84 / UTM zone N
     Result := '+proj=utm +zone=' + ALIntToStr(AEPSG - 32600) + ' +ellps=WGS84 +datum=WGS84 +units=m +no_defs';
+  end;
+end;
+
+function TProjConverterFactory._GetByInitString(
+  const AArgs: AnsiString
+): IProjConverter;
+var
+  VProj4Status: Integer;
+begin
+  Result := nil;
+  if AArgs <> '' then begin
+
+    VProj4Status := InterlockedCompareExchange(FProj4Status, 0, 0);
+
+    if VProj4Status = cProj4NotLoaded then begin
+      try
+        if init_proj4_dll(proj4_dll, True) then begin
+          VProj4Status := cProj4LoadedOK;
+        end else begin
+          VProj4Status := cProj4LoadedError;
+        end;
+        InterlockedExchange(FProj4Status, VProj4Status);
+      except
+        InterlockedExchange(FProj4Status, cProj4LoadedError);
+        raise;
+      end;
+    end;
+
+    if VProj4Status = cProj4LoadedOK then begin
+      Result := TProjConverterByDll.Create(AArgs);
+    end;
   end;
 end;
 
@@ -319,77 +252,26 @@ function TProjConverterFactory.GetByEPSG(const AEPSG: Integer): IProjConverter;
 var
   VArgs: AnsiString;
 begin
-  Result := nil;
-  VArgs := GetArgsByEpsg(AEPSG);
-  if VArgs <> '' then begin
-    Result := GetByInitStringInternal(VArgs);
-  end;
+  VArgs := _GetArgsByEpsg(AEPSG);
+  Result := _GetByInitString(VArgs);
 end;
 
 function TProjConverterFactory.GetByInitString(
   const AArgs: AnsiString
 ): IProjConverter;
 const
-  c_EPSG: AnsiString = 'EPSG:';
+  cEPSG: AnsiString = 'EPSG:';
 var
   VEPSG: Integer;
   VArgs: AnsiString;
 begin
   VArgs := AArgs;
-  if ALSameText(ALCopyStr(AArgs, 1, Length(c_EPSG)), c_EPSG) then begin
-    if ALTryStrToInt(ALCopyStr(AArgs, Length(c_EPSG) + 1, Length(AArgs)), VEPSG) then begin
-      VArgs := GetArgsByEpsg(VEPSG);
+  if ALSameText(ALCopyStr(AArgs, 1, Length(cEPSG)), cEPSG) then begin
+    if ALTryStrToInt(ALCopyStr(AArgs, Length(cEPSG) + 1, Length(AArgs)), VEPSG) then begin
+      VArgs := _GetArgsByEpsg(VEPSG);
     end;
   end;
-  Result := GetByInitStringInternal(VArgs);
-end;
-
-function TProjConverterFactory.GetByInitStringInternal(
-  const AArgs: AnsiString): IProjConverter;
-var
-  VDll: IProjDLLHolder;
-  VProj: PProjPJ;
-begin
-  Result := nil;
-  if AArgs <> '' then begin
-    VDll := nil;
-    FSync.BeginWrite;
-    try
-      VDll := IProjDLLHolder(FDllHolder);
-      if VDll = nil then begin
-        if not FDllFailed then begin
-          try
-            InitDll;
-            VDll := IProjDLLHolder(FDllHolder);
-            if VDll = nil then begin
-              FDllFailed := True;
-            end;
-          except
-            FDllFailed := True;
-            FDllHolder := nil;
-          end;
-        end;
-      end;
-    finally
-      FSync.EndWrite;
-    end;
-    if VDll <> nil then begin
-      VProj := VDll.ProjInit(PAnsiChar(AArgs));
-      if VProj <> nil then begin
-        Result := TProjConverterByDll.Create(VDll, VProj);
-      end;
-    end;
-  end;
-end;
-
-procedure TProjConverterFactory.InitDll;
-const
-  proj4_dll = 'proj.dll';
-var
-  VDll: IProjDLLHolder;
-begin
-  VDll := TProjDLLHolder.Create(proj4_dll);
-  FDllHolder := VDll;
+  Result := _GetByInitString(VArgs);
 end;
 
 end.
