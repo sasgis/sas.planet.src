1f35a192d308a34aa36d31415a78a9834c83c245
zed <starmen@tut.by>
2019-08-22 16:22:22 +0300

    В скрипте компиляции ресурсов включил подробный вывод

--- Resources/Build.Resources.cmd
+++ Resources/Build.Resources.cmd
@@ -1,4 +1,4 @@
-brcc32 -l0409 -foMainIcon.res MainIcon\MainIcon.rc
-brcc32 -l0409 -foVersionInfo.res Version\Version.rc
-brcc32 -l0409 -foCommon.res Common\Common.rc
-brcc32 -l0409 -foBuildInfo.res BuildInfo\BuildInfo.rc
+brcc32 -v -l0409 -foMainIcon.res MainIcon\MainIcon.rc
+brcc32 -v -l0409 -foVersionInfo.res Version\Version.rc
+brcc32 -v -l0409 -foCommon.res Common\Common.rc
+brcc32 -v -l0409 -foBuildInfo.res BuildInfo\BuildInfo.rc
