a537b5bee6a6d4bf5ebb2290fbc9fee34cdb3a0d
zed <starmen@tut.by>
2019-12-04 17:42:59 +0300

    3466: Отключил флаг DetectMMOperationsAfterUninstall для релизной сборки и добавил проверку, что он отключён

--- Src/SASPlanet.modules.pas
+++ Src/SASPlanet.modules.pas
@@ -27,4 +27,17 @@ uses
 
 implementation
 
+{$IF DEFINED(RELEASE) and DEFINED(USE_FAST_MM)}
+  {$IF CompilerVersion >= 32}
+    {$I FastMM4Options.inc}
+    {$IFDEF DetectMMOperationsAfterUninstall}
+      // bug in Delphi 10.2 and up cause FastMM error:
+      // "FastMM has detected a FreeMem call after FastMM was uninstalled"
+      // Delphi bug-report: https://quality.embarcadero.com/browse/RSP-22897
+      // SASPlanet issue: http://www.sasgis.org/mantis/view.php?id=3466
+      {$MESSAGE ERROR 'DetectMMOperationsAfterUninstall (FastMM4Options.inc) must be undefined!'}
+    {$ENDIF}
+  {$IFEND}
+{$IFEND}
+
 end.
