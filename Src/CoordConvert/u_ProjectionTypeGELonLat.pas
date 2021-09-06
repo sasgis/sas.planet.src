39bc318cf5bc653eef6e8712fd6af24d0ea9b572
zed <starmen@tut.by>
2021-09-06 12:34:43 +0300

    Форматирование и чистка кода

--- Src/CoordConvert/u_ProjectionTypeGELonLat.pas
+++ Src/CoordConvert/u_ProjectionTypeGELonLat.pas
@@ -24,9 +24,7 @@ unit u_ProjectionTypeGELonLat;
 interface
 
 uses
-  t_Hash,
   t_GeoTypes,
-  i_Datum,
   u_ProjectionTypeBase;
 
 type
@@ -34,44 +32,28 @@ type
   protected
     function Relative2LonLatInternal(const APoint: TDoublePoint): TDoublePoint; override;
     function LonLat2RelativeInternal(const APoint: TDoublePoint): TDoublePoint; override;
-  public
-    constructor Create(
-      const AHash: THashValue;
-      const ADatum: IDatum;
-      const AProjEPSG: integer
-    );
   end;
 
 implementation
 
-uses
-  Math;
 
 { TProjectionTypeGELonLat }
-
-constructor TProjectionTypeGELonLat.Create(
-  const AHash: THashValue;
-  const ADatum: IDatum;
-  const AProjEPSG: integer
-);
-begin
-  inherited Create(AHash, ADatum, AProjEPSG);
-end;
 
 function TProjectionTypeGELonLat.LonLat2RelativeInternal(
   const APoint: TDoublePoint
 ): TDoublePoint;
 begin
-  Result.x := (0.5 + APoint.x / 360);
-  Result.y := (0.5 - APoint.y / 360);
+  Result.X := (0.5 + APoint.X / 360);
+  Result.Y := (0.5 - APoint.Y / 360);
 end;
 
 function TProjectionTypeGELonLat.Relative2LonLatInternal(
   const APoint: TDoublePoint
 ): TDoublePoint;
 begin
-  Result.X := (APoint.x - 0.5) * 360;
-  Result.y := -(APoint.y - 0.5) * 360;
+  Result.X :=  (APoint.X - 0.5) * 360;
+  Result.Y := -(APoint.Y - 0.5) * 360;
+end;
 end;
 
 end.
