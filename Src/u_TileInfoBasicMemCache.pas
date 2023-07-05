1a8b7d9314acc349f00bdb22f82d73c0b9653e6c
zed <starmen@tut.by>
2023-07-05 12:12:44 +0300

    3857: Добавил опцию для отключения очистки RAM кэша по TTL (в zmp: MemCacheClearStrategy=3)

--- Src/u_TileInfoBasicMemCache.pas
+++ Src/u_TileInfoBasicMemCache.pas
@@ -40,9 +40,10 @@ uses
 
 type
   TClearByTTLStrategy = (
-    csByOldest   = 0, // удалять ВСЕ тайлы из кэша, если истёк TTL у самого СТАРОГО тайла
-    csByYoungest = 1, // удалять ВСЕ тайлы из кэша, если истёк TTL у самого МОЛОДОГО тайла
-    csOneByOne   = 2  // удалять только те тайлы, у которых истёк TTL
+    csByOldest   = 0, // delete ALL tiles from the cache if the TTL of the OLDEST tile has expired
+    csByYoungest = 1, // remove ALL tiles from the cache if the TTL of the YOUNGEST tile has expired
+    csOneByOne   = 2, // delete only those tiles whose TTL has expired
+    csNoOne      = 3  // do not delete any tile by TTL (disable TTL cleaner)
   );
 
   TTileInfoBasicMemCache = class(TBaseInterfacedObject, ITileInfoBasicMemCache)
@@ -443,6 +444,9 @@ var
   VCleanerCalled: Boolean;
   VCounterContext: TInternalPerformanceCounterContext;
 begin
+  if FClearStrategy = csNoOne then begin
+    Exit;
+  end;
   FCS.BeginWrite;
   try
     VCounterContext := FClearByTTLCounter.StartOperation;
