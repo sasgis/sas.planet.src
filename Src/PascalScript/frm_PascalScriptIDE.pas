f694558de96ff7f02915c4468a8859b55a3dee7a
zed <starmen@tut.by>
2023-07-21 12:32:09 +0300

    Актуализация url

--- Src/PascalScript/frm_PascalScriptIDE.pas
+++ Src/PascalScript/frm_PascalScriptIDE.pas
@@ -310,13 +310,7 @@ resourcestring
     'Do you really want to close without saving?';
 
 const
-  cWikiPage =
-    'http://www.sasgis.org/wikisasiya/doku.php/' +
-    '%D0%BE%D0%BF%D0%B8%D1%81%D0%B0%D0%BD%D0%B8%D1%8F_' +
-    '%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%82%D0%B0_%D0%BF' +
-    '%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%' +
-    'D0%B5%D0%BB%D1%8C%D1%81%D0%BA%D0%B8%D1%85_%D0%BA%' +
-    'D0%B0%D1%80%D1%82_zmp';
+  cWikiPage = 'http://www.sasgis.org/wikisasiya/doku.php/zmp';
 
 { TfrmPascalScriptIDE }
 
