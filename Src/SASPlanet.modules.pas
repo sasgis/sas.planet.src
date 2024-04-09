8249acb0771354e3083f94289a7f880acf44d9bd
zed <starmen@tut.by>
2024-04-09 18:16:08 +0300

    Удаление устаревших юнитов

--- Src/SASPlanet.modules.pas
+++ Src/SASPlanet.modules.pas
@@ -28,39 +28,17 @@ interface
 {$I SASPlanet.inc}
 
 uses
-  {$IFDEF USE_FAST_MM_4}
-  FastMM4,
-  {$ENDIF}
-  {$IFDEF USE_FAST_MM_5}
+  {$IFDEF USE_FAST_MM}
   FastMM5,
   {$ENDIF}
   {$IFDEF USE_FAST_MOVE}
   FastMove,
   {$ENDIF}
-  {$IFDEF USE_FAST_CODE}
-  FastCode,
-  {$ENDIF}
-  {$IF CompilerVersion < 21.0} // CompilerVersion < Delphi 2010
-  MidasSpeedFix,
-  {$IFEND}
   {$IF (CompilerVersion < 23) or (CompilerVersion >= 27)}
   MidasLib, // bug in MidasLib.dcu in XE2..XE5 http://qc.embarcadero.com/wc/qcmain.aspx?d=109476
   {$IFEND}
   WinInetFix;
 
 implementation
-
-{$IF DEFINED(RELEASE) and DEFINED(USE_FAST_MM_4)}
-  {$IF CompilerVersion >= 32}
-    {$I FastMM4Options.inc}
-    {$IFDEF DetectMMOperationsAfterUninstall}
-      // bug in Delphi 10.2 and up cause FastMM error:
-      // "FastMM has detected a FreeMem call after FastMM was uninstalled"
-      // Delphi bug-report: https://quality.embarcadero.com/browse/RSP-22897
-      // SASPlanet issue: http://www.sasgis.org/mantis/view.php?id=3466
-      {$MESSAGE ERROR 'DetectMMOperationsAfterUninstall (FastMM4Options.inc) must be undefined!'}
-    {$ENDIF}
-  {$IFEND}
-{$IFEND}
 
 end.
