187f440e787985b9346f655ef1074e7c37e7264c
zed <starmen@tut.by>
2024-11-20 17:14:43 +0300

    Доработка скрипта копирования dll для win64:
      - imagequant: из реп не поддерживает win7, поэтому будет использоваться свой билд под rust 1.75
      - libcurl: по аналогии с 32-х битной версией, статическая сборка из официального источника https://curl.se/windows/

--- Tools/CopyLibs64.cmd
+++ Tools/CopyLibs64.cmd
@@ -5,7 +5,7 @@ set lib=.\..\.bin\win64\lib64\
 
 set PATH=%mingw%;%PATH%
 
-set dll=7z.dll libsqlite3-0.dll imagequant.dll libgeotiff.dll libminizip-ng-1.dll libcurl-4.dll libfreeimage-3.dll
+set dll=7z.dll libsqlite3-0.dll libgeotiff.dll libminizip-ng-1.dll libfreeimage-3.dll
 
 for %%i in (%dll%) do (
   echo Copying: %%i
@@ -13,7 +13,7 @@ for %%i in (%dll%) do (
 )
 
 for %%i in (%dll%) do (
-  python ./dllfetch/dllfetch.py "%lib:\=/%%%i" --dir "%mingw:\=/%" --target-dir "%lib:\=/%"
+  python ./dllfetch/dllfetch.py "%lib:\=/%%%i" --dir "%mingw:\=/%" --target-dir "%lib:\=/%" --ignore libcurl-4.dll imagequant.dll
 )
 
 pause
