e403bacd9d9a553f5517566ddfcf4b86bb6d0e80
zed <starmen@tut.by>
2025-12-31 17:00:07 +0300

    Обновление дефолтного юзерагента

--- Src/Network/c_InetConfig.pas
+++ Src/Network/c_InetConfig.pas
@@ -25,7 +25,7 @@ interface
 
 const
   cUserAgent =
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:143.0) Gecko/20100101 Firefox/143.0';
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:146.0) Gecko/20100101 Firefox/146.0';
 
 implementation
 
